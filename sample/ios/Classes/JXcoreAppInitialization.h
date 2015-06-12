132a2299e0cfcd5321987232be5b967125e9e3fe
Brian Lambert <brianlambert@gmail.com>
2015-06-12 15:43:42 -0400

    Adding samlple JXcoreAppInitialization class which allows jxcore-cordova plugin to initialzie a JXcore iOS application.

---
+++ sample/ios/Classes/JXcoreAppInitialization.h
@@ -0,0 +1,15 @@
+//
+//  JXcoreAppInitialization.h
+//  AA1
+//
+//  Created by Brian Lambert on 6/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "JXcoreAppInitializationProtocol.h"
+
+// JXcoreAppInitialization interface.
+@interface JXcoreAppInitialization : NSObject <JXcoreAppInitializationProtocol>
+
+@end
---
+++ sample/ios/Classes/JXcoreAppInitialization.m
@@ -0,0 +1,20 @@
+//
+//  JXcoreAppInitialization.m
+//  AA1
+//
+//  Created by Brian Lambert on 6/12/15.
+//
+//
+
+#import "JXcoreAppInitialization.h"
+
+// JXcoreAppInitialization implementation.
+@implementation JXcoreAppInitialization
+
+// Initializes the JXcore app.
++ (void)initializeApp
+{
+    NSLog(@"!!!!!!!!!!!!!!! HERE");
+}
+
+@end
