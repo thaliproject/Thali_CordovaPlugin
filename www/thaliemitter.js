b98ece4927a4c76bcf638d39763608734657e0c1
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-06-19 16:37:17 -0400

    Adding through getPeerIdentifier

--- www/thaliemitter.js
+++ www/thaliemitter.js
@@ -1,12 +1,41 @@
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('util').inherits;
 
-
-
 function ThaliEmitter() {
   EventEmitter.call(this);
 }
 
 inherits(ThaliEmitter, EventEmitter);
+
+ThaliEmitter.prototype.getDeviceName = function (cb) {
+  cordova('GetDeviceName').callNative(function (deviceName) {
+    cb(null, deviceName);
+  });
+};
+
+ThaliEmitter.prototype.getFreePort = function (cb) {
+  cordova('getFreePort').callNative(function (freePort) {
+    cb(null, freePort);
+  });
+};
+
+ThaliEmitter.getPeerIdentifier = function (cb) {
+  var key = 'PeerIdentifier';
+  cordova('GetKeyValue').callNative(key, function (value) {
+    if (value !== undefined) {
+      cb(null, value);
+    } else {
+      cordova('MakeGUID').callNative(function (guid) {
+        cordova('SetKeyValue').callNative(key, guid, function (response) {
+          if (!response.result) {
+            cb(new Error('Failed to save peer identifier'));
+          } else {
+            cb(null, guid);
+          }
+        })
+      })
+    }
+  })
+};
 
 module.exports = ThaliEmitter;
