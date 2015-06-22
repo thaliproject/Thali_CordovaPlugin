c0d0da2d01194aeaa3d73163605f0d8bd06f6042
Yaron Y Goland <yarong@microsoft.com>
2015-06-21 18:45:54 -0700

    Changed getServerPortFunction to a promise
    In the previous design getServerPortFunction was a synchronous function
    which wasn’t going to work for Story -1 which requires a bunch of
    native code to set up local connections before it’s ready to work. We
    needed an asynchronous function. So I changed the function to be a
    promise.

--- test/sockettest.js
+++ test/sockettest.js
@@ -4,6 +4,9 @@ var net = require('net');
 var randomstring = require('randomstring');
 var Promise = require('bluebird');
 
+// BUGBUG - This is really just one time use test code but I really should change the code to close the server and
+// client socket to use the bluebird using/disposer pattern.
+
 // Server
 
 // The server is basically just an implementation of echo. Anything it receives on the input it will send back on
@@ -77,63 +80,64 @@ function testMessageState(testMessage, currentMessage, clientSocket, resolve, re
 
 function socketClientTest(getServerPortFunction, messageSize) {
     return new Promise(function(resolve, reject) {
-        // This deals with an edge case I'm not sure even exists. It's theoretically possible that we get an 'end' or
-        // other error event on the client socket and so initiate a retry. But our clientSocket.destroy() call might
-        // not get completely processed before another event (such as error followed by close) gets run on the
-        // same clientSocket and now we'll retry twice! This guards against that.
-        var retryAttempted = false;
+        getServerPortFunction().then(function(serverPort) {
+            // This deals with an edge case I'm not sure even exists. It's theoretically possible that we get an 'end' or
+            // other error event on the client socket and so initiate a retry. But our clientSocket.destroy() call might
+            // not get completely processed before another event (such as error followed by close) gets run on the
+            // same clientSocket and now we'll retry twice! This guards against that.
+            var retryAttempted = false;
 
-        function restartIfTestNotPassed(testPassed, message, clientSocket, reject) {
-            // It's very difficult to know for sure when the current thread will lose control since we call into
-            // functions like destroy and rejectWithRetryError that do things like throw exceptions that can
-            // potentially cause odd changes in thread control. So we make sure to set retryAttempted to true
-            // before doing anything "interesting" to make sure we have set up effectively a locked section that
-            // will make sure that we only retry any particular attempt exactly once. In other words even if the
-            // restartIfTestNotPassed function on the same instance of socketClientTest is called multiple times
-            // by different event listeners we are guaranteed that one and only one call will result in a retry.
-            // This only works, btw, because Javascript is single threaded.
-            if (!testPassed && !retryAttempted) {
-                retryAttempted = true;
-                clientSocket.destroy();
-                rejectWithRetryError(reject, message, true);
+            function restartIfTestNotPassed(testPassed, message, clientSocket, reject) {
+                // It's very difficult to know for sure when the current thread will lose control since we call into
+                // functions like destroy and rejectWithRetryError that do things like throw exceptions that can
+                // potentially cause odd changes in thread control. So we make sure to set retryAttempted to true
+                // before doing anything "interesting" to make sure we have set up effectively a locked section that
+                // will make sure that we only retry any particular attempt exactly once. In other words even if the
+                // restartIfTestNotPassed function on the same instance of socketClientTest is called multiple times
+                // by different event listeners we are guaranteed that one and only one call will result in a retry.
+                // This only works, btw, because Javascript is single threaded.
+                if (!testPassed && !retryAttempted) {
+                    retryAttempted = true;
+                    clientSocket.destroy();
+                    rejectWithRetryError(reject, message, true);
+                }
             }
-        }
 
-        var testMessage = randomstring.generate(messageSize);
-        var serverPort = getServerPortFunction();
-        var testPassed = false;
+            var testMessage = randomstring.generate(messageSize);
+            var testPassed = false;
 
-        var clientSocket = net.createConnection( { port: serverPort }, function() {
-            console.log("We have successfully connected to the server.");
-            clientSocket.write(testMessage);
+            var clientSocket = net.createConnection( { port: serverPort }, function() {
+                console.log("We have successfully connected to the server.");
+                clientSocket.write(testMessage);
+            });
+
+            clientSocket.setTimeout(120000);
+
+            clientSocket.setKeepAlive(true);
+
+            var currentMessage = "";
+
+            clientSocket.on('data', function(data) {
+                currentMessage += data;
+                testPassed = testMessageState(testMessage, currentMessage, clientSocket, resolve, reject);
+            });
+
+            clientSocket.on('end', function() {
+                restartIfTestNotPassed(testPassed, "End before Passing", clientSocket, reject);
+            });
+
+            clientSocket.on('timeout', function() {
+                restartIfTestNotPassed(testPassed, "Timeout before Passing", clientSocket, reject);
+            });
+
+            clientSocket.on('error', function() {
+                restartIfTestNotPassed(testPassed, "Error before Passing", clientSocket, reject);
+            });
+
+            clientSocket.on('close', function() {
+                restartIfTestNotPassed(testPassed, "Close before Passing", clientSocket, reject);
+            });
         });
-
-        clientSocket.setTimeout(120000);
-
-        clientSocket.setKeepAlive(true);
-
-        var currentMessage = "";
-
-        clientSocket.on('data', function(data) {
-            currentMessage += data;
-            testPassed = testMessageState(testMessage, currentMessage, clientSocket, resolve, reject);
-        });
-
-        clientSocket.on('end', function() {
-            restartIfTestNotPassed(testPassed, "End before Passing", clientSocket, reject);
-        });
-
-        clientSocket.on('timeout', function() {
-            restartIfTestNotPassed(testPassed, "Timeout before Passing", clientSocket, reject);
-        });
-
-        clientSocket.on('error', function() {
-            restartIfTestNotPassed(testPassed, "Error before Passing", clientSocket, reject);
-        });
-
-        clientSocket.on('close', function() {
-            restartIfTestNotPassed(testPassed, "Close before Passing", clientSocket, reject);
-        })
     });
 }
 
@@ -164,7 +168,6 @@ exports.startSocketClient = function(getServerPortFunction) {
     return manageMessagePasses(getServerPortFunction, messageSize, maxPasses);
 };
 
-
 // Test
 
 function testSuccessOrFailure(server, getServerPortFunction) {
@@ -184,11 +187,13 @@ function testSuccessOrFailure(server, getServerPortFunction) {
 exports.nodeJSTest = function() {
     var serverPort = 9998;
     var server = exports.startSocketServer(serverPort);
-    return testSuccessOrFailure(server, function() { return serverPort; });
+    return testSuccessOrFailure(server, function() { return Promise.resolve(serverPort); });
 };
 
 // This is a mock up of what the actual test function for story -1 should look like. It's a mock up because
 // the native functions don't actually work yet. But this should give a sense of what things should look like.
+// Note that we should wrap the native function calls into promise based wrappers. It would make the code below
+// massively more readable. Nested callbacks suck. :(
 exports.realTest = function() {
     var serverPort = 9998;
     var server = exports.startSocketClient(serverPort);
@@ -207,13 +212,19 @@ exports.realTest = function() {
                             (peersWeAreTestingAgainst.indexOf(peer.peerIdentifier) == -1)) {
                             peersWeAreTestingAgainst.push(peer.peerIdentifier);
                             testSuccessOrFailure(server, function() {
-                                cordova('ConnectToDevice').callNative(peer.peerIdentifier,
-                                    function(connectToDeviceValue) {
-                                        var connectToDeviceValueArray = JSON.parse(connectToDeviceValue);
-                                        if (connectToDeviceValueArray[0] == "TRUE") {
-                                            return connectToDeviceValueArray[1];
-                                        }
-                                    });
+                                return new Promise(function(resolve, reject) {
+                                    cordova('ConnectToDevice').callNative(peer.peerIdentifier,
+                                        function(connectToDeviceValue) {
+                                            var connectToDeviceValueArray = JSON.parse(connectToDeviceValue);
+                                            if (connectToDeviceValueArray[0] == "TRUE") {
+                                                resolve(connectToDeviceValueArray[1]);
+                                            } else {
+                                                reject("Attempt to get port to connect to remote device failed" +
+                                                " because of " + connectToDeviceValueArray[1]);
+                                            }
+                                        });
+                                });
+
                             })
 
                         }
