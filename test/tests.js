e86257ea7478ff0a31622805145d3b4a812882b2
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-06-26 18:42:27 -0400

    Adding more tests

--- test/tests.js
+++ test/tests.js
@@ -6,23 +6,29 @@ describe('ThaliEmitter', function () {
   describe('#startBroadcasting', function () {
     it('should register the peerAvailabilityChanged event', function () {
       var emitter = new ThaliEmitter();
-      emitter.on('peerAvailabilityChanged', function (data) {
+      emitter.on(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, function (data) {
         assert.equal(data[0].peerIdentifier, 12345);
         assert.equal(data[0].peerName, 'foo');
         assert.equal(data[0].peerAvailable, true);
       });
 
-      Mobile.invokeNative('peerAvailabilityChanged', JSON.stringify([{
+      Mobile.invokeNative(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, JSON.stringify([{
         peerIdentifier: 12345,
         peerName: 'foo',
         peerAvailable: true
       }]));
-
-      assert.equal(true, true);
     });
 
     it('should register the networkChanged event', function () {
+      var emitter = new ThaliEmitter();
 
+      emitter.on(ThaliEmitter.events.NETWORK_CHANGED, function (status) {
+        assert.equal(status.isAvailable, true);
+      });
+
+      Mobile.invokeNative(ThaliEmitter.events.NETWORK_CHANGED, JSON.stringify({
+        isAvailable: true
+      }));
     })
   });
 });
