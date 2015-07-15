4531ccac74cc83ee5ab4f798ce8417c2ab92aac9
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-07-15 12:42:44 -0400

    Making spec desktop only

--- test/thaliemitterspec.js
+++ test/thaliemitterspec.js
@@ -1,47 +1,38 @@
-// Disable for on Android and iOS
-var isMobile = true;
-if (process.platform !== 'android' && process.platform !== 'ios') {
-  require('../thali/mockmobile');
-  isMobile = false;
-}
+require('../thali/mockmobile');
 var ThaliEmitter = require('../thali/thaliemitter');
 var test = require('tape');
 
 function noop () { }
 
-// Only run on desktop
-if (!isMobile) {
+test('#init should register the peerAvailabilityChanged event', function (t) {
+  var emitter = new ThaliEmitter();
 
-  test('#init should register the peerAvailabilityChanged event', function (t) {
-    var emitter = new ThaliEmitter();
-
-    emitter.on(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, function (data) {
-      t.equal(data[0].peerIdentifier, 12345);
-      t.equal(data[0].peerName, 'foo');
-      t.equal(data[0].peerAvailable, true);
-      t.end();
-    });
-
-    Mobile.invokeNative(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, [{
-      peerIdentifier: 12345,
-      peerName: 'foo',
-      peerAvailable: true
-    }]);
+  emitter.on(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, function (data) {
+    t.equal(data[0].peerIdentifier, 12345);
+    t.equal(data[0].peerName, 'foo');
+    t.equal(data[0].peerAvailable, true);
+    t.end();
   });
 
-  test('#init should register the networkChanged event', function (t) {
-    var emitter = new ThaliEmitter();
+  Mobile.invokeNative(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, [{
+    peerIdentifier: 12345,
+    peerName: 'foo',
+    peerAvailable: true
+  }]);
+});
 
-    emitter.on(ThaliEmitter.events.NETWORK_CHANGED, function (status) {
-      t.equal(status.isAvailable, true);
-      t.end();
-    });
+test('#init should register the networkChanged event', function (t) {
+  var emitter = new ThaliEmitter();
 
-    Mobile.invokeNative(ThaliEmitter.events.NETWORK_CHANGED, {
-      isAvailable: true
-    });
+  emitter.on(ThaliEmitter.events.NETWORK_CHANGED, function (status) {
+    t.equal(status.isAvailable, true);
+    t.end();
   });
-}
+
+  Mobile.invokeNative(ThaliEmitter.events.NETWORK_CHANGED, {
+    isAvailable: true
+  });
+});
 
 test('#startBroadcasting should call Mobile("StartBroadcasting") without an error', function (t) {
   var emitter = new ThaliEmitter();
@@ -50,34 +41,31 @@ test('#startBroadcasting should call Mobile("StartBroadcasting") without an erro
       port = 9001;
 
   emitter.startBroadcasting(deviceName, port, function (err) {
-    !isMobile && t.equal(Mobile('StartBroadcasting').callNativeArguments[0], deviceName);
-    !isMobile && t.equal(Mobile('StartBroadcasting').callNativeArguments[1], port);
+    t.equal(Mobile('StartBroadcasting').callNativeArguments[0], deviceName);
+    t.equal(Mobile('StartBroadcasting').callNativeArguments[1], port);
     t.equal(err, undefined);
     t.end();
   });
 
-  Mobile.invokeStartBroadcasting && Mobile.invokeStartBroadcasting();
+  Mobile.invokeStartBroadcasting();
 });
 
-if (!isMobile) {
+test('#startBroadcasting should call Mobile("StartBroadcasting") and handle an error', function (t) {
+  var emitter = new ThaliEmitter();
 
-  test('#startBroadcasting should call Mobile("StartBroadcasting") and handle an error', function (t) {
-    var emitter = new ThaliEmitter();
+  var deviceName = 'foo',
+      port = 9001,
+      errorMessage = 'fail';
 
-    var deviceName = 'foo',
-        port = 9001,
-        errorMessage = 'fail';
-
-    emitter.startBroadcasting(deviceName, port, function (err) {
-      t.equal(Mobile('StartBroadcasting').callNativeArguments[0], deviceName);
-      t.equal(Mobile('StartBroadcasting').callNativeArguments[1], port);
-      t.equal(err.message, errorMessage);
-      t.end();
-    });
-
-    Mobile.invokeStartBroadcasting(errorMessage);
+  emitter.startBroadcasting(deviceName, port, function (err) {
+    t.equal(Mobile('StartBroadcasting').callNativeArguments[0], deviceName);
+    t.equal(Mobile('StartBroadcasting').callNativeArguments[1], port);
+    t.equal(err.message, errorMessage);
+    t.end();
   });
-}
+
+  Mobile.invokeStartBroadcasting(errorMessage);
+});
 
 test('#startBroadcasting should call Mobile("StartBroadcasting") twice and throw an error', function (t) {
   var emitter = new ThaliEmitter();
@@ -89,13 +77,13 @@ test('#startBroadcasting should call Mobile("StartBroadcasting") twice and throw
 
     t.throws(function () {
       emitter.startBroadcasting(deviceName, port, noop);
-      Mobile.invokeStartBroadcasting && Mobile.invokeStartBroadcasting();
+      Mobile.invokeStartBroadcasting();
     });
 
     t.end();
   });
 
-  Mobile.invokeStartBroadcasting && Mobile.invokeStartBroadcasting();
+  Mobile.invokeStartBroadcasting();
 });
 
 test('#stopBroadcasting should call Mobile("StopBroadcasting") and throw an error without Mobile("StartBroadcasting")', function (t) {
@@ -103,7 +91,7 @@ test('#stopBroadcasting should call Mobile("StopBroadcasting") and throw an erro
 
   t.throws(function () {
     emitter.stopBroadcasting(noop);
-    Mobile.invokeStopBroadcasting && Mobile.invokeStopBroadcasting();
+    Mobile.invokeStopBroadcasting();
   });
 
   t.end();
@@ -118,39 +106,37 @@ test('#stopBroadcasting should call Mobile("StopBroadcasting") without an error'
   emitter.startBroadcasting(deviceName, port, function (err) {
 
     emitter.stopBroadcasting(function (err) {
-      !isMobile && t.equal(Mobile('StopBroadcasting').callNativeArguments.length, 1);
+      t.equal(Mobile('StopBroadcasting').callNativeArguments.length, 1);
       t.equal(err, undefined);
       t.end();
     });
 
-    Mobile.invokeStopBroadcasting && Mobile.invokeStopBroadcasting();
+    Mobile.invokeStopBroadcasting();
   });
 
-  Mobile.invokeStartBroadcasting && Mobile.invokeStartBroadcasting();
+  Mobile.invokeStartBroadcasting();
 });
 
-if (!isMobile) {
-  test('#stopBroadcasting should call Mobile("StopBroadcasting") and handle an error', function (t) {
-    var emitter = new ThaliEmitter();
+test('#stopBroadcasting should call Mobile("StopBroadcasting") and handle an error', function (t) {
+  var emitter = new ThaliEmitter();
 
-    var deviceName = 'foo',
-        port = 9001,
-        errorMessage = 'fail';
+  var deviceName = 'foo',
+      port = 9001,
+      errorMessage = 'fail';
 
-    emitter.startBroadcasting(deviceName, port, function (err) {
+  emitter.startBroadcasting(deviceName, port, function (err) {
 
-      emitter.stopBroadcasting(function (err) {
-        t.equal(Mobile('StopBroadcasting').callNativeArguments.length, 1);
-        t.equal(err.message, errorMessage);
-        t.end();
-      });
-
-      Mobile.invokeStopBroadcasting && Mobile.invokeStopBroadcasting(errorMessage);
+    emitter.stopBroadcasting(function (err) {
+      t.equal(Mobile('StopBroadcasting').callNativeArguments.length, 1);
+      t.equal(err.message, errorMessage);
+      t.end();
     });
 
-    Mobile.invokeStartBroadcasting && Mobile.invokeStartBroadcasting();
+    Mobile.invokeStopBroadcasting(errorMessage);
   });
-}
+
+  Mobile.invokeStartBroadcasting();
+});
 
 test('#connect should call Mobile("Connect") with a port and without an error', function (t) {
   var emitter = new ThaliEmitter();
@@ -160,33 +146,30 @@ test('#connect should call Mobile("Connect") with a port and without an error', 
       port = 9001;
 
   emitter.connect(peerIdentifier, function (err, localPort) {
-    !isMobile && t.equal(Mobile('Connect').callNativeArguments[0], peerIdentifier);
-    !isMobile && t.equal(port, localPort);
-    isMobile && t.ok(port >= 0);
+    t.equal(Mobile('Connect').callNativeArguments[0], peerIdentifier);
+    t.equal(port, localPort);
     t.equal(err, null);
     t.end();
   });
 
-  Mobile.invokeConnect && Mobile.invokeConnect(errorMessage, port);
+  Mobile.invokeConnect(errorMessage, port);
 });
 
-if (!isMobile) {
-  test('#connect should call Mobile("Connect") and handle an error', function (t) {
-    var emitter = new ThaliEmitter();
+test('#connect should call Mobile("Connect") and handle an error', function (t) {
+  var emitter = new ThaliEmitter();
 
-    var peerIdentifier = 123,
-        errorMessage = 'fail',
-        port = 9001;
+  var peerIdentifier = 123,
+      errorMessage = 'fail',
+      port = 9001;
 
-    emitter.connect(peerIdentifier, function (err) {
-      t.equal(Mobile('Connect').callNativeArguments[0], peerIdentifier);
-      t.equal(err.message, errorMessage);
-      t.end();
-    });
-
-    Mobile.invokeConnect && Mobile.invokeConnect(errorMessage, port);
+  emitter.connect(peerIdentifier, function (err) {
+    t.equal(Mobile('Connect').callNativeArguments[0], peerIdentifier);
+    t.equal(err.message, errorMessage);
+    t.end();
   });
-}
+
+  Mobile.invokeConnect(errorMessage, port);
+});
 
 test('#connect should call Mobile("Connect") twice with same peer identifier and throw an error', function (t) {
   var emitter = new ThaliEmitter();
@@ -199,14 +182,14 @@ test('#connect should call Mobile("Connect") twice with same peer identifier and
 
     t.throws(function () {
       emitter.connect(peerIdentifier, noop);
-      Mobile.invokeConnect && Mobile.invokeConnect(errorMessage, port);
+      Mobile.invokeConnect(errorMessage, port);
     });
 
     t.end();
 
   });
 
-  Mobile.invokeConnect && Mobile.invokeConnect(errorMessage, port);
+  Mobile.invokeConnect(errorMessage, port);
 });
 
 test('#connect should call Mobile("Connect") twice with different peer identifier and not throw an error', function (t) {
@@ -221,14 +204,14 @@ test('#connect should call Mobile("Connect") twice with different peer identifie
 
     t.doesNotThrow(function () {
       emitter.connect(peerIdentifier2, noop);
-      Mobile.invokeConnect && Mobile.invokeConnect(errorMessage, port);
+      Mobile.invokeConnect(errorMessage, port);
     });
 
     t.end();
 
   });
 
-  Mobile.invokeConnect && Mobile.invokeConnect(errorMessage, port);
+  Mobile.invokeConnect(errorMessage, port);
 });
 
 test('should call throw an error if Mobile("Disconnect") called before Mobile("Connects")', function (t) {
@@ -245,48 +228,43 @@ test('should call throw an error if Mobile("Disconnect") called before Mobile("C
 
 });
 
-if (!isMobile) {
+test('should call Mobile("Disconnect") without an error', function (t) {
+  var emitter = new ThaliEmitter();
 
-  test('should call Mobile("Disconnect") without an error', function (t) {
-    var emitter = new ThaliEmitter();
+  var peerIdentifier = 123,
+      port = 9001;
 
-    var peerIdentifier = 123,
-        port = 9001;
+  emitter.connect(peerIdentifier, function () {
 
-    emitter.connect(peerIdentifier, function () {
-
-      emitter.disconnect(peerIdentifier, function (err) {
-        t.equal(Mobile('Disconnect').callNativeArguments[0], peerIdentifier);
-        t.equal(err, undefined);
-        t.end();
-      });
-
-      Mobile.invokeDisconnect && Mobile.invokeDisconnect();
+    emitter.disconnect(peerIdentifier, function (err) {
+      t.equal(Mobile('Disconnect').callNativeArguments[0], peerIdentifier);
+      t.equal(err, undefined);
+      t.end();
     });
 
-    Mobile.invokeConnect && Mobile.invokeConnect(null, port);
+    Mobile.invokeDisconnect && Mobile.invokeDisconnect();
   });
 
+  Mobile.invokeConnect(null, port);
+});
 
-  test('should call Mobile("Disconnect") and handle an error', function (t) {
-    var emitter = new ThaliEmitter();
+test('should call Mobile("Disconnect") and handle an error', function (t) {
+  var emitter = new ThaliEmitter();
 
-    var peerIdentifier = 123,
-        port = 9001,
-        errorMessage = 'fail';
+  var peerIdentifier = 123,
+      port = 9001,
+      errorMessage = 'fail';
 
-    emitter.connect(peerIdentifier, function () {
+  emitter.connect(peerIdentifier, function () {
 
-      emitter.disconnect(peerIdentifier, function (err) {
-        t.equal(Mobile('Disconnect').callNativeArguments[0], peerIdentifier);
-        t.equal(err.message, errorMessage);
-        t.end();
-      });
-
-      Mobile.invokeDisconnect(errorMessage);
+    emitter.disconnect(peerIdentifier, function (err) {
+      t.equal(Mobile('Disconnect').callNativeArguments[0], peerIdentifier);
+      t.equal(err.message, errorMessage);
+      t.end();
     });
 
-    Mobile.invokeConnect(null, port);
+    Mobile.invokeDisconnect(errorMessage);
   });
 
-}
+  Mobile.invokeConnect(null, port);
+});
