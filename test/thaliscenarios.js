3aa950460fe827071c5a696c6c7fdbe6901cc8ac
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-07-17 10:09:09 -0400

    Fixing last test

--- test/thaliscenarios.js
+++ test/thaliscenarios.js
@@ -78,19 +78,26 @@ test('ThaliEmitter throws on disconnect to bad peer', function (t) {
   });
 });
 
-test('ThaliEmitter can discover and connect to peers', function () {
+test('ThaliEmitter can discover and connect to peers', function (t) {
   var e = new ThaliEmitter();
 
+  t.timeoutAfter(30 * 60 * 1000)
+
+
   e.startBroadcasting((+ new Date()).toString(), 5000, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1)
 
     e.on(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, function (peers) {
+      console.log('peer availability changed');
+
       peers.forEach(function (peer) {
 
         // This will only pick the first available peer
         if (peer.peerAvailable) {
+
           e.connect(peer.peerIdentifier, function (err2, port) {
-            t.noOk(err2);
+
+            t.notOk(err2);
             t.ok(port > 0 && port <= 65536);
 
             e.disconnect(peer.peerIdentifier, function (err3) {
