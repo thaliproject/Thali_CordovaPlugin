134dd2c381fadcd9ba57571d45b9cd7e448fb284
sreesharp <sreejumon@gmail.com>
2015-07-28 11:53:39 -0700

    Fixed the missing terminal output issue

--- thali/installCordovaPlugin.js
+++ thali/installCordovaPlugin.js
@@ -4,12 +4,9 @@ var exec = require('child_process').exec;
 var path = require('path');
 var currentFolder = process.cwd();
 
-String.prototype.endsWith = function(suffix) {
-    return this.indexOf(suffix, this.length - suffix.length -1) !== -1;
-};
 var jxcoreFolder = path.join(__dirname, '../../' );
-console.log(jxcoreFolder);
-if(!jxcoreFolder.endsWith('jxcore')) {
+
+if(path.basename(jxcoreFolder) !== 'jxcore') {
     console.log('Could not locate JXCore folder. Exiting the thali plugin installation..');
     return;
 }
@@ -32,13 +29,16 @@ console.log('Adding the Thali_CordovaPlugin...');
 
 exec('git clone -b story_0_sree --single-branch https://github.com/thaliproject/Thali_CordovaPlugin.git',
     function (error, stdout, stderr) {
-        exec('cordova plugin add Thali_CordovaPlugin',
+        var cordovaProcess = exec('cordova plugin add Thali_CordovaPlugin',
             function (error, stdout, stderr) {
                 console.log('Thali_CordovaPlugin plugin added successfully.......')
                 if (error !== null) {
                     console.log('Thali_CordovaPlugin install error: ' + error);
                 }
             });
+        cordovaProcess.stdout.on('data', function(data) {
+            console.log(data);
+        });
         if (error !== null) {
             console.log('Thali_CordovaPlugin git clone error: ' + error);
         }
