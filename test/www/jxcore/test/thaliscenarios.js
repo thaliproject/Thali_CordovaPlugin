87a405a8d2303a9dfdd917651db148e92893fc2e
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-07-30 10:12:53 -0700

    Fixing tests with messages

--- test/www/jxcore/test/thaliscenarios.js
+++ test/www/jxcore/test/thaliscenarios.js
@@ -7,10 +7,10 @@ test('ThaliEmitter can call startBroadcasting and endBroadcasting without error'
   var e = new ThaliEmitter();
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
 
     e.stopBroadcasting(function (err2) {
-      t.notOk(err2);
+      t.notOk(err2, 'Should be able to call stopBroadcasting without error');
       t.end();
     });
   });
@@ -20,14 +20,14 @@ test('ThaliEmitter calls startBroadcasting twice with error', function (t) {
   var e = new ThaliEmitter();
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
 
     e.startBroadcasting((+ new Date()).toString(), 5001, function (err2) {
 
-      t.assert(!!err2, err2.message);
+      t.assert(!!err2, 'Cannot call startBroadcasting twice');
 
       e.stopBroadcasting(function (err3) {
-        t.notOk(err3);
+        t.notOk(err3, 'Should be able to call stopBroadcasting without error');
         t.end();
       });
     });
@@ -38,13 +38,13 @@ test('ThaliEmitter throws on connection to bad peer', function (t) {
   var e = new ThaliEmitter();
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
 
     e.connect('foobar', function (err2, port) {
-      t.assert(!!err2, err2.message);
+      t.assert(!!err2, 'Should not connect to a bad peer');
 
       e.stopBroadcasting(function (err3) {
-        t.notOk(err3);
+        t.notOk(err3, 'Should be able to call stopBroadcasting without error');
         t.end();
       });
     });
@@ -55,13 +55,13 @@ test('ThaliEmitter throws on disconnect to bad peer', function (t) {
   var e = new ThaliEmitter();
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
 
     e.disconnect('foobar', function (err2, port) {
-      t.assert(err2, err2.message);
+      t.assert(!!err2, 'Disconnect should fail to a non-existant peer ');
 
       e.stopBroadcasting(function (err3) {
-        t.notOk(err3);
+        t.notOk(err3, 'Should be able to call stopBroadcasting without error');
         t.end();
       });
     });
@@ -80,23 +80,22 @@ test('ThaliEmitter can discover and connect to peers', function (t) {
         function connectToPeer(attempts) {
           if (attempts === 0) {
             t.fail('Connecting failed');
-            t.end();
-            return;
+            return t.end();
           }
 
           e.connect(peer.peerIdentifier, function (err2, port) {
             if (err2) {
-              setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
+              return setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
             }
 
-            t.notOk(err2, 'Connect should pass ' + (err2 ? err2.message : ''));
-            t.ok(port > 0 && port <= 65536);
+            t.notOk(err2, 'Should be able to connect without error');
+            t.ok(port > 0 && port <= 65536, 'Port should be within range');
 
             e.disconnect(peer.peerIdentifier, function (err3) {
-              t.notOk(err3, 'Disconnect should pass ' + (err3 ? err3.message : ''));
+              t.notOk(err3, 'Should be able to disconnect without error');
 
               e.stopBroadcasting(function (err4) {
-                t.notOk(err4);
+                t.notOk(err4, 'Should be able to call stopBroadcasting without error');
                 t.end();
               });
             });
@@ -109,7 +108,7 @@ test('ThaliEmitter can discover and connect to peers', function (t) {
   });
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
   });
 });
 
@@ -124,27 +123,25 @@ test('ThaliEmitter can discover and connect to peers and then fail on double con
         function connectToPeer(attempts) {
           if (attempts === 0) {
             t.fail('Connecting failed');
-            t.end();
-            return;
+            return t.end();
           }
 
           e.connect(peer.peerIdentifier, function (err2, port) {
             if (err2) {
-              setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
+              return setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
             }
 
-            t.notOk(err2, 'Connect should pass ' + (err2 ? err2.message : ''));
-            t.ok(port > 0 && port <= 65536);
+            t.notOk(err2, 'Should connect without error');
+            t.ok(port > 0 && port <= 65536, 'Port should be within range');
 
             e.connect(peer.peerIdentifier, function(err3, port) {
-              t.ok(err3, 'Error was ' + err3);
-              t.equal(port, undefined);
+              t.ok(err3, 'Should fail on double connect');
 
               e.disconnect(peer.peerIdentifier, function (err4) {
-                t.notOk(err4, 'Disconnect should pass ' + (err4 ? err4.message : ''));
+                t.notOk(err4, 'Should be able to disconnect without error');
 
                 e.stopBroadcasting(function (err4) {
-                  t.notOk(err4);
+                  t.notOk(err4, 'Should be able to call stopBroadcasting without error');
                   t.end();
                 });
               });
@@ -157,7 +154,7 @@ test('ThaliEmitter can discover and connect to peers and then fail on double con
   });
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
   });
 });
 
@@ -172,26 +169,25 @@ test('ThaliEmitter can discover and connect to peers and then fail on double dis
         function connectToPeer(attempts) {
           if (attempts === 0) {
             t.fail('Connecting failed');
-            t.end();
-            return;
+            return t.end();
           }
 
           e.connect(peer.peerIdentifier, function (err2, port) {
             if (err2) {
-              setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
+              return setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
             }
 
-            t.notOk(err2, 'Connect should pass ' + (err2 ? err2.message : ''));
-            t.ok(port > 0 && port <= 65536);
+            t.notOk(err2, 'Should be able to connect without error');
+            t.ok(port > 0 && port <= 65536, 'Port should be within range');
 
             e.disconnect(peer.peerIdentifier, function(err3) {
-              t.notOk(err3, 'Disconnect should pass ' + (err3 ? err3.message : ''));
+              t.notOk(err3, 'Should be able to disconnect without error')
 
               e.disconnect(peer.peerIdentifier, function (err4) {
-                t.ok(err4, 'Error was ' + err4);
+                t.ok(err4, 'Disconnect should fail ');
 
                 e.stopBroadcasting(function (err5) {
-                  t.notOk(err5);
+                  t.notOk(err5, 'Should be able to call stopBroadcasting without error');
                   t.end();
                 });
               });
@@ -204,7 +200,7 @@ test('ThaliEmitter can discover and connect to peers and then fail on double dis
   });
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1);
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
   });
 });
 
@@ -223,16 +219,15 @@ test('ThaliEmitter can connect and send data', function (t) {
         function connectToPeer(attempts) {
           if (attempts === 0) {
             t.fail('Connecting failed');
-            t.end();
-            return;
+            return t.end();
           }
 
           e.connect(peer.peerIdentifier, function (err2, port) {
             if (err2) {
-              setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
+              return setTimeout(function () { connectToPeer(attempts - 1); }, 1000);
             }
 
-            t.notOk(err2, 'Connect should pass ' + (err2 ? err2.message : ''));
+            t.notOk(err2, 'Should connect to peer without error');
 
             var clientSocket = net.createConnection( { port: port }, function () {
               clientSocket.write(testMessage);
@@ -250,10 +245,10 @@ test('ThaliEmitter can connect and send data', function (t) {
                 t.equal(testData, testMessage, 'the test messages should be equal');
 
                 e.disconnect(peer.peerIdentifier, function (err3) {
-                  t.notOk(err3, 'Disconnect should pass ' + (err3 ? err3.message : ''));
+                  t.notOk(err3, 'Should be able to disconnect without error');
 
                   e.stopBroadcasting(function (err4) {
-                    t.notOk(err4, 'stop broadcasting does not fail');
+                    t.notOk(err4, 'Should be able to call stopBroadcasting without error');
                     t.end();
                   });
                 });
@@ -268,6 +263,6 @@ test('ThaliEmitter can connect and send data', function (t) {
   });
 
   e.startBroadcasting((+ new Date()).toString(), 5001, function (err1) {
-    t.notOk(err1, 'startBroadcasting does not throw');
+    t.notOk(err1, 'Should be able to call startBroadcasting without error');
   });
 });
