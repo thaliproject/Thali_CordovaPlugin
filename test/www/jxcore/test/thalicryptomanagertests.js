89cecce5611a3b0f1cc4ed083d478a0a56924fa5
Srikanth Challa <srichal@microsoft.com>
2015-08-11 13:04:19 -0700

    Fixed cryptomanager test cases as per PR comments.

--- test/www/jxcore/test/thalicryptomanagertests.js
+++ test/www/jxcore/test/thalicryptomanagertests.js
@@ -8,72 +8,105 @@ var test = require('tape');
 var cryptomanager = require('../thali/thalicryptomanager');
 
 
+function deleteFolder(fileLocation, file, cb) {
+  if(file) {
+    fs.unlink(file, function(err) {
+      if(err) {
+        console.error('failed to delete the pkcs12 file - err: ', err);
+      }
+      fs.rmdir(fileLocation);
+      cb();
+    });
+  } else {
+    fs.rmdir(fileLocation);
+    cb();
+  }
+}
+
 test('successfully create a new pkcs12 file and return hash value', function (t) {
   var errorMessage = null,
-      fileLocation = './pkcs12foldergood';
+      fileLocation = './pkcs12folder';
+  
+  fs.mkdirSync(fileLocation);
   
   Mobile.storeGetDocumentsPathReturnArguments(errorMessage, fileLocation);
   
   cryptomanager.getPublicKeyHash(function (err, publicKeyHash) {
     t.equal(err, null);
 
-    var file = path.join(fileLocation, cryptomanager.pkcs12FileName);
+    var file = path.join(fileLocation, cryptomanager.getPkcs12FileName());
     fs.readFile(file, function (err, pkcs12Content) {
       if(err) {
         console.error('failed to read the pkcs12 file - err: ', err);
-        t.end();
-        return;
+        deleteFolder(fileLocation, file, function() {
+          t.end();
+          return;
+        });
       }
       var publicKey = crypto.pkcs12.
-        extractPublicKey(cryptomanager.password, pkcs12Content);
+        extractPublicKey(cryptomanager.getPassword(), pkcs12Content);
       if(!publicKey || publicKey.length <= 0) {
         console.error('extracted public key is invalid');
-        t.end();
-        return;
+        deleteFolder(fileLocation, file, function() {
+          t.end();
+          return;
+        });
       }
       t.equal(publicKeyHash, cryptomanager.
-        generateSlicedSHA256Hash(publicKey, cryptomanager.hashSizeInBytes));
+        getSlicedSHA256Hash(publicKey, cryptomanager.getHashSizeInBytes()));
+      deleteFolder(fileLocation, file, function() {
+        t.end();
+      });
     });
-    t.end();
   });
 });
-
 
 test('successfully read a previous pkcs12 file and return hash value',
   function (t) {
   var errorMessage = null,
-      fileLocation = './pkcs12foldergood';
+      fileLocation = './pkcs12folder';
+
+  fs.mkdirSync(fileLocation);
 
   Mobile.storeGetDocumentsPathReturnArguments(errorMessage, fileLocation);
 
-  var pkcs12Content = crypto.pkcs12.createBundle(cryptomanager.password,
-    cryptomanager.certname, cryptomanager.country, cryptomanager.organization);
+  var pkcs12Content = crypto.pkcs12.createBundle(cryptomanager.getPassword(),
+    cryptomanager.getCertname(), cryptomanager.getCountry(), cryptomanager.getOrganization());
   if (pkcs12Content.length <= 0) {
     console.error('failed to create pkcs12 content');
-    t.end();
-    return;
+    deleteFolder(fileLocation, null, function() {
+      t.end();
+      return;
+    });
   }
 
-  var file = path.join(fileLocation, cryptomanager.pkcs12FileName);
+  var file = path.join(fileLocation, cryptomanager.getPkcs12FileName());
   fs.writeFile(file, pkcs12Content, {flags: 'wx'}, function (err) {
     if (err) {
       console.error('failed to save pkcs12Content - err: ', err);
-      t.end();
-      return;
+      deleteFolder(fileLocation, file, function() {
+        t.end();
+        return;
+      });
     }
     
-    var publicKey = crypto.pkcs12.extractPublicKey(cryptomanager.password, pkcs12Content);
+    var publicKey = crypto.pkcs12.
+    extractPublicKey(cryptomanager.getPassword(), pkcs12Content);
     if(!publicKey || publicKey.length <= 0) {
       console.error('extracted public key is invalid');
-      t.end();
-      return;
+      deleteFolder(fileLocation, file, function() {
+        t.end();
+        return;
+      });
     }
 
     cryptomanager.getPublicKeyHash(function (err, publicKeyHash) {
       t.equal(err, null);
       t.equal(publicKeyHash, cryptomanager.
-        generateSlicedSHA256Hash(publicKey, cryptomanager.hashSizeInBytes));
-      t.end();
+        getSlicedSHA256Hash(publicKey, cryptomanager.getHashSizeInBytes()));
+      deleteFolder(fileLocation, file, function() {
+        t.end();
+      });
     });
   });
 });
