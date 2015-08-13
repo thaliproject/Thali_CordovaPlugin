56fcadfb84fb9e61199dcbdb0337b98804dae2da
Tobe <tobybrad@microssft.com>
2015-08-13 01:41:43 +0100

    Protect against race in the relays

--- src/ios/THENetworkingClientRelay.m
+++ src/ios/THENetworkingClientRelay.m
@@ -34,124 +34,133 @@
 
 -(BOOL)start
 {
-    if (_inputStream != nil && _outputStream != nil) 
+    @synchronized(self)
     {
-        NSLog(@"client: relay started");
-
-        serverSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
-        
-        NSError *err = nil;
-        if (![serverSocket acceptOnPort:0 error:&err])
+        if (_inputStream != nil && _outputStream != nil) 
         {
-            NSString *errorMsg = @"relay failed to listen";
+            NSLog(@"client: relay started");
 
-            NSLog(@"client: %@", errorMsg);
-            if ([self.delegate respondsToSelector:@selector(didNotConnectWithErrorMessage:withPeerIdentifier:)])
+            serverSocket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+            
+            NSError *err = nil;
+            if (![serverSocket acceptOnPort:0 error:&err])
             {
-                [self.delegate didNotConnectWithErrorMessage:errorMsg withPeerIdentifier:_peerIdentifier];
-            }
+                NSString *errorMsg = @"relay failed to listen";
 
-            return NO;
+                NSLog(@"client: %@", errorMsg);
+                if ([self.delegate respondsToSelector:@selector(didNotConnectWithErrorMessage:withPeerIdentifier:)])
+                {
+                    [self.delegate didNotConnectWithErrorMessage:errorMsg withPeerIdentifier:_peerIdentifier];
+                }
+
+                return NO;
+            }
+            else
+            {
+                UInt16 port = [serverSocket localPort];
+                if ([self.delegate respondsToSelector:@selector(didConnectWithLocalPort:withPeerIdentifier:)])
+                {
+                    // This is the port to which the node client will connect in order
+                    // to talk to the server
+      
+                    NSLog(@"client: relay established");
+                    [self.delegate didConnectWithLocalPort:port withPeerIdentifier:_peerIdentifier];
+                }
+                
+                return YES;
+            }
         }
         else
         {
-            UInt16 port = [serverSocket localPort];
-            if ([self.delegate respondsToSelector:@selector(didConnectWithLocalPort:withPeerIdentifier:)])
-            {
-                // This is the port to which the node client will connect in order
-                // to talk to the server
-  
-                NSLog(@"client: relay established");
-                [self.delegate didConnectWithLocalPort:port withPeerIdentifier:_peerIdentifier];
-            }
-            
-            return YES;
+            return NO;
         }
-    }
-    else
-    {
-        return NO;
     }
 }
 
 -(void)stop
 {
     NSLog(@"client: relay stopping");
-    [_socket setDelegate:nil];
-    [_socket disconnect];
-    _socket = nil;
 
-    _inputStream.delegate = nil;
-    _outputStream.delegate = nil;
+    @synchronized(self)
+    {
+        [_socket setDelegate:nil];
+        [_socket disconnect];
+        _socket = nil;
 
-    [_inputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
-    [_outputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+        _inputStream.delegate = nil;
+        _outputStream.delegate = nil;
+
+        [_inputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+        [_outputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+    }
 }
 
 -(void)stream:(NSStream *)stream handleEvent:(NSStreamEvent)eventCode
 {
-    if (stream == _inputStream) 
+    @synchronized(self)
     {
-        switch (eventCode) 
+        if (stream == _inputStream) 
         {
-            case NSStreamEventOpenCompleted:
-            break;
-            
-            case NSStreamEventHasSpaceAvailable:
-            break;
-
-            case NSStreamEventHasBytesAvailable:
+            switch (eventCode) 
             {
-                assert(_socket);
+                case NSStreamEventOpenCompleted:
+                break;
                 
+                case NSStreamEventHasSpaceAvailable:
+                break;
 
-                // Read from input stream, write to socket
-                const uint bufferValue = 1024; // 512, 1024 or 4k
-                uint8_t *buffer = malloc(bufferValue);
-                NSInteger len = [_inputStream read:buffer maxLength:sizeof(bufferValue)];
-                NSMutableData *toWrite = [[NSMutableData alloc] init];
-                [toWrite appendBytes:buffer length:len];
-                [_socket writeData:toWrite withTimeout:-1 tag:0];
+                case NSStreamEventHasBytesAvailable:
+                {
+                    assert(_socket);
 
-                NSLog(@"client: inputStream->_socket (%ld bytes)", (long)len);
+                    // Read from input stream, write to socket
+                    const uint bufferValue = 1024; // 512, 1024 or 4k
+                    uint8_t *buffer = malloc(bufferValue);
+                    NSInteger len = [_inputStream read:buffer maxLength:sizeof(bufferValue)];
+                    NSMutableData *toWrite = [[NSMutableData alloc] init];
+                    [toWrite appendBytes:buffer length:len];
+                    [_socket writeData:toWrite withTimeout:-1 tag:0];
+
+                    NSLog(@"client: inputStream->_socket (%ld bytes)", (long)len);
+                }
+                break;
+
+                case NSStreamEventEndEncountered:
+                {
+                    NSLog(@"client: relay inputStream ended");
+                } 
+                break;
+
+                case NSStreamEventErrorOccurred:
+                {
+                    NSLog(@"client: relay inputStream error");
+                }
+                break;
+
+                default: 
+                {
+                    NSLog(@"client: Unexpected case statement");
+                }
+                break;
             }
-            break;
-
-            case NSStreamEventEndEncountered:
-            {
-                NSLog(@"client: relay inputStream ended");
-            } 
-            break;
-
-            case NSStreamEventErrorOccurred:
-            {
-                NSLog(@"client: relay inputStream error");
-            }
-            break;
-
-            default: 
-            {
-                NSLog(@"client: Unexpected case statement");
-            }
-            break;
         }
-    }
-    else if (stream == _outputStream)
-    {
-        switch (eventCode) 
+        else if (stream == _outputStream)
         {
-            case NSStreamEventOpenCompleted:
-            break;
-            case NSStreamEventHasSpaceAvailable:
-            break;
-            case NSStreamEventHasBytesAvailable:
-            break;
-            case NSStreamEventEndEncountered:
-            break;
-            case NSStreamEventErrorOccurred:
-            break;
-            default:
-            break;
+            switch (eventCode) 
+            {
+                case NSStreamEventOpenCompleted:
+                break;
+                case NSStreamEventHasSpaceAvailable:
+                break;
+                case NSStreamEventHasBytesAvailable:
+                break;
+                case NSStreamEventEndEncountered:
+                break;
+                case NSStreamEventErrorOccurred:
+                break;
+                default:
+                break;
+            }
         }
     }
 }
@@ -191,14 +200,18 @@
 
 -(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
 {
-    assert(sock == _socket);
-
-    NSLog(@"client: socket->outputStream (%lu)", (unsigned long)data.length);
-    if ([_outputStream write:data.bytes maxLength:data.length] == -1)
+    @synchronized(self)
     {
-        NSLog(@"ERROR: Writing to output stream");
+        assert(sock == _socket);
+        assert(_outputStream != nil);
+
+        NSLog(@"client: socket->outputStream (%lu)", (unsigned long)data.length);
+        if ([_outputStream write:data.bytes maxLength:data.length] == -1)
+        {
+            NSLog(@"ERROR: Writing to output stream");
+        }
+        [_socket readDataWithTimeout:-1 tag:tag];
     }
-    [_socket readDataWithTimeout:-1 tag:tag];
 }
 
 @end
--- src/ios/THENetworkingServerRelay.m
+++ src/ios/THENetworkingServerRelay.m
@@ -35,39 +35,42 @@
 
 -(BOOL)start
 {
-    if (_inputStream != nil && _outputStream != nil) 
+    @synchronized(self)
     {
-        NSLog(@"server: relay started");
-
-        _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
-        
-        NSError *err = nil;
-        // This is the connection to the node server, passed in during startBroadcasting
-        if (![_socket connectToHost:@"localhost" onPort:_serverPort withTimeout:-1 error:&err])
+        if (_inputStream != nil && _outputStream != nil) 
         {
-            NSLog(@"server: relay socket connect error  %@",[err localizedDescription]);
-            return NO;
+            NSLog(@"server: relay started");
+
+            _socket = [[GCDAsyncSocket alloc] initWithDelegate:self delegateQueue:dispatch_get_main_queue()];
+            
+            NSError *err = nil;
+            // This is the connection to the node server, passed in during startBroadcasting
+            if (![_socket connectToHost:@"localhost" onPort:_serverPort withTimeout:-1 error:&err])
+            {
+                NSLog(@"server: relay socket connect error  %@",[err localizedDescription]);
+                return NO;
+            }
+            else
+            {
+                NSLog(@"server: relay connected");
+     
+                _inputStream.delegate = self;
+                [_inputStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+                [_inputStream open];
+                
+                _outputStream.delegate = self;
+                [_outputStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+                [_outputStream open];
+                
+                [_socket readDataWithTimeout:-1 tag:1];
+                
+                return YES;
+            }
         }
         else
         {
-            NSLog(@"server: relay connected");
- 
-            _inputStream.delegate = self;
-            [_inputStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
-            [_inputStream open];
-            
-            _outputStream.delegate = self;
-            [_outputStream scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
-            [_outputStream open];
-            
-            [_socket readDataWithTimeout:-1 tag:1];
-            
-            return YES;
+            return NO;
         }
-    }
-    else
-    {
-        return NO;
     }
 }
 
@@ -75,87 +78,94 @@
 {
     NSLog(@"server: relay stopping");
 
-    [_socket setDelegate:nil];
-    [_socket disconnect];
-    _socket = nil;
+    @synchronized(self)
+    {
+        [_socket setDelegate:nil];
+        [_socket disconnect];
+        _socket = nil;
 
-    _inputStream.delegate = nil;
-    _outputStream.delegate = nil;
+        _inputStream.delegate = nil;
+        _outputStream.delegate = nil;
 
-    [_inputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
-    [_outputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+        [_inputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+        [_outputStream removeFromRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+    }
 }
 
 #pragma mark - NSStreamDelegate
 
 -(void)stream:(NSStream *)aStream handleEvent:(NSStreamEvent)eventCode
 {
-    if (aStream == _inputStream) {
-        switch (eventCode) {
-            case NSStreamEventOpenCompleted:
-            {
-                NSLog(@"server: relay inputStream opened");
-            }
-            break;
-
-            case NSStreamEventHasSpaceAvailable:
-            break;
-
-            case NSStreamEventHasBytesAvailable:
-            {
-                assert(_socket);
-    
-                NSLog(@"server: relay inputStream->socket");            
-
-                // Read from input stream, write to socket
-                const uint bufferValue = 1024; // 512, 1024 or 4k
-                uint8_t *buffer = malloc(bufferValue);
-                NSInteger len = [_inputStream read:buffer maxLength:sizeof(bufferValue)];
-                NSMutableData *toWrite = [[NSMutableData alloc] init];
-                [toWrite appendBytes:buffer length:len];
-                [_socket writeData:toWrite withTimeout:-1 tag:0];
-            }
-            break;
-
-            case NSStreamEventEndEncountered:
-                NSLog(@"server: inputStream closed");
-                break;
-            case NSStreamEventErrorOccurred:
-                NSLog(@"server: inputStream error");
-                break;
-            default:
-                break;
-        }
-    }
-    else if (aStream == _outputStream)
+    @synchronized(self)
     {
-        switch (eventCode) 
+        if (aStream == _inputStream) 
         {
-            case NSStreamEventOpenCompleted:
-            {
-                NSLog(@"server: outputStream opened");
-            }
-            break;
-
-            case NSStreamEventHasSpaceAvailable:
-                break;
-            case NSStreamEventHasBytesAvailable:
+            switch (eventCode) {
+                case NSStreamEventOpenCompleted:
+                {
+                    NSLog(@"server: relay inputStream opened");
+                }
                 break;
 
-            case NSStreamEventEndEncountered:
-            {
-                NSLog(@"server: outputStream closed");
-            }
-            break;
-
-            case NSStreamEventErrorOccurred:
-            {
-                NSLog(@"server: outputStream error");
-            }
-            break;
-
-            default:
+                case NSStreamEventHasSpaceAvailable:
                 break;
+
+                case NSStreamEventHasBytesAvailable:
+                {
+                    assert(_socket);
+        
+                    NSLog(@"server: relay inputStream->socket");            
+
+                    // Read from input stream, write to socket
+                    const uint bufferValue = 1024; // 512, 1024 or 4k
+                    uint8_t *buffer = malloc(bufferValue);
+                    NSInteger len = [_inputStream read:buffer maxLength:sizeof(bufferValue)];
+                    NSMutableData *toWrite = [[NSMutableData alloc] init];
+                    [toWrite appendBytes:buffer length:len];
+                    [_socket writeData:toWrite withTimeout:-1 tag:0];
+                }
+                break;
+
+                case NSStreamEventEndEncountered:
+                    NSLog(@"server: inputStream closed");
+                    break;
+                case NSStreamEventErrorOccurred:
+                    NSLog(@"server: inputStream error");
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (aStream == _outputStream)
+        {
+            switch (eventCode) 
+            {
+                case NSStreamEventOpenCompleted:
+                {
+                    NSLog(@"server: outputStream opened");
+                }
+                break;
+
+                case NSStreamEventHasSpaceAvailable:
+                    break;
+                case NSStreamEventHasBytesAvailable:
+                    break;
+
+                case NSStreamEventEndEncountered:
+                {
+                    NSLog(@"server: outputStream closed");
+                }
+                break;
+
+                case NSStreamEventErrorOccurred:
+                {
+                    NSLog(@"server: outputStream error");
+                }
+                break;
+
+                default:
+                    break;
+            }
         }
     }
 }
@@ -186,9 +196,15 @@
 
 -(void)socket:(GCDAsyncSocket *)sock didReadData:(NSData *)data withTag:(long)tag
 {
-    [_outputStream write:data.bytes maxLength:data.length];
-    NSLog(@"server: socket->outputStream (%lu bytes)", (unsigned long)data.length);
-    [_socket readDataWithTimeout:-1 tag:tag];
+    @synchronized(self)
+    {
+        assert(sock == _socket);
+        assert(_outputStream != nil);
+
+        [_outputStream write:data.bytes maxLength:data.length];
+        NSLog(@"server: socket->outputStream (%lu bytes)", (unsigned long)data.length);
+        [_socket readDataWithTimeout:-1 tag:tag];
+    }
 }
 
 @end
