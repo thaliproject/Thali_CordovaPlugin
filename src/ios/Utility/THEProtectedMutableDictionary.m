1d3f16efded52bb3f13089f6763dd1e8fca95b47
Tobe <tobybrad@microssft.com>
2015-08-16 15:50:35 +0100

    To style guide

--- src/ios/Utility/THEProtectedMutableDictionary.m
+++ src/ios/Utility/THEProtectedMutableDictionary.m
@@ -30,61 +30,61 @@
 
 @implementation THEProtectedMutableDictionary
 {
-    pthread_mutex_t _mutex;
-    NSMutableDictionary * _dict;
+  pthread_mutex_t _mutex;
+  NSMutableDictionary * _dict;
 }
 
 - (instancetype)init
 {
-    self = [super init];
-    if (!self)
-    {
-        return nil;
-    }
+  self = [super init];
+  if (!self)
+  {
+    return nil;
+  }
  
-    _dict = [[NSMutableDictionary alloc] init];
+  _dict = [[NSMutableDictionary alloc] init];
 
-    return self;
+  return self;
 }
 
 - (void)updateForKey:(NSObject<NSCopying> *)key updateBlock:(NSObject *(^)(NSObject *))updateBlock
 {
-    pthread_mutex_lock(&_mutex);
+  pthread_mutex_lock(&_mutex);
 
-    NSObject *newObject = updateBlock(_dict[key]);
-    if (newObject == nil)
+  NSObject *newObject = updateBlock(_dict[key]);
+  if (newObject == nil)
+  {
+    [_dict removeObjectForKey:key];
+  }
+  else if (newObject != _dict[key])
+  {
+    if (newObject)
     {
-      [_dict removeObjectForKey:key];
+      _dict[key] = newObject;
     }
-    else if (newObject != _dict[key])
-    {
-      if (newObject)
-      {
-          _dict[key] = newObject;
-      }
-    }
+  }
 
-    pthread_mutex_unlock(&_mutex);
+  pthread_mutex_unlock(&_mutex);
 }
 
 - (void)updateForFilter:(BOOL(^)(NSObject *))filterBlock 
                           updateBlock:(BOOL(^)(NSObject *))updateBlock
 {
-    pthread_mutex_lock(&_mutex);
+  pthread_mutex_lock(&_mutex);
 
-    for (id key in _dict)
+  for (id key in _dict)
+  {
+    NSObject *value = _dict[key];
+    if (filterBlock(value))
     {
-        NSObject *value = _dict[key];
-        if (filterBlock(value))
-        {
-            if (!updateBlock(value))
-            {
-                break;
-            }
-        }
+      if (!updateBlock(value))
+      {
+        break;
+      }
     }
+  }
 
-    pthread_mutex_unlock(&_mutex);
+  pthread_mutex_unlock(&_mutex);
 }
 
 @end
