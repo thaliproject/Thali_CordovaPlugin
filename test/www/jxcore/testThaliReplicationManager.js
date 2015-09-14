af3ac002f108738ea51ed68077f9bd817718c1f6
tobybrad <tobybrad@microsoft.com>
2015-09-14 13:31:40 +0100

    Working tests for replicator

--- test/www/jxcore/testThaliReplicationManager.js
+++ test/www/jxcore/testThaliReplicationManager.js
@@ -1,13 +1,17 @@
 "use strict";
 
-var os = require("os");
+var os = require('os');
+var path = require('path');
 var test = require('tape');
 var uuid = require('uuid');
+var util = require('util');
+var express = require('express');
 var PouchDB = require('pouchdb');
+var bodyParser = require('body-parser');
 var randomstring = require('randomstring');
 var ThaliReplicationManager = require('thali/thalireplicationmanager');
 
-var dbPath = "."; //path.join(os.tmpdir(), 'pouchdb');
+var dbPath = path.join(os.tmpdir(), 'pouchdb');
 var LevelDownPouchDB = process.platform === 'android' || process.platform === 'ios' ?
     PouchDB.defaults({db: require('leveldown-mobile'), prefix: dbPath}) :
     PouchDB.defaults({db: require('leveldown'), prefix: dbPath});
@@ -88,62 +92,103 @@ test('ThaliReplicationManager receives identity', function (t) {
 
   manager.start(5000, 'thali');
 });
-/*
+
 test('ThaliReplicationManager replicates database', function (t) {
 
-  var db = new LevelDownPouchDB('thali');
-  var manager = new ThaliReplicationManager(db);
-
-  manager.on('startError', function () {
-    t.fail('startError event should not be emitted');
-    t.end();
-  });
- 
-  manager.on('stopError', function () {
-    t.fail('stopError event should not be emitted');
-    t.end();
-  });
+  PouchDB.debug.enable('*');
   
-  manager.on('started', function () {
-  });
+  /*
+    Create a local doc with local device name and sync to peer. On receiving that they'll swap
+    their device name for ours and sync it back. When both sides have sucesfully done this and 
+    have seen the changes we know two-sync is working.
+  */
 
-  manager.on('stopped', function () {
-    t.end();
-  });
+  // Need to recreate since reinstall of test does not delete the db
+  // meaning we get unexpected sequence numbers
 
-  // Create a doc to sync to other side
-  var mydocid = uuid.v4();
-  var myrandomdata = randomstring.generate();
-  var mydoc = {
-    _id: mydocid,
-    data : myrandomdata
-  };
+  var db = new LevelDownPouchDB('thali');
+  db.destroy(function(response) {
 
-  db.put(mydoc, function callback(err, result) {
-    t.notOk(err, "Should be able to put doc without error");
-  }); 
+    db = new LevelDownPouchDB('thali');
 
-  // Listen for changes from the other side
-  var changes = db.changes({
-    since: 'now',
-    live: true,
-    include_docs: true
-  }).on('change', function(change) {
-    console.log(change);
-    for (doc in change) {
-      if (doc.id != mydocid) {
-        // A new doc from another peer
-        console.log(doc.id);
+    var mydevicename = null;
+    var mydocid = uuid.v4();
+    var mydoc = {
+      _id: mydocid
+    };
+ 
+    // Listen for changes from the other side
+
+    var seenLocalChanges = false;
+    var seenRemoteChanges = false;
+
+    var manager = new ThaliReplicationManager(db);
+
+    var changes = db.changes({
+      since: 'now',
+      live: true,
+      include_docs: true
+    }).on('change', function(change) {
+      var doc = change.doc;
+      if (doc._id == mydocid) {
+        t.ok(doc.data == mydevicename, "1st change of local doc should contain local id");
+        seenLocalChanges = true;
+        if (seenRemoteChanges) {
+          manager.stop();
+        }
+      } else {
+        t.ok(doc.data != mydevicename, "1st change of remote doc should contain remote id");
+        seenRemoteChanges = true;
+        if (seenLocalChanges) {
+          manager.stop();
+        }
       }
-    }    
-  }).on('complete', function(info) {
-    console.log(info);
-  }).on('error', function (err) {
-    console.log(err);
-  });
+    }).on('complete', function(info) {
+      console.log(info);
+    }).on('error', function (err) {
+      err = err ? " (" + err + ")" : "";
+      t.notOk(err, "Should not see errors in changes" + err);
+    });
 
-  // Start replication
-  manager.start(5000, 'thali');
+    manager.on('startError', function () {
+      t.fail('startError event should not be emitted');
+      t.end();
+    });
+     
+    manager.on('stopError', function () {
+      t.fail('stopError event should not be emitted');
+      t.end();
+    });
+      
+    manager.on('started', function () {
+      manager.getDeviceIdentity(function (err, deviceName) {
+
+        t.notOk(err, "getDeviceIdentity should not return an error");
+        t.ok(deviceName, "deviceName should not be null");
+
+        // Actually create the new doc
+        mydevicename = deviceName;
+        mydoc.data = mydevicename;
+        var r = db.put(mydoc, function (err, result) {
+          err = err ? " (" + err + ")" : "";
+          t.notOk(err, "Should be able to put doc without error" + err);
+        });
+      }); 
+    });
+
+    manager.on('stopped', function () {
+      t.end();
+    });
+
+    var app = express();
+    app.disable('x-powered-by');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/db', require('express-pouchdb')(LevelDownPouchDB, { mode: 'minimumForPouchDB'}));
+    app.listen(4242, function() {
+      // Start replication
+      manager.start(4242, 'thali');
+    });
+  });
 });
-*/
 
