39737dddfde2e25ca444ed6f14ca925a32b5d365
Ville Rantala <ville.rantala@microsoft.com>
2015-10-28 15:52:05 +0200

    CI TEST - DO NOT MERGE!

--- thali/install/install.js
+++ thali/install/install.js
@@ -370,7 +370,7 @@ module.exports = function(callback, appRootDirectory) {
   var appScriptsFolder = path.join(appRootDirectory, "plugins/org.thaliproject.p2p/scripts");
   var jxcoreFolder = path.join(appRootDirectory, 'www/jxcore' );
 
-  var jxCoreVersionNumber = "0.0.7";
+  var jxCoreVersionNumber = "0.0.8";
 
   var thaliProjectName = "thaliproject";
   var thaliDepotName = "Thali_CordovaPlugin";
