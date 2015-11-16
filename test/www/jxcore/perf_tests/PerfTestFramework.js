bcf258d878759df4196bea1fe01fa87c6c8d40e1
Jukka Silvennoinen <jukka.silvennoinen@microsoft.com>
2015-11-16 14:41:04 +0200

    Added reporting on client side (to get values from devices not connected to the server)

--- test/www/jxcore/perf_tests/PerfTestFramework.js
+++ test/www/jxcore/perf_tests/PerfTestFramework.js
@@ -24,6 +24,8 @@ function TestFrameworkClient(name) {
 
     this.doneCallback = function(data) {
         self.emit('done',data);
+
+        self.printResults(data);
     }
 
     console.log('check test folder');
@@ -125,6 +127,146 @@ TestFrameworkClient.prototype.stopAllTests = function(doReport) {
     currentTest.removeListener('done', this.doneCallback);
     currentTest.removeListener('debug', this.debugCallback);
     currentTest = null;
+}
+
+TestFrameworkClient.prototype.printResults = function(data) {
+
+    console.log("-- RESULT DATA " + data);
+    var jsonData = JSON.parse(data);
+
+    if (jsonData) {
+
+        var results = {};
+
+        if (jsonData.peersList) {
+            this.printResultLine('peersList',jsonData.peersList);
+            this.printMinMaxLine(jsonData.peersList);
+
+        } else if (jsonData.connectList) {
+            results.connectList = [];
+            results.connectError = {};
+            this.preProcessResults(jsonData.connectList, results.connectList, results.connectError);
+        } else if (jsonData.sendList) {
+            results.sendList = [];
+            results.sendError = {};
+            this.preProcessResults(jsonData.sendList, results.sendList, results.sendError);
+        } else {
+            console.log('has unknown data : ' + data);
+        }
+
+        if(results.connectList){// && (results[devName].connectList.length > 0)) {
+            results.connectList.sort(this.compare);
+
+            this.printResultLine('connectList',results.connectList);
+            this.printMinMaxLine(results.connectList);
+
+            if(results.connectError) {
+                this.printFailedLine('connectList',results.connectError.failedPeer, results.connectError.notTriedList, results.connectList.length);
+        }
+
+        }else if (results.connectError && results.connectError.failedPeer > 0){
+            console.log("All (" + results.connectError.failedPeer + ") Re-Connect test connections failed");
+        }
+
+        if(results.sendList){// && (results[devName].sendList.length > 0)) {
+            results.sendList.sort(this.compare);
+
+            this.printResultLine('sendList',results.sendList);
+            this.printMinMaxLine(results.sendList);
+
+            if(results.sendError) {
+                this.printFailedLine('sendList',results.sendError.failedPeer, results.sendError.notTriedList, results.sendList.length);
+            }
+        }else if (results.sendError && results.sendError.failedPeer > 0){
+            console.log("All (" + results.sendError.failedPeer + ") SendData test connections failed");
+        }
+    }
+}
+
+TestFrameworkClient.prototype.printFailedLine = function(what,failedPeers, notTriedPeers,successCount) {
+
+    if(!notTriedPeers || !failedPeers ||  (failedPeers.length + successCount) <=0){
+        return;
+    }
+    console.log(what + " failed peers count : " + failedPeers.length + " [" + ((failedPeers.length * 100) / (successCount + failedPeers.length)) + " %]");
+
+    failedPeers.forEach(function(peer) {
+        console.log("- Peer ID : " + peer.name + ", Tried : " + peer.connections);
+    });
+
+    console.log(what + " never tried peers count : " + notTriedPeers.length + " [" + ((notTriedPeers.length * 100) / (successCount + failedPeers.length + notTriedPeers.length)) + " %]");
+
+    notTriedPeers.forEach(function(peer) {
+        console.log("- Peer ID : " + peer.name);
+    });
+}
+
+TestFrameworkClient.prototype.printMinMaxLine  = function(list) {
+    if(!list || list.length <= 0){
+        console.log('Results list does not contain any items');
+        return;
+    }
+    console.log('Result count ' + list.length + ', range ' + list[0].time + ' ms to  '  + list[(list.length - 1)].time + " ms.");
+}
+
+TestFrameworkClient.prototype.printResultLine  = function(what, list) {
+    console.log(what + " : 100% : " + this.getValueOf(list,1.00) + " ms, 99% : " + this.getValueOf(list,0.99)  + " ms, 95 : " + this.getValueOf(list,0.95)  + " ms, 90% : " + this.getValueOf(list,0.90) + " ms.");
+}
+
+TestFrameworkClient.prototype.preProcessResults  = function(source, target,errorTarget){
+
+    if(!target) {
+        target = [];
+    }
+    if(!errorTarget.failedPeer) {
+        errorTarget.failedPeer = [];
+    }
+    if(!errorTarget.notTriedList) {
+        errorTarget.notTriedList = [];
+    }
+
+    source.forEach(function(roundResult) {
+
+        if(!roundResult || roundResult == null){
+            return;
+        }
+
+        if (roundResult.result == "OK") {
+            target.push(roundResult);
+        } else if(roundResult.connections){
+            errorTarget.failedPeer.push(roundResult);
+        }else{ // if connections is zero, then we never got to try to connect before we got timeout
+            errorTarget.notTriedList.push(roundResult);
+        }
+    });
+}
+
+TestFrameworkClient.prototype.getValueOf  = function(array, presentage) {
+
+    if(array.length <= 0){
+        return;
+    }
+
+    var index = Math.round(array.length * presentage);
+    if(index > 0){
+        index = index - 1;
+    }
+    if(index < array.length) {
+        return array[index].time;
+    }
+}
+
+TestFrameworkClient.prototype.extendArray  = function(source, target) {
+    if(!target)
+        return source;
+    return target.concat(source);
+}
+TestFrameworkClient.prototype.compare  = function (a,b) {
+    if (a.time < b.time)
+        return -1;
+    if (a.time > b.time)
+        return 1;
+    return 0;
 }
 
 module.exports = TestFrameworkClient;
