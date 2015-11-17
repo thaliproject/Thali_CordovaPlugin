e16aa062d3a821df6d02f2497b434bfdb57cccd0
tobe <tobybrad@microsoft.com>
2015-11-17 22:52:00 +0000

    Tidy up index.js

--- test/TestServer/index.js
+++ test/TestServer/index.js
@@ -6,13 +6,9 @@
 
 'use strict';
 
-
 var options = {
   transports: ['websocket']
 };
-
-//pingTimeout: 3599000,
-//pingInterval: 60000,
 
 var app = require('express')();
 var http = require('http').Server(app)
@@ -22,40 +18,58 @@ var io = require('socket.io')(http,options);
 var TestDevice = require('./TestDevice');
 var PerfTestFramework = require('./PerfTestFramework');
 var UnitTestFramework = require('./UnitTestFramework');
-var devicesObject = JSON.parse(process.argv[2]);
 
 var timeOutValueToStart = 30000;// after 300 seconds of waiting we'll start even if we did not get desired amount of devices
 
-var TestsFrameworks  = {};
-TestsFrameworks.perftest =  {};
-TestsFrameworks.perftest.android = new PerfTestFramework("Android",devicesObject.devices.android,devicesObject.honorCount);
-TestsFrameworks.perftest.ios     = new PerfTestFramework("iOs",devicesObject.devices.ios,devicesObject.honorCount);
-TestsFrameworks.unittest =  {};
-TestsFrameworks.unittest.android = new UnitTestFramework("Android");
-TestsFrameworks.unittest.ios     = new UnitTestFramework("iOs");
+var deviceConfig = JSON.parse(process.argv[2]);
 
-var weHaveStartedTesting = false;
+// honorCount == true indicates we will always wait for the required
+// number of devices. Otherwise we proceed regardless after a timeout (only for perfTests)
+var honorCount = deviceConfig.honorCount;
+
+var TestFrameworks  = {
+  perftest : {
+    ios : new PerfTestFramework("iOs", deviceConfig.devices.ios, honorCount),
+    android : new PerfTestFramework("Android", deviceConfig.devices.android, honorCount)
+  },
+  unittest : {
+    ios : new UnitTestFramework("iOs"),
+    android : new UnitTestFramework("Android")
+  }
+};
+
 var startTimerId = null;
-if (!devicesObject.honorCount) {
+var testInProgress = false;
+
+if (!deviceConfig.honorCount) {
+
   startTimerId = setTimeout(function () {
-    console.log("-------------- We got wait done, now starting the testing process ------------------");
-    weHaveStartedTesting = true;
 
-    var perfAndCount = TestsFrameworks.perftest.android.getCount();
-    var perfIosCount = TestsFrameworks.perftest.ios.getCount();
-    var unitAndCount = TestsFrameworks.unittest.android.getCount();
-    var unitIosCount = TestsFrameworks.unittest.ios.getCount();
+    console.log("-------- Starting test (after timeout) --------");
+    testInProgress = true;
 
-    TestsFrameworks['perftest']['android'].startTest({"devices":{"ios":perfIosCount,"android":perfAndCount}});
-    TestsFrameworks['perftest']['ios'].startTest({"devices":{"ios":perfIosCount,"android":perfAndCount}});
-    TestsFrameworks['unittest']['android'].startTest({"devices":{"ios":unitIosCount,"android":unitAndCount}});
-    TestsFrameworks['unittest']['ios'].startTest({"devices":{"ios":unitIosCount,"android":unitAndCount}});
+    for (var testType in TestFrameworks) {
+      // Reform the deviceConfig to match the number of devices we actually saw by the
+      // time the timer elapsed
+      var actualConfig = { "devices" : { 
+        "ios" : TestFrameworks[testType]["ios"].getCount(),
+        "android" : TestFrameworks[testType]["android"].getCount()
+      }};
+          
+      TestFramework[testType]["ios"].startTest(actualConfig);
+      TestFramework[testType]["android"].startTest(actualConfig);
+    }
 
   }, timeOutValueToStart);
 }
 
 io.on('connection', function (socket) {
+
+  // A new device has connected to us.. we expect the next thing to happen to be
+  // a 'present' message
+
   socket.on('present', function (msg) {
+
     var presentObj = JSON.parse(msg);
     if (!presentObj.os || !presentObj.name || !presentObj.type) {
       console.log("malformed message");
@@ -63,81 +77,68 @@ io.on('connection', function (socket) {
       return;
     }
 
-    if (weHaveStartedTesting) {
-      var isThisTestFromAddedDevice = false;
-      // each file will do its own present with different name
-      //  and we need to register the names, so we can determine which tests are actually included
-      // thus we need to check whether we would need to let these present pass, even though we have started
-      // there is one connection, i.e. one socket for each device, so we use that to determine what to do
-      if (presentObj.type == "unittest") {
-        if (devicesObject.honorCount) {
-          isThisTestFromAddedDevice = TestsFrameworks['unittest'][presentObj.os].isSocketAlreadyCounted(socket);
-        }
-      } else { //perf test
-        isThisTestFromAddedDevice = TestsFrameworks['perftest'][presentObj.os].isDeviceAlreadyAdded(presentObj.name);
-      }
-
-      if (!isThisTestFromAddedDevice) {
-        console.log("too late arrival");
-        socket.emit('too_late', JSON.stringify({"timeout ": "malformed message"}));
-        return;
-      }
-    }
-
-    var newDevice = new TestDevice(socket, presentObj.name, presentObj.os, presentObj.type, presentObj.btaddress);
+    // Add the new device to the test type/os it reports as belonging too
+    var newDevice = new TestDevice(
+      socket, presentObj.name, presentObj.os, presentObj.type, presentObj.btaddress
+    );
     TestsFrameworks[presentObj.type][presentObj.os].addDevice(newDevice, socket);
 
-    // implemented by both unit & performance tests
-    if (devicesObject.honorCount && !weHaveStartedTesting) {
+    var iosTests = TestFrameworks[presentObj.type]['ios'];
+    var androidTests = TestFrameworks[presentObj.type]['android'];
+    var testFramework = TestFrameworks[presentObj.type][presentObj.os];
 
-      var androidCount = TestsFrameworks[presentObj.type]['android'].getCount();
-      var iosCount = TestsFrameworks[presentObj.type]['ios'].getCount();
+    if (!testInProgress) {
 
-      if (devicesObject.devices.android <= androidCount
-          && devicesObject.devices.ios <= iosCount) {
-        weHaveStartedTesting = true;
-        TestsFrameworks[presentObj.type]['android'].startTest({"devices": {"ios": iosCount, "android": androidCount}});
-        TestsFrameworks[presentObj.type]['ios'].startTest({"devices": {"ios": iosCount, "android": androidCount}});
+      var androidCount = androidTests.getCount();
+      var iosCount = iosTests.getCount();
+
+      if (androidCount >= devicesConfig.devices.android && iosCount > devicesConfig.devices.ios) {
+        testInProgress = true;
+        androidTests.startTest({"devices": {"ios": iosCount, "android": androidCount}});
+        iosTests.startTest({"devices": {"ios": iosCount, "android": androidCount}});
       }
     }
 
     // implemented by both unit & performance tests
     socket.on('disconnect', function () {
-      if (!weHaveStartedTesting) {
-        socket.emit('test_error', JSON.stringify({"timeout ": "message not acceptable in current Test Server state"}));
+      if (!testInProgress) {
+        socket.emit(
+          'test_error', 
+          JSON.stringify({"timeout ": "message not acceptable in current Test Server state"})
+        );
         return;
       }
-      TestsFrameworks[presentObj.type][presentObj.os].removeDevice(newDevice);
+      testFramework.removeDevice(newDevice);
     });
 
     //this event only happens with unit tests, presentObj.type == unittest
     socket.on('setup_unit_test', function (msg) {
       var msgData = JSON.parse(msg);
       if (msgData.name == newDevice.getName()) {
-        TestsFrameworks[presentObj.type][presentObj.os].addTest(newDevice, msgData.test);
+        testFramework.addTest(newDevice, msgData.test);
       }
     });
 
     //this event only happens with unit tests, presentObj.type == unittest
     socket.on('unit_test_done', function (msg) {
-      if (!weHaveStartedTesting) {
+      if (!testInProgress) {
         socket.emit('test_error', JSON.stringify({"timeout ": "message not acceptable in current Test Server state"}));
         return;
       }
 
       var msgData = JSON.parse(msg);
       if (msgData.name == newDevice.getName()) {
-        TestsFrameworks[presentObj.type][presentObj.os].ClientStopEventReceived(msgData.name, msgData.test);
+        testFramework.ClientStopEventReceived(msgData.name, msgData.test);
       }
     });
 
     //this event only happens with performance tests, presentObj.type == perftest
     socket.on('test data', function (data) {
-      if (!weHaveStartedTesting) {
+      if (!testInProgress) {
         socket.emit('test_error', JSON.stringify({"timeout ": "message not acceptable in current Test Server state"}));
         return;
       }
-      TestsFrameworks[presentObj.type][presentObj.os].ClientDataReceived(newDevice.getName(), data);
+      testFramework.ClientDataReceived(newDevice.getName(), data);
     });
   });
 });
