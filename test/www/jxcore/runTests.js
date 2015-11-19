8bbc68e90903025087f908d4cb077799b7b7c8ca
tobe <tobybrad@microsoft.com>
2015-11-19 16:43:06 +0000

    Defer test execution until .begin

--- test/www/jxcore/runTests.js
+++ test/www/jxcore/runTests.js
@@ -2,10 +2,15 @@
 
 var fs = require('fs-extra-promise');
 var path = require('path');
+var thaliTape = require('./lib/thali-tape');
 
+// Load the test modules.. execution of tests is deferred until .begin()
 fs.readdirSync(path.join(__dirname, "bv_tests")).forEach(function(fileName) {
     if ((fileName.indexOf("test") == 0) &&
         fileName.indexOf(".js", fileName.length - 3) != -1) {
         require(path.join(__dirname, "bv_tests", fileName));
     }
 });
+
+// Begin the test process
+thaliTape.begin();
