5eb6693f9956d4104a980658eb410db577040458
tobe <tobybrad@microsoft.com>
2015-11-19 16:45:29 +0000

    Generalise event handling, add some new commands

--- test/www/jxcore/lib/CoordinatorConnector.js
+++ test/www/jxcore/lib/CoordinatorConnector.js
@@ -1,37 +1,19 @@
 /**
  * Class used for connecting & communicating with the coordinator server
+ * It's responsibilities are to maintain a connection with the test server
+ * and to translate events to/from it.
  */
 'use strict';
 
 var util = require('util');
-var socketIo = require('socket.io-client');
+var io = require('socket.io-client');
 var EventEmitter = require('events').EventEmitter;
 
-function CoordinatorConnector() {
+function CoordinatorConnector() 
+{
   EventEmitter.call(this);
-}
 
-util.inherits(CoordinatorConnector, EventEmitter);
-
-var debug_log(msg) {
-  console.log("CoordinatorConnector-debug: " + msg);
-}
-
-CoordinatorConnector.prototype.init = function (ipAddress, port){
-
-  var self = this;
-  var options = {
-    transports: ['websocket']
-  };
-
-  // Connect to the server, this.socket is a singleton managed by socketIo instance
-  this.socket = socketIo('http://' + ipAddress + ':' + port + '/',options);
-  this.socket.on('connect', function () {
-    debug_log("connected");
-    self.emit('connect');
-  });
-
-  // Mapping of errors to event to emit when they occur
+  // Mapping of errors to events to emit when they occur
   this.errors = {
     "error" : "error",
     "connect_error" : "error",
@@ -39,65 +21,131 @@ CoordinatorConnector.prototype.init = function (ipAddress, port){
     "test_error" : "test_error"
   };
 
-  for (var e in this.errors) {
-    this.socket.on(e, function(err) {
-      debug_log(e);
-      self.emit(self.errors[e], JSON.stringify({type:e, data:err}));
-    });
-  }
-
-  // Mapping of server messages to events to emit when the occur
+  // Mapping of server/socket messages to events to emit when the occur
   this.messages = {
+    "connect" : "connect",
     "disconnect" : "disconnect",
     "command" : "command",
+    "register_tests" : "register_tests",
     "start_unit_test" : "setup_ready",
     "end_unit_test" : "tear_down_ready",
     "too_late" : "too_late",
-    "start_tests" : "start_tests"
+    "start_tests" : "start_tests",
+    "setup" : "setup",
+    "start_test" : "start_test",
+    "teardown" : "teardown",
+    "schedule" : "schedule"
   };
 
+}
+
+util.inherits(CoordinatorConnector, EventEmitter);
+
+function debug_log(msg) {
+  console.log("CoordinatorConnector-debug: " + msg);
+}
+
+CoordinatorConnector.prototype.connect = function (ipAddress, port) 
+{
+  // Connect to the server, this.socket is a singleton managed by io instance
+
+  var options = {
+    transports: ['websocket']
+  };
+
+  var self = this;
+  this.socket = io('http://' + ipAddress + ':' + port + '/', options);
+
+  // We're about to do some looping with closures. Some of this
+  // code may look odd. Don't panic: http://www.mennovanslooten.nl/blog/post/62
+
+  for (var e in this.errors) {
+    this.socket.on(e, function(err) {
+      return function(data) {
+        debug_log("Socket error: " + err + ":" + data);
+        self.emit(self.errors[err], JSON.stringify({type:err, data:data}));
+      };
+    }(e));
+  }
+
   for (var m in this.messages) {
-    this.socket.on(m, function(data) {
-      debug_log(m + " Data:" + data);
-      self.emit(self.error[m], data);
-    });
+    this.socket.on(m, function(msg) {
+      return function(data) {
+        debug_log("Socket message: " + msg + ":" + data);
+        self.emit(self.messages[msg], data);
+      };
+    }(m));
   }
 };
 
-CoordinatorConnector.prototype.close = function(){
+CoordinatorConnector.prototype.close = function()
+{
+  // Disconnect from the test server
+
+  if (!this.socket) {
+    throw new Error("Not Connected");
+  }
+
   debug_log('close');
   this.socket.close();
   this.emit('closed');
 };
 
-CoordinatorConnector.prototype.present = function(name,type,bluetoothAddress){
-  if (jxcore.utils.OSInfo().isAndroid) {
+CoordinatorConnector.prototype.present = function(name, type, tests, bluetoothAddress) 
+{
+  // Inform the test server who we are, what type of test we're prepared to run
+  // and what our bluetoothAddress is (Android only)
+
+  console.log(tests);
+  if (typeof jxcore !== 'undefined' && jxcore.utils.OSInfo().isAndroid) {
     this.socket.emit('present', JSON.stringify({
       "os": "android", 
       "name": name,
       "type": type,
+      "tests": tests,
       "btaddress": bluetoothAddress
     }));
   } else {
     this.socket.emit('present', JSON.stringify({
       "os" : "ios",
       "name": name,
-      "type": type
+      "type": type,
+      "tests": tests
     }));
   }
 };
 
-CoordinatorConnector.prototype.sendData = function(data){
-  console.log('DBG, CoordinatorConnector sendData called');
+CoordinatorConnector.prototype.sendData = function(data) 
+{
+  // Send test data to the server (debug only)
+
+  debug_log("sendData");
   this.socket.emit('test data', data);
 };
 
-CoordinatorConnector.prototype.setUp = function(deviceName,testName){
-  this.socket.emit('setup_unit_test', JSON.stringify({"name":deviceName,"test":testName}));
+CoordinatorConnector.prototype.scheduleComplete = function()
+{
+  // Inform the test server that we have completed set up
+  this.socket.emit('schedule_complete');
 };
 
-CoordinatorConnector.prototype.tearDown = function(deviceName,testName){
-  this.socket.emit('unit_test_done', JSON.stringify({"name":deviceName,"test":testName}));
+CoordinatorConnector.prototype.setupComplete = function(deviceName, testName)
+{
+  // Inform the test server that we have completed set up
+  this.socket.emit('setup_complete', JSON.stringify({"name":deviceName, "test":testName}));
+};
+
+CoordinatorConnector.prototype.testComplete = function(deviceName, testName)
+{
+  // Inform the test server that we have completed set up
+  this.socket.emit('test_complete', JSON.stringify({"name":deviceName, "test":testName}));
+};
+
+
+CoordinatorConnector.prototype.teardownComplete = function(deviceName, testName)
+{
+  // Inform the test server that we are tearing down the named test
+  this.socket.emit('teardown_complete', JSON.stringify({"name":deviceName, "test":testName}));
 };
 
 module.exports = CoordinatorConnector;
