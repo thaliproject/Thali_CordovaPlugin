3cadc6eac6b0e54ae3e8973890adef3e1ab033aa
tobe <tobybrad@microsoft.com>
2015-11-19 16:44:14 +0000

    createStream starts tests running, move and defer that until later

--- test/www/jxcore/UnitTest_app.js
+++ test/www/jxcore/UnitTest_app.js
@@ -19,49 +19,11 @@ testUtils.toggleRadios(true);
 var myName = "UNIT-TEST";
 testUtils.setMyName(myName);
 
-//app.listen(5000, function () {
-
-  var failedRows = [];
-  var rows = [], total = 0, passed = 0, failed = 0;
-
-  test.createStream({ objectMode: true })
-    .on('data', function (row) {
-        // Log for results
-        console.log(JSON.stringify(row));
-
-        if (row.type === 'assert') {
-            total++;
-            row.ok && passed++;
-            !row.ok && failed++;
-        }
-        rows.push(row);
-
-        testUtils.logMessageToScreen(row.id + ' isOK: ' + row.ok + ' : ' + row.name);
-
-        if(row.ok && row.name) {
-            if(!row.ok){
-                failedRows.push(row);
-            }
-        }
-    })
-    .on('end', function () {
-        // Log final results
-        testUtils.logMessageToScreen("------ Final results ---- ");
-
-        for(var i = 0; i < failedRows.length; i++){
-            testUtils.logMessageToScreen(failedRows[i].id + ' isOK: ' + failedRows[i].ok + ' : ' + failedRows[i].name);
-        }
-
-        testUtils.logMessageToScreen('Total: ' + total + ', Passed: ' + passed + ', Failed: ' + failed);
-        console.log('Total: %d\tPassed: %d\tFailed: %d', total, passed, failed);
-        testUtils.toggleRadios(false);
-
-        console.log("****TEST TOOK:  ms ****" );
-        console.log("****TEST_LOGGER:[PROCESS_ON_EXIT_SUCCESS]****");
-    });
-
-  require('./runTests.js');
-//});
+var failedRows = [];
+var rows = [], total = 0, passed = 0, failed = 0;
+/*
+*/
+require('./runTests.js');
 
 // Log that the app.js file was loaded.
 console.log('Test app app.js loaded');
