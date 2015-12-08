a73f0e13bcf5fb7fe18de7a4a6464a543ce9b3c6
tobe <tobybrad@microsoft.com>
2015-12-08 03:21:44 +0000

    Make collecting deviceName and bt address correctly async

--- test/www/jxcore/PerfTest_app.js
+++ test/www/jxcore/PerfTest_app.js
@@ -18,59 +18,63 @@ var TestFrameworkClient = require('./perf_tests/PerfTestFrameworkClient');
  code for connecting to the coordinator server
  -----------------------------------------------------------------------------------*/
 
-var bluetoothAddress = "";
+function getDeviceCharacteristics(cb) {
 
-var deviceName = null;
+  if (typeof jxcore == 'undefined') {
+    cb("PERF_TEST-" + Math.random(), null);
+  }
+  else if (jxcore.utils.OSInfo().isAndroid) {
 
-if (typeof jxcore == 'undefined') {
-  deviceName = "PERF_TEST-" + Math.random();
-}
-else if (jxcore.utils.OSInfo().isAndroid) {
+    Mobile('GetBluetoothAddress').callNative(function (err, address) {
 
-  Mobile('GetBluetoothAddress').callNative(function (err, address) {
+      if (err) {
+        console.log("GetBluetoothAddress returned error: " + err + ", address : " + address);
+        return;
+      }
 
-    if (err) {
-      console.log("GetBluetoothAddress returned error: " + err + ", address : " + address);
-      return;
-    }
+      var bluetoothAddress = address;
+      console.log("Got Device Bluetooth address: " + bluetoothAddress);
+      Mobile('GetDeviceName').callNative(function (name) {
 
-    bluetoothAddress = address;
-    console.log("Got Device Bluetooth address: " + bluetoothAddress);
-    Mobile('GetDeviceName').callNative(function (name) {
+        var deviceName = name + "_PT" + Math.round((Math.random() * (10000)));
+        testUtils.setMyName(deviceName);
 
-      deviceName = name + "_PT" + Math.round((Math.random() * (10000)));
-      testUtils.setMyName(deviceName);
+        // once we have had the BT off and we just turned it on,
+        // we need to wait untill the BLE support is reported rigth way
+        // seen with LG G4, Not seen with Motorola Nexus 6
+        setTimeout(function () {
+          Mobile('IsBLESupported').callNative(function (err) {
+            if (err) {
+              console.log("BLE advertisement not supported: " + err );
+              return;
+            }
+            console.log("BLE supported!!");
+          });
+        }, 5000);
 
-      // once we have had the BT off and we just turned it on,
-      // we need to wait untill the BLE support is reported rigth way
-      // seen with LG G4, Not seen with Motorola Nexus 6
-      setTimeout(function () {
-        Mobile('IsBLESupported').callNative(function (err) {
-          if (err) {
-            console.log("BLE advertisement not supported: " + err );
-            return;
-          }
-          console.log("BLE supported!!");
-        });
-      }, 5000);
+        cb(deviceName, bluetoothAddress);
+      });
     });
-  });
 
-} else {
-
-  bluetoothAddress = "C0:FF:FF:EE:42:00";
-  Mobile('GetDeviceName').callNative(function (name) {
-    deviceName = name + "_PT" + Math.round((Math.random() * (10000)));
-    testUtils.setMyName(deviceName);
-  });
+  } else {
+    var bluetoothAddress = "C0:FF:FF:EE:42:00";
+    Mobile('GetDeviceName').callNative(function (name) {
+      var deviceName = name + "_PT" + Math.round((Math.random() * (10000)));
+      testUtils.setMyName(deviceName);
+      cb(deviceName, bluetoothAddress);
+    });
+  }
 }
 
 /*----------------------------------------------------------------------------------
  code for handling test communications
  -----------------------------------------------------------------------------------*/
 
-// The test framework client will coordinate everything from here..
-var testFramework = new TestFrameworkClient(deviceName, bluetoothAddress);
+var testFramework;
+getDeviceCharacteristics(function(deviceName, bluetoothAddresss) {
+  // The test framework client will coordinate everything from here..
+  testFramework = new TestFrameworkClient(deviceName, bluetoothAddress);
+});
 
 // Log that the app.js file was loaded.
 console.log('Perf Test app loaded loaded');
