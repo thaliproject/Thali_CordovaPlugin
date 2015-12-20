bb431e62f61dfe63f0e673793fa220870de2f3f9
tobe <tobybrad@microsoft.com>
2015-12-20 22:54:49 +0000

    Swap tests, this used to work

--- test/TestServer/PerfTestConfig.js
+++ test/TestServer/PerfTestConfig.js
@@ -11,22 +11,21 @@ var config = {
   
   testConfig : [
     {
-      "name": "testSendData.js",
-      "serverTimeout": 1200000,
-      "timeout": 1000000,
-      "rounds": 1,
-      "dataTimeout": 20000,
-      "dataAmount": 100000,
-      "conReTryTimeout": 5000,
-      "conReTryCount": 5
-    },
-
-    {
       "name": "testFindPeers.js",
       "serverTimeout": 1200000,
       "timeout": 1000000,
       "rounds": 1,
       "dataTimeout": 10000,
+      "dataAmount": 100000,
+      "conReTryTimeout": 5000,
+      "conReTryCount": 5
+    },
+    {
+      "name": "testSendData.js",
+      "serverTimeout": 1200000,
+      "timeout": 1000000,
+      "rounds": 1,
+      "dataTimeout": 20000,
       "dataAmount": 100000,
       "conReTryTimeout": 5000,
       "conReTryCount": 5
---
+++ test/www/jxcore/perf_tests/testSendData2.js
@@ -0,0 +1,114 @@
+var randomstring = require('randomstring');
+var ThaliEmitter = require('thali/thaliemitter');
+var EventEmitter = require('events').EventEmitter;
+
+function testSendData2(testConfig, deviceName, addressList) {
+
+  this.testConfig = testConfig;
+  this.deviceName = deviceName;
+
+  this.addressList = addressList;
+
+  this.peers = {};
+  this.peerQueue = [];
+  this.runningPeer = false;
+  this.emitter = new ThaliEmitter();
+  this.emitter.on(ThaliEmitter.events.PEER_AVAILABILITY_CHANGED, this.peerAvailabilityChanged);
+}
+
+inherits(testSendData2, EventEmitter);
+
+testSendData2.prototype.peerAvailabilityChanged = function(peers) {
+  peers.forEach(function(peer) {
+    console.log("%s %s", peer.peerIdentifer, peer.peerAvailable);
+    if (peer.peerIdentifier in this.peers) {
+      // Update to an existing peer
+      this.peers[peerIdentifier].available = peer.peerAvailable;
+    } else {
+      // New peers
+      this.peers[peer.peerIdentifier] = { 
+        available : peer.peerAvailable,
+        arrivalTime : new Date(),
+        bytesToSend : this.testConfig.dataAmount
+      };
+      this.peerQueue.push(peer.peerIdentifier);
+    }
+  });
+  this.pumpQueue();
+}
+
+testSendData2.prototype.pumpQueue = function() {
+  if (this.peerQueue.length == 0 || this.runningPeer) {
+    return;
+  }
+
+  this.peerQueue.forEach(function(peerIdentifier) {
+    var peer = this.peers[peerIdentifier];
+    if (peerAvailable && peer.state == "waiting") {
+      startPeer(peerIdentifier);
+      break;
+    }
+  });
+}
+
+testSendData2.prototype.startPeer = function(peerIdentifier) {
+
+  this.runningPeer = true;
+  this.peers[peerIdentifier].state = "running";
+
+  console.log("Running peer:%s", peerIdentifier);
+
+  this.emitter.once(ThaliEmitter.events.CONNECTION_ERROR, function(peer) {
+    console.log("Connection error: %s", peer.peerIdentifier);
+  });
+
+  var self = this;
+  this.emitter.connect(peerIdentifier, function(err, port) {
+
+    if (err) {
+      this.peers[peerIdentifier].state = "waiting";
+      return;
+    }
+
+    // Connect to remove server and send bytes..
+    console.log("Link established to %s", peerIdentifer);
+    var sock = net.connect(port, function() {
+      console.log("Connected to %s", peerIdentifier);
+      var toSend = randomstring.generate(self.peers[peerIdentifier].bytesToSend >> 1);
+      var success = sock.write(toSend);
+      console.log("Write result: %s", sucess);
+    }
+
+    var bytesReceived = 0;
+    // Server acknowledges by sending a '.' for every 100 bytes it receives
+    sock.on('data', function(data) {
+      console.log(data);
+      self.peers[peerIdentifier].bytesToSend -= dataLength;
+      if (self.peer[peerIdentifier].bytesToSend == 0) {
+        sock.end();
+        self.emitter.disconnect(peerIdentifier, function() {
+          console.log("Completed peer:%s", peerIdentifier);
+          self.peers[peerIdentifer].state == "done";
+          self.runningPeer = false;
+          self.pumpQueue();
+        }
+      }
+    }
+
+    sock.on('error', function(reason) {
+      console.log("Socket error: %s", reason);
+    });
+  });
+}
+
+testSendData2.prototype.start = function(serverPort) {
+  this.emitter.startBroadcasting(this.deviceName, serverPort, function(err) {
+    if (err) {
+      console.log("ERROR: Couldn't start broadcasting");
+    }
+  });
+}
+
+testSendData2.prototype.stop = function() {
+  this.emitter.stopBroadcasting();
+}
