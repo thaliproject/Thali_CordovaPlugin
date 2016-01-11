79923a9af19fd1b6b7ecb2ee984736404d8f0504
tobe <tobybrad@microsoft.com>
2016-01-11 10:35:35 +0000

    Add android devices back in

--- test/TestServer/UnitTestConfig.js
+++ test/TestServer/UnitTestConfig.js
@@ -1,15 +1,16 @@
 // Some user-accessible config for unit tests
 // Format is: { platform:{ ...settings.. } }
 
-// numDevices - The number of devices the server will start a test with
+// numDevices - The number of devices the server will start a test with (-1 == all devices)
 //              Any other devices after that will just be ignored
+//              Note: Unit tests are designed to require just two devices
 
 var config = {
   ios: {
     numDevices:2
   },
   android: {
-    numDevices:0
+    numDevices:2
   }
 }
 
