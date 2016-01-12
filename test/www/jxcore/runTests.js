24b79fffe0519a5a0106ee8db2285a0d10077582
tobe <tobybrad@microsoft.com>
2016-01-12 12:10:41 +0000

    Log syntax errors etc from test files
    Log load errors
    
    Neaten up error logging
    
    Neaten up error logging

--- test/www/jxcore/runTests.js
+++ test/www/jxcore/runTests.js
@@ -7,10 +7,16 @@ var thaliTape = require('./lib/thali-tape');
 var testsToRun = process.argv.length > 2 ? process.argv[2] : 'bv_tests';
 
 fs.readdirSync(path.join(__dirname, testsToRun)).forEach(function(fileName) {
-    if ((fileName.indexOf('test') == 0) &&
-        fileName.indexOf('.js', fileName.length - 3) != -1) {
-        require(path.join(__dirname, testsToRun, fileName));
+  if ((fileName.indexOf('test') == 0) && fileName.indexOf('.js', fileName.length - 3) != -1) {
+    var filePath = path.join(__dirname, testsToRun, fileName);
+    try {
+      require(filePath);
+    } catch (e) {
+      console.error("Error loading: %s", filePath);
+      console.error(e.stack);
+      throw e; 
     }
+  }
 });
 
 thaliTape.begin();
