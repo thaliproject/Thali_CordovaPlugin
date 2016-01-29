8d2dc854dcdafaac494089610d81db8552b94f26
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2016-01-28 19:48:10 -0500

    Initial tests

--- test/www/jxcore/bv_tests/testThaliNotificationBeacons.js
+++ test/www/jxcore/bv_tests/testThaliNotificationBeacons.js
@@ -1,3 +1,15 @@
 'use strict';
 
-var tape = require('../lib/thali-tape');
+var test = require('../lib/thali-tape')();
+
+test('#parseBeacons invalid ECDH public key in beaconStreamWithPreAmble', function (t) {
+  t.end();
+});
+
+test('#parseBeacons invalid expiration in beaconStreamWithPreAmble', function (t) {
+  t.end();
+});
+
+test('#parseBeacons invalid size for encryptedBeaconKeyId in beaconStreamWithPreAmble', function (t) {
+  t.end();
+});
