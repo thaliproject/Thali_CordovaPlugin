a64db17220d86ac59e634cdf016377e3fc071ecb
tobe <tobybrad@microsoft.com>
2016-02-19 11:04:08 +0000

    Allow mock to queue up handlers

--- test/www/jxcore/lib/MockMobile.js
+++ test/www/jxcore/lib/MockMobile.js
@@ -1,41 +1,75 @@
 
+var next = {};
+var registered = {};
+
 var mocks = {
+
+  // Default handlers for mocks.
+  // If there's an entry in next[key], call that instead
 
   startListeningForAdvertisements : function(cb) {
     cb(null);
   },
   startUpdateAdvertisingAndListening : function(port, cb) {
     cb(null);
-  },
-  connect : function(peerIdentifer, cb) {
-    console.log("connect");
-    cb({});
-  },
+  }
 }
 
-var registered = {};
+
 var Mobile = function (key) {
   return  {
+
+    // Call a native function
     callNative: function () {
+
+      // If there's a handler specified then call that
+      if (key in next) {
+        var fn = next[key].shift();
+        if (next[key].length == 0) {
+          delete next[key];
+        }
+        console.log(fn);
+        console.log(fn.prototype);
+        fn.apply(this, arguments);
+        return;
+      }
+
+      // .. else call the default
       if (key in mocks) { 
         console.log("calling native: " + key);
-        mocks[key](arguments[0], arguments[1]);
+        mocks[key].apply(this, arguments);
       }
       else {
         throw new Error("Mock does not implement: ", key);
       }
     },
+
+    // Queue up handlers for native calls
+    nextNative: function(fn) {
+      if (key in next) {
+        next[key].push(fn);
+      }
+      else {
+        next[key] = [fn];
+      }
+    },
+
+    // Register a function
     registerToNative: function (callback) {
       registered[key] = callback;
     },
-    call: function() {
+
+    // Call a registered function
+    callRegistered: function() {
+      // Call a registered function
+
       if (key in registered) {
         console.log("calling node: " + key);
-        registered[key](arguments[0], arguments[1]);
+        registered[key].apply(this, arguments);
       }
       else {
         throw new Error("Function not registered: ", key);
-      }     
+      }
     }
   }
 }
