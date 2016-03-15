058a2d4ea22b313124e5f9802d42f30161a5e52a
Ville Rantala <ville.rantala@microsoft.com>
2016-03-15 13:43:46 +0200

    Enable native tests

--- test/www/jxcore/bv_tests/testThaliMobileNative.js
+++ test/www/jxcore/bv_tests/testThaliMobileNative.js
@@ -1,9 +1,5 @@
 'use strict';
 
-if (jxcore.utils.OSInfo().isMobile) {
-  return;
-}
-
 var net = require('net');
 var randomstring = require('randomstring');
 var tape = require('../lib/thali-tape');
