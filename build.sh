3afe4e82aa8de8b610e390dc8d0941236d96a6d0
Marek Czyz <mczyz@ra.rockwell.com>
2016-06-06 12:56:05 +0200

    No iOS coordinated tests

--- build.sh
+++ build.sh
@@ -65,7 +65,7 @@ echo "MC after setup desktop";ERROR_ABORT
 cd test/www/jxcore/;ERROR_ABORT
 #jx npm test;ERROR_ABORT
 #jx npm run test-meta;ERROR_ABORT
-jx npm run test-coordinated;ERROR_ABORT
+#jx npm run test-coordinated;ERROR_ABORT
 echo "MC after coordinated";ERROR_ABORT
 
 # Verify that docs can be generated
