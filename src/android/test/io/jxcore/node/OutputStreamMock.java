d6901b8b201507cb12bb0d49ffd0baad4ad7960f
Jarosław Słota <jslota@ra.rockwell.com>
2016-06-17 13:32:04 +0200

    UT for ThaliCordovaPlugin - Extracted mock classes

---
+++ src/android/test/io/jxcore/node/OutputStreamMock.java
@@ -0,0 +1,12 @@
+package io.jxcore.node;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class OutputStreamMock extends OutputStream {
+
+    @Override
+    public void write(int oneByte) throws IOException {
+
+    }
+}
