8aa0c2b9951c26862a7bacebac4b68883350dabf
Marcin Lesniczek <mlesnic@ra.rockwell.com>
2016-06-27 12:15:51 +0200

    UT for ThaliCordovaPlugin - Removed unused imports in IncomingSocketThreadTest

--- src/android/test/io/jxcore/node/IncomingSocketThreadTest.java
+++ src/android/test/io/jxcore/node/IncomingSocketThreadTest.java
@@ -1,31 +1,15 @@
 package io.jxcore.node;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothSocket;
-import android.content.Context;
-import android.os.CountDownTimer;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.thaliproject.p2p.btconnectorlib.ConnectionManager;
-import org.thaliproject.p2p.btconnectorlib.ConnectionManagerSettings;
-import org.thaliproject.p2p.btconnectorlib.DiscoveryManager;
-import org.thaliproject.p2p.btconnectorlib.DiscoveryManagerSettings;
-import org.thaliproject.p2p.btconnectorlib.PeerProperties;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNot.not;
 public class IncomingSocketThreadTest {
 
     ListenerMock mListenerMock;
