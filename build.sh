0f7437725d2e390b87ba82509f1a974e37284cf5
Marek Czyz <mczyz@ra.rockwell.com>
2016-07-27 13:39:01 +0200

    Enabled all tests

--- build.sh
+++ build.sh
@@ -58,7 +58,7 @@ thali/install/setUpDesktop.sh;ERROR_ABORT
 cd test/www/jxcore/;ERROR_ABORT
 jx npm test;ERROR_ABORT
 jx npm run test-meta;ERROR_ABORT
-#jx npm run test-coordinated;ERROR_ABORT
+jx npm run test-coordinated;ERROR_ABORT
 
 # Verify that docs can be generated
 #cd $PROJECT_ROOT/thali/;ERROR_ABORT
