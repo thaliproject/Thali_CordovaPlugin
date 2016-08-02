0ae8d5de6341ecc367409d0da590dc364cdf68c9
Marcin Lesniczek <mlesnic@ra.rockwell.com>
2016-08-02 09:51:24 +0200

    Update UnitTest_app.js

--- test/www/jxcore/UnitTest_app.js
+++ test/www/jxcore/UnitTest_app.js
@@ -13,29 +13,29 @@ if (typeof Mobile === 'undefined') {
 var testUtils = require('./lib/testUtils');
 var ThaliMobile = require('thali/NextGeneration/thaliMobile');
 var Promise = require('lie');
-var utResult;
+//var utResult;
 
 Mobile('ExecuteNativeTests').callNative(function (result) {
-  utResult = true;
+  //utResult = true;
   if (result && result.executed) {
     console.log("Total number of executed tests: ", result.total);
     console.log("Number of passed tests: ", result.passed);
     console.log("Number of failed tests: ", result.failed);
     console.log("Number of ignored tests: ", result.ignored);
     console.log("Total duration: ", result.duration);
-    if (result.failed > 0) {
-      utResult = false;
-    }
+    // if (result.failed > 0) {
+    //   utResult = false;
+    // }
   }
 });
 
-if (!utResult) {
-  console.log("Failed to execute UT.");
-  console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_FAILED]****');
-}
+// if (!utResult) {
+//   console.log("Failed to execute UT.");
+//   console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_FAILED]****');
+// }
 
 // finish testing here (the node part will be omitted)
-console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_SUCCESS]****');
+//console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_SUCCESS]****');
 
 ThaliMobile.getNetworkStatus()
 .then(function (networkStatus) {
