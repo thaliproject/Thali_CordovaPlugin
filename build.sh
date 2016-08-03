e3527ba3f2145e4c5f176606bf061ed6d4eaa12e
Marcin Lesniczek <mlesnic@ra.rockwell.com>
2016-08-03 22:04:31 +0300

    Rerun

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-### Testing environment prepares separate packages for each node.
+### Testing environment prepares separate packages for each node..
 ### Package builder calls this script with each node's IP address
 ### Make sure multiple calls to this script file compiles the application file
 
