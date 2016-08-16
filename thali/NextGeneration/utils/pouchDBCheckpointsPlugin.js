81cd762be23c32b33a90dd693212135fb8ca138a
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-08-16 17:35:22 +0300

    Fixed plugin accoriding to spec.

--- thali/NextGeneration/utils/pouchDBCheckpointsPlugin.js
+++ thali/NextGeneration/utils/pouchDBCheckpointsPlugin.js
@@ -1,61 +1,54 @@
 'use strict';
 
-module.exports = {
-  onCheckpointReached: function (handler) {
-    checkpoints = this.__opts.checkpoints || [];
-
-    addEventHandlerForCheckpointReached.bind(this)(handler);
+module.exports.onCheckpointReached = function (handler) {
+  if (!this.__checkpointPlugin) {
+    initializeCheckpointPluginData.bind(this)();
   }
+
+  addHandlerForCheckpointReached.bind(this)(handler);
 };
 
-var checkpoints;
-var events;
-var sizeWrapperInitialized;
+var initializeCheckpointPluginData = function () {
+  var plugin = this.__checkpointPlugin = {};
 
-var addEventHandlerForCheckpointReached = function (handler) {
-  if (!events) {
-    initializeDBEvents.bind(this)();
-  }
+  plugin.delay = 500;
+  plugin.handlers = [];
+  plugin.callbacksAreNotified = false;
 
-  if(!sizeWrapperInitialized) {
-    initializeSizeWrapper.bind(this)();
-  }
+  initializeCheckpoint.bind(this)();
+  initializeDBEvents.bind(this)();
+  initializeSizeWrapper.bind(this)();
+}
 
-  var db = this;
-  function checkDBSize () {
-    db.info()
-      .then(function (response) {
-        var reachedCheckpoint;
-        var dbSize = response.disk_size;
+var addHandlerForCheckpointReached = function (handler) {
+  var plugin = this.__checkpointPlugin;
 
-        checkpoints.forEach(function (checkpoint) {
-          if (dbSize >= checkpoint) {
-            reachedCheckpoint = checkpoint;
-          }
-        });
+  plugin.handlers.push(handler);
+};
 
-        if (reachedCheckpoint) {
-          handler(reachedCheckpoint);
-        }
-      })
-      .catch(function (error) {
-        console.log('Error while fetching db info: ', error);
-      });
-  }
+var initializeCheckpoint = function () {
+  var plugin = this.__checkpointPlugin;
 
-  events.on('change', checkDBSize);
+  plugin.checkpoint = this.__opts.checkpoint;
 }
 
 var initializeDBEvents = function () {
-  events = this.changes({
+  var plugin = this.__checkpointPlugin;
+  var delay = plugin.delay;
+
+  plugin.events = this.changes({
     live: true,
     since: 'now'
   });
 
-  this.on('destroyed', function () {
-    events.cancel();
+  plugin.events
+    .on('change', callOnceAfterDelay(checkDBSize.bind(this), delay));
+
+  this
+    .on('destroyed', function () {
+      plugin.events.cancel();
   });
-}
+};
 
 var initializeSizeWrapper = function () {
   if (!this.installSizeWrapper) {
@@ -63,4 +56,52 @@ var initializeSizeWrapper = function () {
   }
 
   this.installSizeWrapper();
+
+  var plugin = this.__checkpointPlugin;
+  plugin.sizeWrapperIsInitialized = true;
+};
+
+var checkDBSize = function () {
+  var db = this;
+  var plugin = this.__checkpointPlugin;
+
+  return db.info()
+    .then(function (response) {
+      var dbSize = response.disk_size;
+      var checkpoint = plugin.checkpoint;
+      var callbacksAreCalled = plugin.callbacksAreCalled;
+
+      // If a database shrinks after a callback has been already called
+      // the callback should be called again when reaching a checkpoint
+      if(callbacksAreCalled && dbSize < checkpoint) {
+        plugin.callbacksAreCalled = false;
+      }
+      // Callback should be called only once
+      // after the first reaching of a checkpoint
+      if (!callbacksAreCalled && dbSize >= checkpoint) {
+        plugin.handlers
+          .forEach(function (handler) {
+            handler(checkpoint);
+          });
+
+        plugin.callbacksAreCalled = true;
+      }
+    })
+    .catch(function (error) {
+      db.emit('error', error);
+      console.log('Error while fetching db info: ', error);
+    });
+}
+
+var callOnceAfterDelay = function (fn, delay) {
+  var timeout = null;
+  return function () {
+    if (!timeout) {
+      timeout = setTimeout(function () {
+        fn();
+        clearTimeout(timeout);
+        timeout = null;
+      }, delay);
+    }
+  }
 }
