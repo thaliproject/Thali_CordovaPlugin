642d735bb45b066f5b93cee23f17029189c5d46e
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-08-16 17:34:35 +0300

    Fixed test according to spec. Added new test case

--- test/www/jxcore/bv_tests/testPouchDBCheckpointPlugin.js
+++ test/www/jxcore/bv_tests/testPouchDBCheckpointPlugin.js
@@ -4,6 +4,7 @@ var tape = require('../lib/thaliTape');
 var sinon = require('sinon');
 
 var fs = require('fs-extra-promise');
+var Promise = require('lie');
 var PouchDB = require('pouchdb')
   .plugin(require('pouchdb-size'))
   .plugin(require('thali/NextGeneration/utils/pouchDBCheckpointsPlugin'))
@@ -11,44 +12,108 @@ var PouchDB = require('pouchdb')
      db: require('leveldown-mobile')
    });
 
-// DB defaultDirectory should be unique among all tests
-// and any instance of this test.
+// DB directory should be unique among all tests and any instance of this test.
 // This is especially required for tape.coordinated.
 var db;
-var dbName = 'pouchdb-' + Date.now();
+var dbName = 'pouchdb-uuid:' + Math.random();
 var dbOptions = {
-  checkpoints: [100, 200]
+  checkpoint: 900
 };
+var checkpointPluginDelay = 500;
 
 var test = tape({
   setup: function (t) {
     db = new PouchDB(dbName, dbOptions);
-    t.end();
+    t.end()
   },
   teardown: function (t) {
-    fs.removeSync(dbName);
-    t.end();
+    db.destroy()
+      .then(function () {
+        fs.removeSync(dbName);
+        t.end();
+      })
+      .catch(function (error) {
+        t.fail(error);
+      });
   }
 });
 
-var doc = {
-  _id: Date.now().toString()
+var Doc = function () {
+  this._id = Date.now().toString()
 };
 
-test('onCheckpointReached callback calling', function (t) {
+test('onCheckpointReached callback calling on a single change', function (t) {
   var spy = sinon.spy();
 
   db.onCheckpointReached(spy);
 
-  db.put(doc)
+  db.put(new Doc())
     .then(function () {
-      // Small latency is needed to calculate database size
+      // A small latency is needed to calculate database size after put
       setTimeout(function () {
-        t.ok(spy.calledOnce, 'checkpointReached handler should be called once');
+        t.ok(spy.calledOnce, 'checkpointReached handler should be called once. Called ' + spy.callCount + ' time(s)');
         t.end();
-      }, 100);
+      }, checkpointPluginDelay + 300);
     })
     .catch(function (error) {
       t.fail(error);
     });
 });
+
+test('onCheckpointReached callback calling on multiple changes' +
+'that are in checkpoints plugin delay interval', function (t) {
+    var spy = sinon.spy();
+
+    db.onCheckpointReached(spy);
+
+    db.put(new Doc())
+      // put some extra docs for 'change' event emission
+      .then(function () {
+        return db.put(new Doc());
+      })
+      .then(function () {
+        return db.put(new Doc());
+      })
+      .then(function () {
+        // A small latency is needed to calculate database size after put
+        setTimeout(function () {
+          t.ok(spy.calledOnce, 'checkpointReached handler should be called once. Called ' + spy.callCount + ' time(s)');
+          t.end();
+        }, checkpointPluginDelay + 300);
+      })
+      .catch(function (error) {
+        t.fail(error);
+      });
+    });
+
+//  TODO Investigate how to force PouchDB to shrink database size
+//  Atfer that test can be enabled
+/*
+test('onCheckpointReached callback calling after database shrinks', function (t) {
+  var spy = sinon.spy();
+  var doc = new Doc();
+
+  db.onCheckpointReached(spy);
+
+  db.put(doc)
+    .then(function (res) {
+      return db.get(res.id);
+    })
+    .then(function (doc) {
+      return db.remove(doc);
+    })
+    .then(function () {
+      return db.compact();
+    })
+    .then(function () {
+      // A small latency is needed to calculate database size after put
+      setTimeout(function () {
+        t.ok(spu.calledTwice, 'checkpointReached reached handler should be called twice');
+        t.end();
+      }, 200)
+    })
+    .catch(function (error) {
+      t.fail(error);
+    });
+});
+*/
