a0b5911dce2f36e4d68ea46d0c4420cbb3edea42
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-08-24 13:05:19 +0300

    Added initial implementation of multiConnect

--- thali/NextGeneration/thaliMobileNativeWrapper.js
+++ thali/NextGeneration/thaliMobileNativeWrapper.js
@@ -578,7 +578,15 @@ module.exports.getNonTCPNetworkStatus = function () {
  * the localhost port to connect to or an Error object.
  */
 module.exports._multiConnect = function (peerIdentifier) {
-  return Promise.reject(new Error('Not yet implemented'));
+  return gPromiseQueue.enqueue(function (resolve, reject) {
+    Mobile('multiConnect')
+      .callNative(function (errorMessage) {
+        if (errorMessage) {
+          return reject(new Error(errorMessage));
+        }
+        resolve();
+      });
+  });
 };
 
 /**
