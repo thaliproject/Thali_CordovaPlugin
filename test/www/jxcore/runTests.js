795e45de8b18b90fa82dcf057f21f804f29b1956
Yaron Y Goland <yarong@microsoft.com>
2016-08-24 12:11:05 -0700

    Re-activating all tests on all platforms

--- test/www/jxcore/runTests.js
+++ test/www/jxcore/runTests.js
@@ -33,8 +33,7 @@ if (hasJavaScriptSuffix(testsToRun)) {
   loadFile(path.join(__dirname, testsToRun));
 } else {
   fs.readdirSync(path.join(__dirname, testsToRun)).forEach(function (fileName) {
-    if ((fileName.indexOf('testNativeMethod') === 0) && //to enable all tests change 'testNativeMethod' to 'test'
-         hasJavaScriptSuffix(fileName)) {
+    if ((fileName.indexOf('test') === 0) && hasJavaScriptSuffix(fileName)) {
       var filePath = path.join(__dirname, testsToRun, fileName);
       loadFile(filePath);
     }
