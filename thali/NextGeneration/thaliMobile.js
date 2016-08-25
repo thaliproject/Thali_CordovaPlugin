c17535026f758e8f1556dc671f46c41275162b0b
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-08-25 16:11:20 +0300

    Fixed class name for more suitable

--- thali/NextGeneration/thaliMobile.js
+++ thali/NextGeneration/thaliMobile.js
@@ -385,7 +385,7 @@ module.exports.getNetworkStatus = function () {
  * put on the TCP connection. For some transports a handshake can take quite a
  * long time.
  */
-var AddressPortInfo = function (peer) {
+var AddressHostInfo = function (peer) {
   this.hostAddress = peer.hostAddress;
   this.portNumber = peer.portNumber;
   this.suggestedTCPTimeout = peer.suggestedTCPTimeout;
@@ -454,7 +454,7 @@ module.exports.getPeerHostInfo = function(peerIdentifier, connectionType) {
 
   var getAddressPortInfo = getAddressPortStarategies[connectionType];
   if (!getAddressPortInfo) {
-    return Promise.reject(new Error('getAddressPort is not implemented for ' + connectionType));
+    return Promise.reject(new Error('getPeerHostInfo is not implemented for ' + connectionType));
   }
 
   return getAddressPortInfo(peer);
@@ -466,7 +466,7 @@ getAddressPortStarategies[connectionTypes.MULTI_PEER_CONNECTIVITY_FRAMEWORK] = g
 getAddressPortStarategies[connectionTypes.TCP_NATIVE] = getWifiAddressPortInfo;
 
 var getBluetoothAddressPortInfo = fucntion (peer) {
-  var portInfo = new AddressPortInfo({
+  var portInfo = new AddressHostInfo({
     hostAddress: '127.0.0.1',
     portNumber: peer.portNumber,
     suggestedTCPTimeout: thaliConfig.TCP_TIMEOUT_BLUETOOTH
@@ -478,7 +478,7 @@ var getMPCFAddressPortInfo = fucntion (peer) {
   return ThaliMobileNativeWrapper
     ._multiConnect(peer.peerIdentifier)
     .then(function (portNumber) {
-      var portInfo = new AddressPortInfo({
+      var portInfo = new AddressHostInfo({
         hostAddress: '127.0.0.1',
         portNumber: portNumber,
         suggestedTCPTimeout: thaliConfig.TCP_TIMEOUT_MPCF
@@ -488,7 +488,7 @@ var getMPCFAddressPortInfo = fucntion (peer) {
 }
 
 var getWifiAddressPortInfo = fucntion (peer) {
-  var portInfo = new AddressPortInfo({
+  var portInfo = new AddressHostInfo({
     hostAddress: peer.hostAddress,
     portNumber: peer.portNumber,
     suggestedTCPTimeout: thaliConfig.TCP_TIMEOUT_WIFI
