71c862df56b77ddbe2814bb84fd836186653a221
Ilya Laryionau <larryonoff@gmail.com>
2016-08-25 13:54:44 +0300

    migrate build iOS process from cordova build into xcodebuild

--- thali/install/setUpTests.sh
+++ thali/install/setUpTests.sh
@@ -27,77 +27,136 @@ TEST_PROJECT_NAME=ThaliTest
 
 cd `dirname $0`
 cd ../..
-REPO_ROOT_PATH=$(pwd)
+REPO_ROOT_DIR=$(pwd)
+TEST_PROJECT_ROOT_DIR=$(readlink -f ${REPO_ROOT_DIR}/../${TEST_PROJECT_NAME})
 
+# Prepares test project
+prepare_project()
+{
+  cd $REPO_ROOT_DIR/test/TestServer
+  jx npm install
+  jx generateServerAddress.js $2
+  cd $REPO_ROOT_DIR/..
+  cordova create $TEST_PROJECT_NAME com.test.thalitest $TEST_PROJECT_NAME
+  mkdir -p $TEST_PROJECT_NAME/thaliDontCheckIn/localdev
 
-# Begin setting up the project
-#
-cd test/TestServer
-jx npm install
-jx generateServerAddress.js $2
-cd $REPO_ROOT_PATH/..
-cordova create $TEST_PROJECT_NAME com.test.thalitest $TEST_PROJECT_NAME
-mkdir -p $TEST_PROJECT_NAME/thaliDontCheckIn/localdev
+  if [ $IS_MINIGW_PLATFORM == true ]; then
+      # The thali package might be installed as link and there will
+      # be troubles later on if this link is tried to be copied so
+      # remove it here.
+      rm -rf $REPO_ROOT_DIR/test/www/jxcore/node_modules/thali
+      cp -R $REPO_ROOT_DIR/test/www/ $TEST_PROJECT_NAME/
+  else
+      rsync -a --no-links $REPO_ROOT_DIR/test/www/ $TEST_PROJECT_NAME/www
+  fi
+}
 
-if [ $IS_MINIGW_PLATFORM == true ]; then
-    # The thali package might be installed as link and there will
-    # be troubles later on if this link is tried to be copied so
-    # remove it here.
-    rm -rf $REPO_ROOT_PATH/test/www/jxcore/node_modules/thali
-    cp -R $REPO_ROOT_PATH/test/www/ $TEST_PROJECT_NAME/
-else
-    rsync -a --no-links $REPO_ROOT_PATH/test/www/ $TEST_PROJECT_NAME/www
-fi
+# Installs Thali
+install_thali()
+{
+  cd $TEST_PROJECT_ROOT_DIR/www/jxcore
+  jx npm install $REPO_ROOT_DIR/thali --save --no-optional --autoremove "*.gz"
 
-# Begin adding platforms
-#
-cd $TEST_PROJECT_NAME
+  if [ $IS_MINIGW_PLATFORM == true ]; then
+      # On Windows the package.json file will contain an invalid local file URI for Thali,
+      # which needs to be replaced with a valid value. Otherwise the build process
+      # will be aborted. Restore write permission after running sed in case
+      # Windows security settings removed it.
+      sed -i 's/"thali": ".*"/"thali": "*"/g' package.json
+      chmod 644 package.json
+  fi
 
-# add Android platform
-cordova platform add android
+  # SuperTest which is used by some of the BVTs include a PEM file (for private
+  # keys) that makes Android unhappy so we remove it below in addition to the gz
+  # files.
+  jx npm install --no-optional --autoremove "*.gz,*.pem"
 
-# A file that identifies the current build as a UT build, which results in copying native UT files to the platform folder
-touch platforms/android/unittests
+  # In case autoremove fails to delete the files, delete them explicitly.
+  find . -name "*.gz" -delete
+  find . -name "*.pem" -delete
 
-# add iOS platform
-if [ $IS_DARWIN_PLATFORM == true ]; then
-  cordova platform add ios
+  cp -v $1 app.js
+}
+
+add_android_platform()
+{
+  cd $TEST_PROJECT_ROOT_DIR
+
+  cordova platform add android
 
   # A file that identifies the current build as a UT build
-  touch platforms/ios/unittests
-fi
+  touch platforms/android/unittests
+}
 
-# run Thali install
-#
-cd www/jxcore
-jx npm install $REPO_ROOT_PATH/thali --save --no-optional --autoremove "*.gz"
+# Prepares and builds Android app
+build_android()
+{
+  cd $TEST_PROJECT_ROOT_DIR
 
-if [ $IS_MINIGW_PLATFORM == true ]; then
-    # On Windows the package.json file will contain an invalid local file URI for Thali,
-    # which needs to be replaced with a valid value. Otherwise the build process
-    # will be aborted. Restore write permission after running sed in case
-    # Windows security settings removed it.
-    sed -i 's/"thali": ".*"/"thali": "*"/g' package.json
-    chmod 644 package.json
-fi
+  cordova build android --release --device
+}
 
-# SuperTest which is used by some of the BVTs include a PEM file (for private
-# keys) that makes Android unhappy so we remove it below in addition to the gz
-# files.
-jx npm install --no-optional --autoremove "*.gz,*.pem"
+add_ios_platform_if_possible()
+{
+  if [ $IS_DARWIN_PLATFORM == true ]; then
+    cd $TEST_PROJECT_ROOT_DIR
 
-# In case autoremove fails to delete the files, delete them explicitly.
-find . -name "*.gz" -delete
-find . -name "*.pem" -delete
+    cordova platform add ios
 
-cp -v $1 app.js
+    # A file that identifies the current build as a UT build
+    touch platforms/ios/unittests
+  fi
+}
 
-# build Android
-cordova build android --release --device
+# Prepares and builds iOS app
+build_ios_if_possible()
+{
+  if [ $IS_DARWIN_PLATFORM == true ]; then
+    cd $TEST_PROJECT_ROOT_DIR
 
-# build iOS
-if [ $IS_DARWIN_PLATFORM == true ]; then
-  cordova build ios --device
-fi
+    # cordova really doesn't have any flexibility in making builds via CLAIM
+    # they're using their xcconfig that doesn't work in our case
+    # in ideal case we just run the command below
+    # `cordova build ios --device`
+    #
+    # in our case we have to build directly using xcode cli
+    # since we have some required project settings
+    # we've analyzed `cordova build` source code
+    # and emulated it's behaviour
+    #
+    # according to cordova build documentation
+    # it's a shortcut for `cordova prepare` + `cordova compile`
+    # so we have to run cordova prepare and xcodebuild then
+
+    cordova prepare ios --device
+
+    TEST_PROJECT_DIR=$TEST_PROJECT_ROOT_DIR/platforms/ios
+    TEST_PROJECT_PATH=$TEST_PROJECT_DIR/$TEST_PROJECT_NAME.xcodeproj
+
+    echo "Building project: ${TEST_PROJECT_PATH}"
+
+    (\
+    cd $TEST_PROJECT_DIR && \
+    xcodebuild \
+      -xcconfig $REPO_ROOT_DIR/thali/install/ios/build-ci.xcconfig \
+      -project $TEST_PROJECT_NAME.xcodeproj \
+      -target $TEST_PROJECT_NAME \
+      -configuration Debug \
+      -destination 'platform=iOS' \
+      build \
+      CONFIGURATION_BUILD_DIR="${TEST_PROJECT_DIR}/build/device" \
+      SHARED_PRECOMPS_DIR="${TEST_PROJECT_DIR}/build/sharedpch" \
+    )
+
+  fi
+}
+
+# Please note that functions order is important
+prepare_project $1 $2
+# add_android_platform
+add_ios_platform_if_possible
+install_thali $1 $2
+# build_android
+build_ios_if_possible
 
 echo "Remember to start the test coordination server by running jx index.js"
