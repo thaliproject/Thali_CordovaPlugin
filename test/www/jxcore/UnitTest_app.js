d2195a926f32099ac51a3b3d309eb82c71555f3f
Ilya Laryionau <larryonoff@gmail.com>
2016-08-25 21:32:32 +0300

    update node.js `executeNativeTests` native call

--- test/www/jxcore/UnitTest_app.js
+++ test/www/jxcore/UnitTest_app.js
@@ -13,22 +13,34 @@ if (typeof Mobile === 'undefined') {
 var testUtils = require('./lib/testUtils');
 var ThaliMobile = require('thali/NextGeneration/thaliMobile');
 var Promise = require('lie');
-var utResult;
+var utResult = false;
 
 if (process.platform === 'android' || process.platform === 'ios') {
   Mobile('executeNativeTests').callNative(function (result) {
-    utResult = true;
+    if (result) {
+      if (result.executed) {
+        console.log('*Native tests were executed*');
 
-    if (result && result.executed) {
+        if (result.failed > 0) {
+          utResult = false;
+        } else {
+          utResult = true;
+        }
+      } else {
+        console.log('*Native tests were not executed*');
+
+        utResult = false;
+      }
+
       console.log('Total number of executed tests: ', result.total);
       console.log('Number of passed tests: ', result.passed);
       console.log('Number of failed tests: ', result.failed);
       console.log('Number of ignored tests: ', result.ignored);
       console.log('Total duration: ', result.duration);
+    } else {
+      console.log('*Native tests results are empty*');
 
-      if (result.failed > 0) {
-        utResult = false;
-      }
+      utResult = false;
     }
   });
 
