49fbd6ad878772314df082fa41e1e1a4bcbd96d6
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-08-25 15:49:18 +0300

    Implemented ThaliMobileNativeWrapper multiConnect method

--- thali/NextGeneration/thaliMobile.js
+++ thali/NextGeneration/thaliMobile.js
@@ -466,7 +466,7 @@ var getBluetoothAddressPortInfo = fucntion (peer) {
 }
 
 var getMPCFAddressPortInfo = fucntion (peer) {
-  return thaliMobileNativeWrapper
+  return ThaliMobileNativeWrapper
     ._multiConnect(peer.peerIdentifier)
     .then(function (portNumber) {
       var portInfo = new AddressPortInfo({
