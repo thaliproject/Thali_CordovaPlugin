59eaa8ee80050740b7cc35a3bb97c4ed7fcc4821
mlesnic <mlesnic@ra.rockwell.com>
2016-08-26 14:52:42 +0200

    Tests

--- src/android/test/com/test/thalitest/RegisterExecuteUT.java
+++ src/android/test/com/test/thalitest/RegisterExecuteUT.java
@@ -5,30 +5,97 @@ import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.thaliproject.p2p.btconnectorlib.PeerProperties;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Date;
 
+import io.jxcore.node.ConnectionHelper;
+import io.jxcore.node.ConnectionHelperTest;
 import io.jxcore.node.jxcore;
 
 public final class RegisterExecuteUT {
     private RegisterExecuteUT() throws Exception {
         throw new Exception("Constructor should not be called.");
-
     }
 
+    static String TAG = "RegisterExecuteUT";
+
+    private static void FireTestedMethod(String methodName) {
+        ConnectionHelperTest.mConnectionHelper = new ConnectionHelper();
+        try {
+                if(methodName.equals("onPeerLost")){
+                    Method onPeerLostMethod = ConnectionHelperTest.mConnectionHelper.getClass().getMethod(methodName, PeerProperties.class);
+                    onPeerLostMethod.invoke(ConnectionHelperTest.mConnectionHelper, new PeerProperties("11:22:33:22:11:00"));
+                }
+                if(methodName.equals("onPeerDiscovered")){
+                    Method onPeerDiscoveredMethod = ConnectionHelperTest.mConnectionHelper.getClass().getMethod(methodName, PeerProperties.class);
+                    onPeerDiscoveredMethod.invoke(ConnectionHelperTest.mConnectionHelper, new PeerProperties("33:44:55:44:33:22"));
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+
     public static void Register() {
-        jxcore.RegisterMethod("executeNativeTests", new jxcore.JXcoreCallback() {
+        jxcore.RegisterMethod("TestNativeMethod", new jxcore.JXcoreCallback() {
+            @Override
+            public void Receiver(ArrayList<Object> params, final String callbackId) {
+                String methodToTest = "";
+
+                if (params.size() == 0) {
+                    Log.e(TAG, "Required parameter (toast message) missing");
+                } else {
+                    methodToTest = params.get(0).toString();
+                    FireTestedMethod(methodToTest);
+                }
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("Testing_", methodToTest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                final String jsonObjectAsString = jsonObject.toString();
+
+                jxcore.CallJSMethod(callbackId, jsonObjectAsString);
+            }
+        });
+
+        jxcore.RegisterMethod("ExecuteNativeTests", new jxcore.JXcoreCallback() {
             @Override
             public void Receiver(ArrayList<Object> params, String callbackId) {
-                String logtag = "executeNativeTests";
+                ConnectionHelperTest.mConnectionHelper = new ConnectionHelper();
+                String logtag = "ExecuteNativeTests";
                 Log.d(logtag, "Running unit tests");
                 Result resultTest = ThaliTestRunner.runTests();
 
                 JSONObject jsonObject = new JSONObject();
                 Boolean jsonObjectCreated = false;
+                String failures = "";
+
+                for (Failure failure: resultTest.getFailures()) {
+                    failures += failure.getMessage() + "\n";
+                }
 
                 try {
+                    if(!failures.equals("")){
+                        jsonObject.put("failures", failures);
+                    }
+
                     jsonObject.put("total", resultTest.getRunCount());
                     jsonObject.put("passed", resultTest.getRunCount() -
                             resultTest.getFailureCount() - resultTest.getIgnoreCount());
