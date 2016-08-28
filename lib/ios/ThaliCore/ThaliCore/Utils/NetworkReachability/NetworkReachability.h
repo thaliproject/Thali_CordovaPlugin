80f05ff752b80f9bf6f9860a45b68dae71bf2a84
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-08-28 18:12:14 +0300

    Replace Reachability.swift with NetworkReachability (empty template).

---
+++ lib/ios/ThaliCore/ThaliCore/Utils/NetworkReachability/NetworkReachability.h
@@ -0,0 +1,13 @@
+//
+//  NetworkReachability.h
+//  ThaliCore
+//
+//  Created by Dersim Davaod on 8/17/16.
+//  Copyright © 2016 Thali. All rights reserved.
+//
+
+#ifndef NetworkReachability_h
+#define NetworkReachability_h
+
+
+#endif /* NetworkReachability_h */
