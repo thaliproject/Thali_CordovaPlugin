6eef0fdbe4b77c93fc564ec939ebcd6ce90d9dfc
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-08-28 20:21:30 +0300

    Add BluetoothHarwareControlManager which runs private API (empty class).

---
+++ lib/ios/ThaliCore/ThaliCoreCITests/Utils/HardwareControl/Bluetooth/BluetoothHarwareControlManager.h
@@ -0,0 +1,13 @@
+//
+//  BluetoothHarwareControlManager.h
+//  ThaliCore
+//
+//  Created by Dersim Davaod on 8/17/16.
+//  Copyright © 2016 Thali. All rights reserved.
+//
+
+#ifndef BluetoothHarwareControlManager_h
+#define BluetoothHarwareControlManager_h
+
+
+#endif /* BluetoothHarwareControlManager_h */
---
+++ lib/ios/ThaliCore/ThaliCoreCITests/Utils/HardwareControl/Bluetooth/BluetoothHarwareControlManager.m
@@ -0,0 +1,9 @@
+//
+//  BluetoothHarwareControlManager.m
+//  ThaliCore
+//
+//  Created by Dersim Davaod on 8/17/16.
+//  Copyright © 2016 Thali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
