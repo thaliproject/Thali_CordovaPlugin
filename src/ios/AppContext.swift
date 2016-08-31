cd20975820a36467e7719fd51571a50c89df9058
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-08-31 20:40:15 +0300

    Fix typo in code.

--- src/ios/AppContext.swift
+++ src/ios/AppContext.swift
@@ -213,11 +213,11 @@ extension PeerAvailability {
         #if TEST
             // We use background queue because CI tests use main_queue synchronously
             // Otherwise we won't be able to get centralManager state.
-            let centralManagerDispathQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
+            let centralManagerDispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)
         #else
-            let centralManagerDispathQueue = nil
+            let centralManagerDispatchQueue = nil
         #endif
-        bluetoothManager = CBCentralManager(delegate: self, queue: centralManagerDispathQueue)
+        bluetoothManager = CBCentralManager(delegate: self, queue: centralManagerDispatchQueue)
 
     }
 
