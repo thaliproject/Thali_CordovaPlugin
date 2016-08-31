3d6df89f7c16feb03b1ce0ec4c633283618dd0bf
Marcin Leśniczek <mlesnic@ra.rockwell.com>
2016-08-31 10:46:36 +0200

    Code style fixes in UnitTest_app.js, added catch in radiosPromise

--- test/www/jxcore/UnitTest_app.js
+++ test/www/jxcore/UnitTest_app.js
@@ -12,60 +12,64 @@ if (typeof Mobile === 'undefined') {
 
 var testUtils = require('./lib/testUtils');
 var ThaliMobile = require('thali/NextGeneration/thaliMobile');
-var Promise = require('lie');
+var radiosPromise = require('lie');
 var utResult;
 
 ThaliMobile.getNetworkStatus()
-    .then(function (networkStatus) {
-        var promiseList = [];
-        if (networkStatus.wifi === 'off') {
-            promiseList.push(testUtils.toggleWifi(true));
+.then(function (networkStatus) {
+  var promiseList = [];
+
+  if (networkStatus.wifi === 'off') {
+    promiseList.push(testUtils.toggleWifi(true));
+  }
+  if (networkStatus.bluetooth === 'off') {
+    promiseList.push(testUtils.toggleBluetooth(true));
+  }
+
+  radiosPromise.all(promiseList)
+  .then(function () {
+    Mobile('executeNativeTests').callNative(function (result) {
+      utResult = true;
+      if (result && result.executed) {
+        console.log('Total number of executed tests: ', result.total);
+        console.log('Number of passed tests: ', result.passed);
+        console.log('Number of failed tests: ', result.failed);
+        console.log('Number of ignored tests: ', result.ignored);
+        console.log('Total duration: ', result.duration);
+
+        if (result.failed > 0) {
+          console.log('Failures: \n', result.failures);
+          utResult = false;
         }
-        if (networkStatus.bluetooth === 'off') {
-            promiseList.push(testUtils.toggleBluetooth(true));
-        }
-      
-        Promise.all(promiseList)
-            .then(function () {
-            Mobile('executeNativeTests').callNative(function (result) {
-                utResult = true;
-                if (result && result.executed) {
-                    console.log("Total number of executed tests: ", result.total);
-                    console.log("Number of passed tests: ", result.passed);
-                    console.log("Number of failed tests: ", result.failed);
-                    console.log("Number of ignored tests: ", result.ignored);
-                    console.log("Total duration: ", result.duration);
-
-                    if (result.failed > 0) {
-                        console.log('Failures: \n', result.failures);
-                        utResult = false;
-                    }
-                }
-            });
-
-            setImmediate(function() {
-                if (!utResult) {
-                    console.log("Failed to execute UT.");
-                    global.nativeUTFailed = true;
-                        
-                    console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_FAILED]****');
-                    return;
-                } else {        
-                    console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_SUCCESS]****');
-                }
-
-                Mobile('GetDeviceName').callNative(function (name) {
-                    console.log('My device name is: %s', name);
-                    testUtils.setName(name);
-                    // The setImmediate is to avoid this issue:
-                    // https://github.com/thaliproject/Thali_CordovaPlugin/issues/563
-                    setImmediate(function () {
-                        require('./runTests.js');
-                    });
-                });
-            });
-        });
+      }
     });
 
+    setImmediate(function () {
+      if (!utResult) {
+        console.log('Failed to execute UT.');
+        global.nativeUTFailed = true;
+
+        console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_FAILED]****');
+        return;
+      } else {
+        console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_SUCCESS]****');
+      }
+
+      global.Mobile('GetDeviceName').callNative(function (name) {
+        console.log('My device name is: %s', name);
+        testUtils.setName(name);
+        // The setImmediate is to avoid this issue:
+        // https://github.com/thaliproject/Thali_CordovaPlugin/issues/563
+        setImmediate(function () {
+          require('./runTests.js');
+        });
+      });
+    });
+  }, function (err) {
+    console.log('Error during turning on bluetooth and WiFi radios. ', err);
+    console.log('****TEST_LOGGER:[PROCESS_ON_EXIT_FAILED]****');
+  });
+});
 
 console.log('Unit Test app is loaded');
+
