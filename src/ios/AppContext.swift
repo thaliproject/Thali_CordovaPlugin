70393ea205e3e438ff4593ba89489080817eef6e
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-08-31 20:39:13 +0300

    Follow DRY pattern

--- src/ios/AppContext.swift
+++ src/ios/AppContext.swift
@@ -130,13 +130,14 @@ public typealias ClientConnectCallback = (String, String) -> Void
             }
         }
 
-        let wifiEnabled = NetworkReachability().isWiFiEnabled()
-        let wifiConnected = NetworkReachability().isWiFiConnected()
+        let networkReachability = NetworkReachability()
+        let wifiEnabled = networkReachability.isWiFiEnabled()
+        let wifiConnected = networkReachability.isWiFiConnected()
 
         wifiState = wifiEnabled ? .on : .off
 
         let bssid = ((wifiState == .on) && wifiConnected)
-            ? NetworkReachability().BSSID()
+            ? networkReachability.BSSID()
             : "null"
 
         let networkStatus = [
