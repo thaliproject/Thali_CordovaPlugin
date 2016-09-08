c030c3b2f496dc3644037be8384c1a4cc8214625
andrew-aladev <aladjev.andrew@gmail.com>
2016-09-08 18:48:20 +0000

    lie -> bluebird

--- test/TestServer/IPAddressToFile.js
+++ test/TestServer/IPAddressToFile.js
@@ -3,7 +3,7 @@
 var fs = require('fs-extra-promise');
 var os = require('os');
 var path = require('path');
-var Promise = require('lie');
+var Promise = require('bluebird');
 
 function writeFiles(address) {
   function writeServerAddress(filePath) {
