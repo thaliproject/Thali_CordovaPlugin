c64d91253aed5d8247a491dfb02643552d6183a5
Marek Czyz <mczyz@ra.rockwell.com>
2016-09-09 11:12:02 +0200

    Rerun without meta tests

--- build.sh
+++ build.sh
@@ -56,7 +56,7 @@ cordova -v;ERROR_ABORT
 thali/install/setUpDesktop.sh;ERROR_ABORT
 cd test/www/jxcore/;ERROR_ABORT
 jx npm test;ERROR_ABORT
-jx npm run test-meta;ERROR_ABORT
+# jx npm run test-meta;ERROR_ABORT
 jx npm run test-coordinated;ERROR_ABORT
 
 # Verify that docs can be generated
