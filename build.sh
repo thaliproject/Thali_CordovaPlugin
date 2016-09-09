9dfab58c17383348879e7c7ad6c3904b9281f4f8
czyzm <marekczyz@poczta.fm>
2016-09-09 09:54:22 +0200

    Without coordinated tests

--- build.sh
+++ build.sh
@@ -57,7 +57,7 @@ thali/install/setUpDesktop.sh;ERROR_ABORT
 cd test/www/jxcore/;ERROR_ABORT
 jx npm test;ERROR_ABORT
 jx npm run test-meta;ERROR_ABORT
-jx npm run test-coordinated;ERROR_ABORT
+# jx npm run test-coordinated;ERROR_ABORT
 
 # Verify that docs can be generated
 #cd $WORKING_DIR/thali/;ERROR_ABORT
