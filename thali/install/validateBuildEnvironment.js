97a6170f21c0524904d70685e7b7a23cf37649c4
Yaron Y Goland <yarong@microsoft.com>
2016-09-15 17:31:05 -0700

    Updated code & added check to setUpTests
    setUTests - added environment check
    
    validateBuildEnvironment - Made the code cleaner, separated out versions,
    used some ES6 voodoo to make the code tighter and added a check if we are
    run from the command line or not

--- thali/install/validateBuildEnvironment.js
+++ thali/install/validateBuildEnvironment.js
@@ -1,219 +1,224 @@
 'use strict';
 
+/* jshint esnext: true */
+
 var exec = require('child-process-promise').exec;
-var Promise = require('lie');
 var path = require('path');
 var fs = require('fs-extra-promise');
 
 var androidBrewBasePath = '/usr/local/Cellar/android-sdk';
 
+var versions =
+{
+  xcode: '7.3.1',
+  osX: '10.11.6',
+  node: '6.3.1',
+  npm: '3.10.3',
+  brew: '0.9.9',
+  ruby: '2.3.0p0',
+  wget: '1.18',
+  openssl: '0.9.8zh',
+  jxcore: '0.3.1.4',
+  androidSDKTools: '24.4.1_1',
+  androidBuildTools: '23.0.3',
+  androidPlatform: '23',
+  cordovaAndroidSetMinSDK: '22',
+  get cordovaAndroidSetBuildToolsVersion() {
+    return this.androidBuildTools;
+  },
+  get cordovaAndroidSetCompileSdkVersion() {
+    return this.androidPlatform;
+  },
+  get AndroidHome() {
+    return androidSdkVersionPath();
+  },
+  python: '2.7.10',
+  cordova: '6.3.1',
+  java: '1.8.0_102',
+  git: '2.7.4'
+};
+
+module.exports.versions = versions;
+
+function execAndCheck(command, checkStdErr, validator) {
+  return exec(command)
+    .then(function (result) {
+      var output = checkStdErr ? result.stderr : result.stdout;
+      return validator(output) ? true :
+        Promise.reject('Command: ' + command + ' failed');
+    });
+}
+
+function versionResultGenerator(commandAndResult, validator) {
+  if (typeof commandAndResult.versionCheck === 'function') {
+    return new Promise(function (resolve, reject) {
+      return validator(commandAndResult.versionCheck()) ?
+        resolve(true) : reject(false);
+    });
+  }
+  return execAndCheck(commandAndResult.versionCheck,
+                      commandAndResult.checkStdErr, validator);
+}
+
 var commandsAndResults =
   {
-    'xcode': {
-      'versionRequired': '7.3.1',
-      'versionCheck': 'xcodebuild -version',
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('Xcode '+ expectedVersion + '\n');
-      }
+    xcode: {
+      versionCheck: 'xcodebuild -version',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) => result.startsWith('Xcode '+ versions[objectName] + '\n'))
     },
-    'osX': {
-      'versionRequired': '10.11.6',
-      'versionCheck': 'sw_vers -productVersion',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    osX: {
+      versionCheck: 'sw_vers -productVersion',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) => versions[objectName] === result.trim())
     },
-    'node': {
-      'versionRequired': '6.3.1',
-      'versionCheck': 'node -v',
-      'versionResult': function (expectedVersion, result) {
-        return 'v' + expectedVersion === result.trim();
-      }
+    node: {
+      versionCheck: 'node -v',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  'v' + versions[objectName] === result.trim())
     },
-    'npm': {
-      'versionRequired': '3.10.3',
-      'versionCheck': 'npm -v',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    npm: {
+      versionCheck: 'npm -v',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  versions[objectName] === result.trim())
     },
-    'brew': {
-      'versionRequired': '0.9.9',
-      'versionCheck': 'brew -v',
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('Homebrew ' + expectedVersion + ' ');
-      }
+    brew: {
+      versionCheck: 'brew -v',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.startsWith('Homebrew ' + versions[objectName] +
+                      ' '))
     },
-    'ruby': {
-      'versionRequired': '2.3.0p0',
-      'versionCheck': 'ruby -v',
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('ruby ' + expectedVersion + ' ');
-      }
+    ruby: {
+      versionCheck: 'ruby -v',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.startsWith('ruby ' + versions[objectName] + ' '))
     },
-    'wget': {
-      'versionRequired': '1.18',
-      'versionCheck': 'wget -V',
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('GNU Wget ' + expectedVersion + ' ');
-      }
+    wget: {
+      versionCheck: 'wget -V',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.startsWith('GNU Wget ' + versions[objectName] +
+          ' '))
     },
-    'openssl': {
-      'versionRequired': '0.9.8zh',
-      'versionCheck': 'openssl version',
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('OpenSSL ' + expectedVersion + ' ');
-      }
+    openssl: {
+      versionCheck: 'openssl version',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.startsWith('OpenSSL ' + versions[objectName] + ' '))
     },
-    'jxcore': {
-      'versionRequired': '0.3.1.4',
-      'versionCheck': 'jx -jxv',
-      'versionResult': function (expectedVersion, result) {
-        return 'v' + expectedVersion === result.trim();
-      }
+    jxcore: {
+      versionCheck: 'jx -jxv',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  'v' + versions[objectName] === result.trim())
     },
-    'androidSDKTools' : {
-      'versionRequired': '24.4.1_1',
-      'versionCheck': function () {
-        return fs.readdirAsync(androidBrewBasePath);
-      },
-      'versionResult': function (expectedVersion, result) {
-        return result.indexOf(expectedVersion) !== -1;
-      }
+    androidSDKTools: {
+      versionCheck: () => fs.readdirSync(androidBrewBasePath),
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.indexOf(versions[objectName]) !== -1)
     },
-    'androidBuildTools': {
-      'versionRequired': '23.0.3',
-      'versionCheck': function () {
-        return fs.readdirAsync(path.join(androidSdkVersionPath(),
-                                'build-tools'));
-      },
-      'versionResult': function (expectedVersion, result) {
-        return result.indexOf(expectedVersion) !== -1;
-      }
+    androidBuildTools: {
+      versionCheck: () => fs.readdirSync(path.join(androidSdkVersionPath(),
+                                'build-tools')),
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.indexOf(versions[objectName]) !== -1)
     },
-    'androidPlatform': {
-      'versionRequired': '23',
-      'versionCheck': function () {
-        return fs.readdirAsync(path.join(androidSdkVersionPath(), 'platforms'));
-      },
-      'versionResult': function (expectedVersion, result) {
-        return result.indexOf('android-'+expectedVersion) !== -1;
-      }
+    androidPlatform: {
+      versionCheck: () => fs.readdirSync(path.join(androidSdkVersionPath(),
+                                          'platforms')),
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.indexOf('android-'+versions[objectName]) !== -1)
     },
-    'cordovaAndroidSetMinSDK': {
-      'versionRequired': '22',
-      'versionCheck': 'echo $ORG_GRADLE_PROJECT_cdvMinSdkVersion',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    cordovaAndroidSetMinSDK: {
+      versionCheck: 'echo $ORG_GRADLE_PROJECT_cdvMinSdkVersion',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  versions[objectName] === result.trim())
     },
-    'cordovaAndroidSetBuildToolsVersion': {
-      'versionRequired': function () {
-        return commandsAndResults.androidBuildTools.versionRequired;
-      },
-      'versionCheck': 'echo $ORG_GRADLE_PROJECT_cdvBuildToolsVersion',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    cordovaAndroidSetBuildToolsVersion: {
+      versionCheck: 'echo $ORG_GRADLE_PROJECT_cdvBuildToolsVersion',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  versions[objectName] === result.trim())
     },
-    'cordovaAndroidSetCompileSdkVersion': {
-      'versionRequired': function () {
-        return commandsAndResults.androidPlatform.versionRequired;
-      },
-      'versionCheck': 'echo $ORG_GRADLE_PROJECT_cdvCompileSdkVersion',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    cordovaAndroidSetCompileSdkVersion: {
+      versionCheck: 'echo $ORG_GRADLE_PROJECT_cdvCompileSdkVersion',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  versions[objectName] === result.trim())
     },
-    'AndroidHome': {
-      'versionRequired': function () {
-        return androidSdkVersionPath();
-      },
-      'versionCheck': 'echo $ANDROID_HOME',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    AndroidHome: {
+      versionCheck: 'echo $ANDROID_HOME',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  versions[objectName] === result.trim())
     },
-    'python': {
-      'versionRequired': '2.7.10',
-      'versionCheck': 'python -V',
-      'checkStdErr': true, // http://bugs.python.org/issue28160 - fixed in 3.4
-      'versionResult': function (expectedVersion, result) {
-        return 'Python ' + expectedVersion === result.trim();
-      }
+    python: {
+      versionCheck: 'python -V',
+      checkStdErr: true, // http://bugs.python.org/issue28160 - fixed in 3.4
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  'Python ' + versions[objectName] === result.trim())
     },
-    'cordova': {
-      'versionRequired': '6.3.1',
-      'versionCheck': 'cordova -v',
-      'versionResult': function (expectedVersion, result) {
-        return expectedVersion === result.trim();
-      }
+    cordova: {
+      versionCheck: 'cordova -v',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  versions[objectName] === result.trim())
     },
-    'java': {
-      'versionRequired': '1.8.0_102',
-      'versionCheck': 'java -version',
-      'checkStdErr': true, // http://bugs.java.com/bugdatabase/view_bug.do?bug_id=JDK-8166116
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('java version "' + expectedVersion + '"\n');
-      }
+    java: {
+      versionCheck: 'java -version',
+      checkStdErr: true, // http://bugs.java.com/bugdatabase/view_bug.do?bug_id=JDK-8166116
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.startsWith('java version "' + versions[objectName] +
+          '"\n'))
     },
-    'git': {
-      'versionRequired': '2.7.4',
-      'versionCheck': 'git --version',
-      'versionResult': function (expectedVersion, result) {
-        return result.startsWith('git version ' + expectedVersion + ' ');
-      }
+    git: {
+      versionCheck: 'git --version',
+      versionResult: (objectName) => versionResultGenerator(
+        commandsAndResults[objectName],
+        (result) =>  result.startsWith('git version ' + versions[objectName] +
+          ' '))
     }
   };
 
 function androidSdkVersionPath() {
   return path.join(androidBrewBasePath,
-                    commandsAndResults.androidSDKTools.versionRequired);
-}
-
-module.exports.commandsAndResults = commandsAndResults;
-
-function execAndCheck(command, checkStdErr, expectedVersion, validator) {
-  return exec(command)
-    .then(function (result) {
-      var output = checkStdErr ? result.stderr : result.stdout;
-      return validator(expectedVersion, output) ? true :
-        Promise.reject('Command: ' + command + ' failed');
-    });
+                    versions.androidSDKTools);
 }
 
 function processCommandsAndResults(commandsAndResults) {
   var promises = [];
   Object.getOwnPropertyNames(commandsAndResults).forEach(function (name) {
-    var commandAndResult = commandsAndResults[name];
-    var versionRequired = typeof commandAndResult.versionRequired === 'function'
-      ? commandAndResult.versionRequired() : commandAndResult.versionRequired;
-    if (typeof commandAndResult.versionCheck === 'function') {
-      promises.push(
-        commandAndResult.versionCheck()
-          .then(function (directories) {
-            return commandAndResult.versionResult(versionRequired, directories);
-          }));
-      return;
-    }
-    promises.push(execAndCheck(commandAndResult.versionCheck,
-                               commandAndResult.checkStdErr, versionRequired,
-                                commandAndResult.versionResult));
+    promises.push(commandsAndResults[name].versionResult(name));
   });
   return Promise.all(promises);
 }
 
-processCommandsAndResults(commandsAndResults)
-  .then(function () {
-    // Good to clean this up in case we have changed the version of jxcore
-    var home = process.env.HOME;
-    var jx = path.join(home, '.jx');
-    var nodeGyp = path.join(home, 'node-gyp');
-    return Promise.all([fs.removeAsync(jx), fs.removeAsync(nodeGyp)]);
-  }).then(function () {
+// Detects if we were called from the command line
+if (require.main === module) {
+  processCommandsAndResults(commandsAndResults)
+    .then(function () {
+      // Good to clean this up in case we have changed the version of jxcore
+      var home = process.env.HOME;
+      var jx = path.join(home, '.jx');
+      var nodeGyp = path.join(home, 'node-gyp');
+      return Promise.all([fs.removeAsync(jx), fs.removeAsync(nodeGyp)]);
+    }).then(function () {
     console.log('Environment validated');
-    process.exit(1);
+    process.exit(0);
   }).catch(function (err) {
     console.log('Environment not valid: ' + err);
     process.exit(-1);
   });
+}
