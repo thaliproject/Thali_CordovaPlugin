84c209e8a2237facd63b6d55a5ec7f5f17c59b1a
Andrew Aladjev <aladjev.andrew@gmail.com>
2016-09-22 01:03:51 +0300

    added endsWith polyfill

---
+++ thali/install/utils/polyfills.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+if (!String.prototype.endsWith) {
+  // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+  Object.defineProperty(String.prototype, 'endsWith', {
+    value: function(searchString, position) {
+      var subjectString = this.toString();
+      if (position === undefined || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+    }
+  });
+}
