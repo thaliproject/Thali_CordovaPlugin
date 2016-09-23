e15a0bc9d5731e02d8e0bf68255e1d4b4cc22b15
Eugene Chapko <eugene.chapko@gmail.com>
2016-09-23 16:43:01 +0300

    style changes

--- thali/NextGeneration/utils/usn.js
+++ thali/NextGeneration/utils/usn.js
@@ -17,44 +17,43 @@ var format = require('util').format;
 
 var USN = {
   _prefix: 'data:',
+};
 
-  /**
-   * @param {string} usn
-   * @returns {{peerIdentifier: string, generation: number}}
-   * @throws Will throw when provided usn has an invalid prefix (other than
-   * 'data:'), has incorrect number of segments or its generation is not a number.
-   */
-  parse: function (usn) {
-    if (usn.indexOf(USN._prefix) !== 0) {
-      throw new Error(
-        format('Invalid USN (expected "%s" prefix): %s', USN._prefix, usn)
-      );
-    }
-    var unprefixed = usn.substring(USN._prefix.length);
-    var segments = unprefixed.split(':');
-    var peerIdentifier = segments[0];
-    var generation = Number(segments[1]);
-
-    if (segments.length !== 2) {
-      throw new Error('Invalid USN (expected 2 segments): ' + usn);
-    }
-    if (isNaN(generation)) {
-      throw new Error('Invalid USN (generation is not a number): ' + usn);
-    }
-    return {
-      peerIdentifier: peerIdentifier,
-      generation: generation
-    };
-  },
-
-  /**
-   * @param {UsnPeer} peer
-   * @returns {string}
-   */
-  stringify: function (peer) {
-    return USN._prefix + peer.peerIdentifier + ':' + peer.generation;
+/**
+ * @param {string} usn
+ * @returns {UsnPeer}
+ * @throws Will throw when provided usn has an invalid prefix (other than
+ * 'data:'), has incorrect number of segments or its generation is not a number.
+ */
+USN.parse = function (usn) {
+  if (usn.indexOf(USN._prefix) !== 0) {
+    throw new Error(
+      format('Invalid USN (expected "%s" prefix): %s', USN._prefix, usn)
+    );
   }
+  var unprefixed = usn.substring(USN._prefix.length);
+  var segments = unprefixed.split(':');
+  var peerIdentifier = segments[0];
+  var generation = Number(segments[1]);
+
+  if (segments.length !== 2) {
+    throw new Error('Invalid USN (expected 2 segments): ' + usn);
+  }
+  if (isNaN(generation)) {
+    throw new Error('Invalid USN (generation is not a number): ' + usn);
+  }
+  return {
+    peerIdentifier: peerIdentifier,
+    generation: generation
+  };
+};
+
+/**
+ * @param {UsnPeer} peer
+ * @returns {string}
+ */
+USN.stringify = function (peer) {
+  return USN._prefix + peer.peerIdentifier + ':' + peer.generation;
 };
 
 module.exports = USN;
-
