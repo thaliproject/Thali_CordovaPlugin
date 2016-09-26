d79207a2d45034aad80d4cb0603ceb5bb3c09d20
Yaron Y Goland <yarong@microsoft.com>
2016-09-25 21:37:14 -0700

    Validator and cleaned up sh scripts
    build.sh - Removed the non-functional ERROR_ABORT (our use of -e meant
    that the ERROR_ABORT function would never actually be called) and replaced
    with a call to trap that will actually run. Also fixed a call to check if
    CIGIVEMEMYIP.sh exists (to tell if we are in CI) which depended on being
    able to fail that our new stricter environment setting don't like.
    
    setUpDesktop.sh - Same as above
    
    setUpTests.sh - Same as above but also had to set some default variables
    because our new environment variables don't like unbound variables even
    if they are positional.
    
    validateBuildEnvironment.js - Updated git because old version doesn't work
    well with worktrees. Put in a check to see if the configured NPM registry
    actually will return a meaningful value. I had to do this because I can't
    find a way to force a Sinopia server to identify itself as a Sinopia server.

--- thali/install/setUpDesktop.sh
+++ thali/install/setUpDesktop.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
+set -euo pipefail
 
-ERROR_ABORT() {
-  if [[ $? != 0 ]]; then
-    LOG $RED_COLOR "SetUp Desktop Failed\n"
-    exit -1
-  fi
+NORMAL_COLOR='\033[0m'
+RED_COLOR='\033[0;31m'
+
+OUTPUT() {
+  echo -e "${RED_COLOR}$BASH_COMMAND FAILED - setUpDesktop failure${NORMAL_COLOR}"
 }
+
+trap OUTPUT ERR
 
 NVM_NODEJS_ORG_MIRROR=https://jxcore.azureedge.net
 export NVM_NODEJS_ORG_MIRROR
 JX_NPM_JXB=jxb311
 export JX_NPM_JXB
 
-cd `dirname $0`;ERROR_ABORT
-node validateBuildEnvironment.js;ERROR_ABORT
-cd ../../test/TestServer;ERROR_ABORT
-jx npm install;ERROR_ABORT
-jx generateServerAddress.js;ERROR_ABORT
-cd ../../thali;ERROR_ABORT
-jx npm install --no-optional;ERROR_ABORT
-jx npm link;ERROR_ABORT
-cd install;ERROR_ABORT
-jx npm install;ERROR_ABORT
-cd ../../test/www/jxcore;ERROR_ABORT
-jx npm link thali;ERROR_ABORT
-jx npm install --no-optional;ERROR_ABORT
+cd `dirname $0`
+node validateBuildEnvironment.js
+cd ../../test/TestServer
+jx npm install
+jx generateServerAddress.js
+cd ../../thali
+jx npm install --no-optional
+jx npm link
+cd install
+jx npm install
+cd ../../test/www/jxcore
+jx npm link thali
+jx npm install --no-optional
