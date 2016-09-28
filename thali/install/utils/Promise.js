539c1bbc62b9b01d07f75b0693e3c9b6fbf3c60d
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-09-28 12:52:14 +0300

    Disabled bluebird debuggability for install process

--- thali/install/utils/Promise.js
+++ thali/install/utils/Promise.js
@@ -2,12 +2,4 @@
 
 var Promise = require('bluebird');
 
-// Temporary disabled TODO: Issue #1158
-// Promise.config({
-//   warnings:        true,
-//   longStackTraces: true,
-//   cancellation:    true,
-//   monitoring:      true
-// });
-
 module.exports = Promise;
