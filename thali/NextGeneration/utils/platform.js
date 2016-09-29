ee8164d02483eaa1405af10d52b7fa9c32e68ad8
Artem Stepanishchev <artem.stepanishchev@hiqo-solutions.com>
2016-09-29 11:39:18 +0300

    Added ability to retrieve real platform even if current platform is overridden

--- thali/NextGeneration/utils/platform.js
+++ thali/NextGeneration/utils/platform.js
@@ -35,7 +35,7 @@ module.exports = Object.defineProperties({}, {
         return _platform === _platforms.IOS;
       }
     },
-    // These methods are presented ONLY for testing reasons
+    // The methods presented below ONLY for testing reasons
     '_override': {
       value: function (platform) {
         return _platform = platform;
@@ -45,5 +45,27 @@ module.exports = Object.defineProperties({}, {
       value: function () {
         return _platform = process.platform;
       }
-    }
+    },
+    // Returns REAL values based on `process.platform`
+    '_realName': {
+      value: function () {
+        return process.platform;
+      }
+    },
+    '_isRealMobile': {
+      get: function () {
+        return this._isRealAndroid || this._isRealIOS;
+      }
+
+    },
+    '_isRealAndroid': {
+      get: function () {
+        return process.platform === _platforms.ANDROID;
+      }
+    },
+    '_isRealIOS': {
+      get: function () {
+        return process.platform === _platforms.IOS;
+      }
+    },
   });
