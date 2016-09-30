b7357915dc0cfbd7b444adea4b2b3400fedff01a
andrew-aladev <aladjev.andrew@gmail.com>
2016-09-30 18:16:35 +0300

    fixed replication peer action test

--- test/www/jxcore/bv_tests/testThaliReplicationPeerAction.js
+++ test/www/jxcore/bv_tests/testThaliReplicationPeerAction.js
@@ -224,8 +224,7 @@ test('Make sure docs replicate',
                                                          randomDBName,
                                                          remotePouchDB) {
       var thaliReplicationPeerAction = null;
-      var DifferentDirectoryPouch =
-        testUtils.getPouchDBFactoryInRandomDirectory();
+      var DifferentDirectoryPouch = testUtils.getLevelDownPouchDb();
       var localPouchDB = new DifferentDirectoryPouch(randomDBName);
       createDocs(remotePouchDB, 10)
      .then(function (docs) {
@@ -284,8 +283,7 @@ test('Do nothing and make sure we time out', function (t) {
     };
     // Using a different directory really shouldn't make any difference
     // to this particular test but I'm being paranoid
-    var DifferentDirectoryPouch =
-      testUtils.getPouchDBFactoryInRandomDirectory();
+    var DifferentDirectoryPouch = testUtils.getLevelDownPouchDb();
     var originalTimeout = ThaliReplicationPeerAction.maxIdlePeriodSeconds;
     ThaliReplicationPeerAction.maxIdlePeriodSeconds = 2;
     thaliReplicationPeerAction =
@@ -322,8 +320,7 @@ test('Do something and make sure we time out', function (t) {
   testCloseAllServer = testUtils.setUpServer(function (serverPort, randomDBName,
                                                        remotePouchDB) {
     var thaliReplicationPeerAction = null;
-    var DifferentDirectoryPouch =
-      testUtils.getPouchDBFactoryInRandomDirectory();
+    var DifferentDirectoryPouch = testUtils.getLevelDownPouchDb();
     var localPouchDB = new DifferentDirectoryPouch(randomDBName);
     var thaliReplicationPeerActionStartOutput = null;
     var originalTimeout = ThaliReplicationPeerAction.maxIdlePeriodSeconds;
@@ -398,8 +395,7 @@ test('Start replicating and then catch error when server goes', function (t) {
   testCloseAllServer = testUtils.setUpServer(function (serverPort, randomDBName,
                                                        remotePouchDB) {
     var thaliReplicationPeerAction = null;
-    var DifferentDirectoryPouch =
-      testUtils.getPouchDBFactoryInRandomDirectory();
+    var DifferentDirectoryPouch = testUtils.getLevelDownPouchDb();
     createDocs(remotePouchDB, 10)
       .then(function () {
         var notificationForUs = {
