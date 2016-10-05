9dc8de601de83c9f653e73ff3802c8f5257808f8
Ilya Laryionau <larryonoff@gmail.com>
2016-10-05 12:29:14 +0300

    update build script according to the changes in iOS branch

--- thali/install/setUpTests.sh
+++ thali/install/setUpTests.sh
@@ -3,13 +3,28 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Check if we are running in MinGW
-runningInMinGw=false
+ERROR_ABORT()
+{
+  if [[ $? != 0 ]]; then
+    exit -1
+  fi
+}
 
-if [ "$(uname -s | cut -c 1-5)" == "MINGW" ]; then
-    echo "Running in MinGW"
-    runningInMinGw=true
+# Check the platform we are running
+IS_MINIGW_PLATFORM=false
+IS_DARWIN_PLATFORM=false
+
+if test x"$(uname -s | cut -c 1-5)" == xMINGW ; then
+  echo "Running in MinGW Platform"
+
+  IS_MINIGW_PLATFORM=true
+elif test x"`uname`" = xDarwin ; then
+  echo "Running in Darwin Platform"
+
+  IS_DARWIN_PLATFORM=true
 fi
+
+TEST_PROJECT_NAME=ThaliTest
 
 # The first argument must be the name of the test file to make into the app.js
 # The second argument is optional and specifies a string with an IP address to
@@ -19,67 +34,122 @@ fi
 
 cd `dirname $0`
 cd ../..
-repositoryRoot=$(pwd)
-cd test/TestServer
-jx npm install --no-optional
-jx generateServerAddress.js $2
-cd $repositoryRoot/..
-cordova create ThaliTest com.test.thalitest ThaliTest
-cordovaRoot=$(pwd)/ThaliTest
-mkdir -p ThaliTest/thaliDontCheckIn/localdev
+REPO_ROOT_DIR=$(pwd)
+TEST_PROJECT_ROOT_DIR=${REPO_ROOT_DIR}/../${TEST_PROJECT_NAME}
 
-if [ $runningInMinGw == true ]; then
-    # The thali package might be installed as link and there will
-    # be troubles later on if this link is tried to be copied so
-    # remove it here.
-    rm -rf $repositoryRoot/test/www/jxcore/node_modules/thali
-    cp -R $repositoryRoot/test/www/ ThaliTest/
-else
-    rsync -a --no-links $repositoryRoot/test/www/ ThaliTest/www
-fi
+# Prepares test project
+prepare_project()
+{
+  echo "Preparing ${TEST_PROJECT_NAME} Cordova project"
 
-cd ThaliTest
-# TODO Temporarily disabling ios build
-#cordova platform add ios
-cordova platform add android
-cd www/jxcore
-jx installCustomPouchDB.js
-jx npm install $repositoryRoot/thali --save --no-optional --autoremove "*.gz"
+  cd $REPO_ROOT_DIR/test/TestServer
+  jx npm install --no-optional
+  jx generateServerAddress.js $2
+  cd $REPO_ROOT_DIR/..
+  cordova create $TEST_PROJECT_NAME com.test.thalitest $TEST_PROJECT_NAME;ERROR_ABORT
+  mkdir -p $TEST_PROJECT_NAME/thaliDontCheckIn/localdev;ERROR_ABORT
 
-if [ $runningInMinGw == true ]; then
-    # On Windows the package.json file will contain an invalid local file URI for Thali,
-    # which needs to be replaced with a valid value. Otherwise the build process
-    # will be aborted. Restore write permission after running sed in case
-    # Windows security settings removed it.
-    sed -i 's/"thali": ".*"/"thali": "*"/g' package.json
-    chmod 644 package.json
-fi
+  if [ $IS_MINIGW_PLATFORM == true ]; then
+      # The thali package might be installed as link and there will
+      # be troubles later on if this link is tried to be copied so
+      # remove it here.
+      rm -rf $REPO_ROOT_DIR/test/www/jxcore/node_modules/thali;ERROR_ABORT
+      cp -R $REPO_ROOT_DIR/test/www/ $TEST_PROJECT_NAME/;ERROR_ABORT
+  else
+      rsync -a --no-links $REPO_ROOT_DIR/test/www/ $TEST_PROJECT_NAME/www;ERROR_ABORT
+  fi
+}
 
-# SuperTest which is used by some of the BVTs include a PEM file (for private
-# keys) that makes Android unhappy so we remove it below in addition to the gz
-# files.
-jx npm install --no-optional --autoremove "*.gz,*.pem"
+install_thali()
+{
+  echo "Installing Thali into ${TEST_PROJECT_NAME}"
 
-# In case autoremove fails to delete the files, delete them explicitly.
-find . -name "*.gz" -delete
-find . -name "*.pem" -delete
+  cd $TEST_PROJECT_ROOT_DIR/www/jxcore;ERROR_ABORT
+  jx installCustomPouchDB.js
+  jx npm install $REPO_ROOT_DIR/thali --save --no-optional --autoremove "*.gz";ERROR_ABORT
 
-cp -v $1 app.js
+  if [ $IS_MINIGW_PLATFORM == true ]; then
+      # On Windows the package.json file will contain an invalid local file URI for Thali,
+      # which needs to be replaced with a valid value. Otherwise the build process
+      # will be aborted. Restore write permission after running sed in case
+      # Windows security settings removed it.
+      sed -i 's/"thali": ".*"/"thali": "*"/g' package.json
+      chmod 644 package.json
+  fi
 
-cd $repositoryRoot/thali/install
-jx npm install
-cd $cordovaRoot/thaliDontCheckIn
-jx $repositoryRoot/thali/install/setUpThaliTestIds.js
+  # SuperTest which is used by some of the BVTs include a PEM file (for private
+  # keys) that makes Android unhappy so we remove it below in addition to the gz
+  # files.
+  jx npm install --no-optional --autoremove "*.gz,*.pem";ERROR_ABORT
 
-# A file that identifies the current build as a UT build, which results in copying Android native UT files to the platform folder
-cd $cordovaRoot
-touch platforms/android/unittests
+  # In case autoremove fails to delete the files, delete them explicitly.
+  find . -name "*.gz" -delete
+  find . -name "*.pem" -delete
 
-cordova build android --release --device
+  cp -v $1 app.js;ERROR_ABORT
 
-# TODO Temporarily disabling ios build
-#if [ $runningInMinGw == false ]; then
-#    cordova build ios --device
-#fi
+  cd $REPO_ROOT_DIR/thali/install
+  jx npm install --no-optional
+  cd $TEST_PROJECT_ROOT_DIR/thaliDontCheckIn
+  jx $REPO_ROOT_DIR/thali/install/setUpThaliTestIds.js
+}
+
+add_android_platform()
+{
+  echo "Adding Android platform into ${TEST_PROJECT_NAME}"
+
+  cd $TEST_PROJECT_ROOT_DIR
+
+  cordova platform add android
+
+  # A file that identifies the current build as a UT build
+  touch platforms/android/unittests
+}
+
+build_android()
+{
+  echo "Building Android app"
+
+  cd $TEST_PROJECT_ROOT_DIR
+
+  cordova build android --release --device;ERROR_ABORT
+}
+
+# Adds iOS platform when we're running on macOS
+add_ios_platform_if_possible()
+{
+  if [ $IS_DARWIN_PLATFORM == true ]; then
+    echo "Adding iOS platform into ${TEST_PROJECT_NAME}"
+
+    cd $TEST_PROJECT_ROOT_DIR
+
+    cordova platform add ios;ERROR_ABORT
+
+    # A file that identifies the current build as a UT build
+    touch platforms/ios/unittests;ERROR_ABORT
+  fi
+}
+
+# Builds iOS platform when we're running on macOS
+build_ios_if_possible()
+{
+  if [ $IS_DARWIN_PLATFORM == true ]; then
+    echo "Building iOS app"
+
+    cd $TEST_PROJECT_ROOT_DIR
+
+    cordova build ios --device;ERROR_ABORT
+
+  fi
+}
+
+# Please note that functions order is important
+prepare_project $1 $2
+add_android_platform
+# add_ios_platform_if_possible
+install_thali $1 $2
+build_android
+# build_ios_if_possible
+# TODO: iOS builds are disabled temporarily
 
 echo "Remember to start the test coordination server by running jx index.js"
