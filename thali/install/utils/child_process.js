62eb3cc3f8729d4e0d9150a7551aa6adafc48a1f
Ilya Laryionau <larryonoff@gmail.com>
2016-10-19 19:32:58 +0300

    Use updated version exec with live streaming of stdout, stderr #1379

--- thali/install/utils/child_process.js
+++ thali/install/utils/child_process.js
@@ -10,36 +10,15 @@ var Promise = require('./Promise');
 
 function exec(command, options) {
   return new Promise(function (resolve, reject) {
-    child_process.exec(command, options,
-      function (error, stdout, stderr) {
-        if (error) {
-          reject(error);
-          return;
-        }
+    var childProcess = child_process.exec(command, options);
 
-        // Log output even if command doesn't exit with an error,
-        // because otherwise useful debugging information might get lost.
-        if (stdout) { console.log(stdout); }
-        if (stderr) { console.log(stderr); }
-
-        resolve();
-      });
-  });
-}
-
-function spawn(command, options) {
-  return new Promise(function (resolve, reject) {
-    var commandSplit = command.split(' ');
-    var commandOnly = commandSplit.shift();
-    var commandSpawn = child_process.spawn(commandOnly, commandSplit, options);
-
-    commandSpawn.stdout.on('data', function (data) {
+    childProcess.stdout.on('data', function (data) {
       console.log('' + data);
     });
-    commandSpawn.stderr.on('data', function (data) {
+    childProcess.stderr.on('data', function (data) {
       console.log('' + data);
     });
-    commandSpawn.on('close', function (code) {
+    childProcess.on('close', function (code) {
       if (code !== 0) {
         return reject('`' + command + '` (exited with error code' + code + ')');
       }
@@ -49,4 +28,3 @@ function spawn(command, options) {
 }
 
 module.exports.exec = exec;
-module.exports.spawn = spawn;
