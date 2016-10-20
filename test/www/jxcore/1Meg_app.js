3c2821c87d592ce38481540730c64b85fbf3cd1b
andrew-aladev <aladjev.andrew@gmail.com>
2016-10-20 08:48:06 +0000

    implemented 1 meg app

--- test/www/jxcore/1Meg_app.js
+++ test/www/jxcore/1Meg_app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var LEADER_TIMEOUT = 10 * 1000;
-var SYNC_TIMEOUT   = 10 * 1000;
-var TEST_TIMEOUT   = 5 * 60 * 1000;
-var DATA_LENGTH    = 1e6;
+// sync timeout should be >= leader timeout to properly synchronize peers after leader lookup.
+var SYNC_TIMEOUT = LEADER_TIMEOUT;
+var TEST_TIMEOUT = 5 * 60 * 1000;
+
+var DATA_LENGTH = 1e6;
 
 if (typeof Mobile === 'undefined') {
   global.Mobile = require('./lib/wifiBasedNativeMock.js')();
@@ -13,7 +15,7 @@ var net          = require('net');
 var assert       = require('assert');
 var Promise      = require('bluebird');
 var uuid         = require('node-uuid');
-var randomstring = require("randomstring");
+var randomstring = require('randomstring');
 
 var makeIntoCloseAllServer = require('thali/NextGeneration/makeIntoCloseAllServer');
 
@@ -27,7 +29,6 @@ var testUtils = require('./lib/testUtils');
 
 // We couldn't get our local peerIdentifier.
 // So we will send peerIdentifier to the peer itself.
-var myPeerId;
 var peerIds = {};
 
 function resolveLeader () {
@@ -115,6 +116,8 @@ function resolveLeader () {
     });
   }
 
+  var myPeerId;
+
   function receivePeerId (socket) {
     socket.once('data', function (data) {
       var id = data.toString();
@@ -135,12 +138,193 @@ function resolveLeader () {
   .then(stop)
   .then(function () {
     assert(myPeerId, 'my peer id should exist');
+    var peers = Object.keys(peerIds);
+    assert(peers.length > 0, 'peer ids should exist');
 
-    var isLeader = Object.keys(peerIds)
-    .every(function (peerId) {
+    var isLeader = peers.every(function (peerId) {
       return myPeerId < peerId;
     });
     return isLeader;
+  });
+}
+
+function sendRandomData() {
+  return new Promise(function (resolve) {
+
+    function start () {
+      return new Promise(function (resolve) {
+        Mobile('startListeningForAdvertisements')
+        .callNative(function (error) {
+          assert(!error, 'Called startListeningForAdvertisements without error');
+          Mobile('peerAvailabilityChanged').registerToNative(peersHandler);
+          resolve();
+        });
+      });
+    }
+
+    function stop() {
+      return new Promise(function (resolve) {
+        Mobile('stopListeningForAdvertisements')
+        .callNative(function (error) {
+          assert(!error, 'Should be able to call stopListeningForAdvertisements');
+          resolve();
+        });
+      });
+    }
+
+    function peersHandler (peers) {
+      peers.forEach(function (peer) {
+        if (peer.peerAvailable) {
+          sendData(peer.peerIdentifier);
+        }
+      });
+    }
+
+    var randomData     = randomstring.generate(DATA_LENGTH);
+    var peersConfirmed = {};
+    var peersTimer     = {};
+
+    function finishPeer(id) {
+      peersConfirmed[id] = true;
+      var timer = process.hrtime(peersTimer[id]);
+      logger.info('peer finished in %d seconds', timer[0] + timer[1] / 1e9);
+      resolveConfirmedPeers();
+    }
+
+    function resolveConfirmedPeers () {
+      if (Object.keys(peerIds).length === Object.keys(peersConfirmed).length) {
+        stop()
+        .then(function () {
+          logger.debug('we are stopped');
+          resolve();
+        });
+      }
+    }
+
+    function sendData (id) {
+      if (peersConfirmed[id]) {
+        logger.debug('we already finished with peer: \'%s\'', id);
+        return;
+      }
+      if (!peersTimer[id]) {
+        peersTimer[id] = process.hrtime();
+      }
+
+      Mobile('connect')
+      .callNative(id, function (error, data) {
+        if (error) {
+          logger.error('Got error \'%s\'', error.toString());
+          return;
+        }
+        data = JSON.parse(data);
+
+        var socket = new net.Socket()
+        .connect(data.listeningPort, function() {
+          socket.write(randomData);
+
+          socket.on('data', function (data) {
+            if (data.toString() === '0') {
+              logger.debug('we finished with peer: \'%s\'', id);
+              finishPeer(id);
+            } else {
+              logger.error('we received invalid confirmation from peer: \'%s\'', id);
+            }
+            socket.end();
+          })
+
+          .on('error', function (error) {
+            logger.error('Got error: \'%s\', stack: \'%s\'', error.toString(), error.stack);
+
+            // We want to reconnect.
+            logger.debug('we are going to reconnect');
+            socket.destroy();
+            setImmediate(function () {
+              sendData(id);
+            });
+          });
+        });
+      });
+    }
+
+    start();
+  });
+}
+
+function receiveRandomData() {
+  return new Promise(function (resolve) {
+    var server;
+
+    function start () {
+      return new Promise(function (resolve) {
+        server = makeIntoCloseAllServer(
+          net.createServer(receiveData)
+        );
+        server.listen(0, function () {
+          resolve();
+        });
+      })
+      .then(function () {
+        return new Promise(function (resolve) {
+          Mobile('startUpdateAdvertisingAndListening')
+          .callNative(server.address().port, function (error) {
+            assert(!error, 'Called startUpdateAdvertisingAndListening without error');
+            resolve();
+          });
+        });
+      });
+    }
+
+    function stop() {
+      return new Promise(function (resolve) {
+        Mobile('stopAdvertisingAndListening')
+        .callNative(function (error) {
+          assert(!error, 'Should be able to call stopAdvertisingAndListening');
+          resolve();
+        });
+      })
+      .then(function () {
+        return new Promise(function (resolve) {
+          server.closeAll(resolve);
+        });
+      });
+    }
+
+    var totalLength = 0;
+
+    function receiveData(socket) {
+      logger.debug('we got a connection from leader');
+
+      socket.on('data', function (data) {
+        data = data.toString();
+        totalLength += data.length;
+        logger.debug(
+          'we received data from leader, length: %d, total length: %d',
+          data.length, totalLength
+        );
+
+        if (totalLength >= DATA_LENGTH) {
+          // This is our confirmation byte.
+          logger.debug('we sent a confirmation byte to leader');
+          socket.write('0', function () {
+
+            socket.once('close', function () {
+              stop()
+              .then(function () {
+                logger.debug('we are stopped');
+                resolve();
+              });
+            })
+            socket.end();
+          });
+        }
+      })
+
+      .on('error', function (error) {
+        logger.error('Got error: \'%s\', stack: \'%s\'', error.toString(), error.stack);
+      });
+    }
+
+    start();
   });
 }
 
@@ -159,177 +343,21 @@ resolveLeader()
 })
 .then(function (isLeader) {
   if (isLeader) {
-    sendRandomData();
+    return sendRandomData();
   } else {
-    receiveRandomData();
+    return receiveRandomData();
   }
+})
+.then(function () {
+  process.exit(0);
 });
 
-function sendRandomData() {
-  function start () {
-    return new Promise(function (resolve) {
-      Mobile('startListeningForAdvertisements')
-      .callNative(function (error) {
-        assert(!error, 'Called startListeningForAdvertisements without error');
-        Mobile('peerAvailabilityChanged').registerToNative(peersHandler);
-        resolve();
-      });
-    });
-  }
-
-  function stop() {
-    return new Promise(function (resolve) {
-      Mobile('stopListeningForAdvertisements')
-      .callNative(function (error) {
-        assert(!error, 'Should be able to call stopListeningForAdvertisements');
-        resolve();
-      });
-    });
-  }
-
-  var confirmedPeers = {};
-
-  function resolveConfirmedPeers () {
-    if (Object.keys(peerIds).length === Object.keys(confirmedPeers).length) {
-      stop()
-      .then(function () {
-        logger.debug('we are stopped');
-      });
-    }
-  }
-
-  function peersHandler (peers) {
-    peers.forEach(function (peer) {
-      if (peer.peerAvailable) {
-        sendData(peer.peerIdentifier);
-      }
-    });
-  }
-
-  var randomData = randomstring.generate(DATA_LENGTH);
-
-  function sendData (id) {
-    if (confirmedPeers[id]) {
-      logger.debug('we already finished with peer: \'%s\'', id);
-      return;
-    }
-
-    Mobile('connect')
-    .callNative(id, function (error, data) {
-      if (error) {
-        logger.error('Got error \'%s\'', error.toString());
-        return;
-      }
-      data = JSON.parse(data);
-
-      var socket = new net.Socket()
-      .connect(data.listeningPort, function() {
-        socket.write(randomData);
-
-        socket.on('data', function (data) {
-          if (data.toString() === '0') {
-            logger.debug('we finished with peer: \'%s\'', id);
-            confirmedPeers[id] = true;
-            resolveConfirmedPeers();
-          } else {
-            logger.error('we received invalid confirmation');
-          }
-          socket.end();
-        })
-
-        .on('error', function (error) {
-          logger.error('Got error: \'%s\', stack: \'%s\'', error.toString(), error.stack);
-
-          // We want to reconnect.
-          socket.destroy();
-          setImmediate(function () {
-            sendData(id);
-          });
-        });
-      });
-    });
-  }
-
-  start();
-}
-
-function receiveRandomData() {
-  var server;
-
-  function start () {
-    return new Promise(function (resolve) {
-      server = makeIntoCloseAllServer(
-        net.createServer(receiveData)
-      );
-      server.listen(0, function () {
-        resolve();
-      });
-    })
-    .then(function () {
-      return new Promise(function (resolve) {
-        Mobile('startUpdateAdvertisingAndListening')
-        .callNative(server.address().port, function (error) {
-          assert(!error, 'Called startUpdateAdvertisingAndListening without error');
-          resolve();
-        });
-      });
-    });
-  }
-
-  function stop() {
-    return new Promise(function (resolve) {
-      Mobile('stopAdvertisingAndListening')
-      .callNative(function (error) {
-        assert(!error, 'Should be able to call stopAdvertisingAndListening');
-        resolve();
-      });
-    })
-    .then(function () {
-      return new Promise(function (resolve) {
-        server.closeAll(resolve);
-      });
-    });
-  }
-
-  var totalLength = 0;
-
-  function receiveData(socket) {
-    logger.debug('we got a connection from leader');
-
-    socket.on('data', function (data) {
-      data = data.toString();
-      totalLength += data.length;
-      logger.debug(
-        'we received data from leader, length: %d, total length: %d',
-        data.length, totalLength
-      );
-
-      if (totalLength >= DATA_LENGTH) {
-        // This is our confirmation byte.
-        logger.debug('we sent a confirmation byte to leader');
-        socket.write('0', function () {
-
-          socket.once('close', function () {
-            stop()
-            .then(function () {
-              logger.debug('we are stopped');
-            });
-          })
-          socket.end();
-        });
-      }
-    })
-
-    .on('error', function (error) {
-      logger.error('Got error: \'%s\', stack: \'%s\'', error.toString(), error.stack);
-    });
-  }
-
-  start();
-}
+setTimeout(function () {
+  logger.error('finished with timeout');
+  process.exit(1);
+}, TEST_TIMEOUT);
 
 logger.debug('1 meg app is loaded');
-setTimeout(function () {
+process.once('exit', function (){
   logger.debug('1 meg app is finished');
-  process.exit();
-}, TEST_TIMEOUT);
+});
