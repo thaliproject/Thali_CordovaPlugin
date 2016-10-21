e68a5d3457659dd029450761b94071236956e77e
andrew-aladev <aladjev.andrew@gmail.com>
2016-10-21 13:39:13 +0300

    Revert "added infinite loop for thali manager coordinated"
    This reverts commit b4197495b1545ab1d320aebead58e65543e74e6d.

--- test/www/jxcore/bv_tests/testThaliManagerCoordinated.js
+++ test/www/jxcore/bv_tests/testThaliManagerCoordinated.js
@@ -7,7 +7,6 @@ if (!tape.coordinated) {
 
 var testUtils = require('../lib/testUtils.js');
 
-var objectAssign = require('object-assign');
 var crypto = require('crypto');
 var Promise = require('bluebird');
 var PouchDB = require('pouchdb');
@@ -54,7 +53,9 @@ var test = tape({
 // We have 'localDoc' and 'oldRemoteDocs' already in DB.
 // We are waiting until 'newRemoteDocs' will appears in DB.
 // We are waiting for confirmation that 'localDoc' and 'oldRemoteDocs' is in DB too.
-function waitForRemoteDocs(pouchDB, localDoc, oldRemoteDocs, newRemoteDocs) {
+function waitForRemoteDocs(
+  pouchDB, localDoc, oldRemoteDocs, newRemoteDocs, ignoreRev
+) {
   // We can remove '_rev' key from compared values.
   // We can just stringify docs, they wont be circular.
   function toString(doc) {
@@ -166,7 +167,7 @@ test('test write', function (t) {
   })
   .then(function () {
     // Our local DB should have this doc.
-    return waitForRemoteDocs(pouchDB, localDoc, [], []);
+    return waitForRemoteDocs(pouchDB, localDoc, [], [], false);
   })
   .then(function () {
     // Starting Thali Manager.
@@ -191,7 +192,7 @@ test('test write', function (t) {
     // Lets check that all imaginary docs has been replicated to our local db.
     // We can't predict what '_rev' remote doc will have,
     // so we shouldn't check '_rev' here.
-    return waitForRemoteDocs(pouchDB, localDoc, [], docs);
+    return waitForRemoteDocs(pouchDB, localDoc, [], docs, true);
   })
   .then(function () {
     t.end();
@@ -242,7 +243,7 @@ test('test repeat write 1', function (t) {
         test2: true
       };
     });
-    return waitForRemoteDocs(pouchDB, localDoc, oldDocs, newDocs);
+    return waitForRemoteDocs(pouchDB, localDoc, oldDocs, newDocs, true);
   })
   .then(function () {
     t.end();
@@ -263,17 +264,6 @@ test('test repeat write 2', function (t) {
   // It should consist of it's public key (base64 representation)
   // and 2 test booleans.
   var localDoc;
-
-  // Our partners should update its docs the same way.
-  var oldDocs = partnerKeys.map(function (partnerKey) {
-    return {
-      _id: partnerKey.toString('base64'),
-      test1: true,
-      test2: true
-    };
-  });
-  var newDocs;
-
   thaliManager.start(partnerKeys)
   .then(function () {
     return pouchDB.get(publicBase64KeyForLocalDevice);
@@ -285,63 +275,30 @@ test('test repeat write 2', function (t) {
     // Lets update our doc with new boolean.
     localDoc.test3 = true;
     return pouchDB.put(localDoc)
-    .then(function (response) {
-      localDoc._rev = response.rev;
-    });
-  })
-  .then(function () {
-    newDocs = oldDocs.map(function (doc) {
-      return objectAssign({}, doc, {
-        test3: true
-      });
-    });
-    return waitForRemoteDocs(pouchDB, localDoc, oldDocs, newDocs)
-    .then(function () {
-      oldDocs = newDocs;
-    });
-  })
-
-  .then(function () {
-    // infinite loop
-    function sendData(index) {
-      console.log('sending data for index: %d', index);
-      var name = 'test' + index;
-      localDoc[name] = true;
-
-      return pouchDB.put(localDoc)
       .then(function (response) {
         localDoc._rev = response.rev;
-      })
-      .then(function () {
-        newDocs = oldDocs.map(function (doc) {
-          doc = objectAssign({}, doc);
-          doc[name] = true;
-          return doc;
-        });
-        return waitForRemoteDocs(pouchDB, localDoc, oldDocs, newDocs)
-        .then(function () {
-          oldDocs = newDocs;
-        });
-      })
-      .then(function () {
-        console.log('sent data for index: %d', index);
-
-        return new Promise(function (resolve, reject) {
-          setImmediate(function () {
-            sendData(index + 1)
-            .then(resolve)
-            .catch(reject);
-          });
-        });
       });
-    }
-    return sendData(4);
   })
-
   .then(function () {
-    t.end();
+    // Our partners should update its docs the same way.
+    var oldDocs = partnerKeys.map(function (partnerKey) {
+      return {
+        _id: partnerKey.toString('base64'),
+        test1: true,
+        test2: true
+      };
+    });
+    var newDocs = partnerKeys.map(function (partnerKey) {
+      return {
+        _id: partnerKey.toString('base64'),
+        test1: true,
+        test2: true,
+        test3: true
+      };
+    });
+    return waitForRemoteDocs(pouchDB, localDoc, oldDocs, newDocs, true);
   })
-  .catch(function () {
+  .then(function () {
     t.end();
   });
 });
