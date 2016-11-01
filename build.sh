93c399b4d033243991fd192c1d08aad651bc77e3
Ilya Laryionau <larryonoff@gmail.com>
2016-11-01 20:14:29 +0300

    update build.sh logging

--- build.sh
+++ build.sh
@@ -93,6 +93,8 @@ thali/install/setUpTests.sh $TEST_TYPE $SERVER_ADDRESS
 
 if [ $RUN_IN_CI == 0 ]; then
 
+  echo "We're running in CI. So we're copying .apk and .app into ${WORKING_DIR}"
+
   # Make sure we are back in the project root folder
   # after the setting up the tests
   cd $WORKING_DIR
