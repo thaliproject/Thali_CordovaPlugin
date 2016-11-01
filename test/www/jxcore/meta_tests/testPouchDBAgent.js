05a4ffb89f3e92118703bc0a953f7d169beb69a7
Andrew Aladjev <aladjev.andrew@gmail.com>
2016-11-01 15:49:31 +0300

    implemented pouchdb agent test

--- test/www/jxcore/meta_tests/testPouchDBAgent.js
+++ test/www/jxcore/meta_tests/testPouchDBAgent.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var Promise        = require('bluebird');
+var objectAssign   = require('object-assign');
 var ForeverAgent   = require('forever-agent');
 var express        = require('express');
 var expressPouchDB = require('express-pouchdb');
@@ -20,8 +22,15 @@ var test = tape({
   }
 });
 
-test('a', function (t) {
+test('PouchDB agent works as expected', function (t) {
   var agent = new ForeverAgent();
+
+  var requestsData = [];
+  var _addRequest = agent.addRequest;
+  agent.addRequest = function (data) {
+    requestsData.push(objectAssign({}, data));
+    return _addRequest.apply(this, arguments);
+  }
 
   var app = express();
   app.use('/db', expressPouchDB(PouchDB));
@@ -29,21 +38,35 @@ test('a', function (t) {
   var server = http.createServer(app);
   server.listen(0, function () {
     var port  = server.address().port;
-    var url   = 'http://localhost:' + port + '/db/fit';
+    var url   = 'http://localhost:' + port + '/db';
     var db    = new PouchDB(url , {
       ajax: {
         agent: agent
       }
     });
-    console.log(db.__opts.ajax.agent === agent);
-    db.get('fit')
+
+    function query(name) {
+      return db.get(name)
+        .then(function () {
+          t.fail('we should not find a document');
+        })
+        .catch(function (error) {
+          t.ok(error, 'error is not empty');
+          t.equals(error.status, 404, 'status should be 404');
+        })
+        .then(function () {
+          var lastData = requestsData[requestsData.length - 1];
+          t.equals(lastData.method, 'GET', 'method is \'get\'');
+          t.equals(lastData.path, '/db/' + name + '?', 'path is ok');
+        });
+    }
+
+    query('fit')
       .then(function () {
-        t.pass('ok');
-        t.end();
+        return query('foo');
       })
-      .catch(function (error) {
-        console.error('got error', error);
-        t.fail('got error' + error);
+      .then(function () {
+        t.ok(requestsData.length > 2, 'we should call agent more than twice');
         t.end();
       });
   });
