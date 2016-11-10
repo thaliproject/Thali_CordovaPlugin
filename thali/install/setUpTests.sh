9cd5996e120a9efa06025ec06601260cfb01dc48
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-11-10 13:53:55 +0300

    Revert "Disable Android build temporarily."
    This reverts commit 1c3697bccf885f406110d1625f3cdd2d608c58ba.

--- thali/install/setUpTests.sh
+++ thali/install/setUpTests.sh
@@ -182,10 +182,10 @@ build_ios_if_possible()
 # Please note that functions order is important
 IPADDRESS=${2:-}
 prepare_project ${IPADDRESS}
-#add_android_platform
+add_android_platform
 add_ios_platform_if_possible
 install_thali $1 ${IPADDRESS}
-#build_android
+build_android
 build_ios_if_possible
 
 echo "Remember to start the test coordination server by running jx index.js"
