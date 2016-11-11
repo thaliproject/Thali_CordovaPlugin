bd573e9e1d35c4d2388b037eb4301860b0af9009
Ilya Laryionau <larryonoff@gmail.com>
2016-11-11 13:03:12 +0300

    fix validateBuildEnvironment dependencies

--- thali/install/setUpTests.sh
+++ thali/install/setUpTests.sh
@@ -42,6 +42,8 @@ TEST_PROJECT_ROOT_DIR=${REPO_ROOT_DIR}/../${TEST_PROJECT_NAME}
 prepare_project()
 {
   IPADDRESS=${1:-}
+  cd $REPO_ROOT_DIR/thali/install
+  npm install --no-optional
   node $REPO_ROOT_DIR/thali/install/validateBuildEnvironment.js
 
   echo "Preparing ${TEST_PROJECT_NAME} Cordova project"
