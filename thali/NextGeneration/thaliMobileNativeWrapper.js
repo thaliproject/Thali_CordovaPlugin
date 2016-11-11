1c6189f60b363a9d44b63233ed2de9edfe12eea1
Igor Serzhan <igor.serzhan@hiqo-solutions.com>
2016-11-11 13:39:50 +0300

    Implemented incomingConnectionToPortNumberFailed for iOS platform

--- thali/NextGeneration/thaliMobileNativeWrapper.js
+++ thali/NextGeneration/thaliMobileNativeWrapper.js
@@ -1097,9 +1097,11 @@ module.exports._registerToNative = function () {
         return;
       }
 
-      if (gServersManagerLocalPort !== portNumber) {
+      var originalPortNumber = (platform.isAndroid) ? gServersManagerLocalPort : gRouterServerPort;
+
+      if (originalPortNumber !== portNumber) {
         logger.info('got incomingConnectionToPortNumberFailed for port ' +
-          portNumber + ' but we are listening on ' + gServersManagerLocalPort);
+          portNumber + ' but we are listening on ' + originalPortNumber);
         return;
       }
 
