989d8324062109040119f3cedc599178a8da8f1c
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-11-14 19:29:44 +0300

    Update code style in BrowserManager.swift.

--- lib/ios/ThaliCore/ThaliCore/MultipeerConnectivity/BrowserManager.swift
+++ lib/ios/ThaliCore/ThaliCore/MultipeerConnectivity/BrowserManager.swift
@@ -13,30 +13,83 @@
 public final class BrowserManager {
 
   // MARK: - Public state
+
+  /**
+   Bool flag indicates if `BrowserManager` object is listening for advertisements.
+   */
   public var listening: Bool {
     return currentBrowser?.listening ?? false
   }
 
   // MARK: - Internal state
+
+  /**
+   `Peer` objects that can be invited into the session.
+   */
   internal private(set) var availablePeers: Atomic<[Peer]> = Atomic([])
+
+  /**
+   Active `Relay` objects.
+   */
   internal private(set) var activeRelays: Atomic<[String: BrowserRelay]> = Atomic([:])
 
   // MARK: - Private state
+
+  /**
+   Currently active `Browser` object.
+   */
   private var currentBrowser: Browser?
+
+  /**
+   The type of service to browse.
+   */
   private let serviceType: String
+
+  /**
+   Invite timeout after which the session between peers can be treated as failed.
+   */
   private let inputStreamReceiveTimeout: NSTimeInterval
-  private let peersAvailabilityChangedHandler: ([PeerAvailability]) -> Void
+
+  /**
+   Handle change of peer availability.
+   */
+  private let peerAvailabilityChangedHandler: ([PeerAvailability]) -> Void
 
   // MARK: - Public state
+
+  /**
+   Returns a new `BrowserManager` object.
+
+   - parameters:
+     - serviceType:
+       The type of service to browse.
+
+     - inputStreamReceiveTimeout:
+       Invite timeout after which the session between peers can be treated as failed.
+   
+     - peerAvailabilityChangedHandler:
+       Called when PeerAvailability is changed.
+
+   - returns:
+     An initialized `BrowserManager` object.
+   */
   public init(serviceType: String,
               inputStreamReceiveTimeout: NSTimeInterval,
-              peersAvailabilityChangedHandler: ([PeerAvailability]) -> Void) {
+              peerAvailabilityChanged: ([PeerAvailability]) -> Void) {
     self.serviceType = serviceType
-    self.peersAvailabilityChangedHandler = peersAvailabilityChangedHandler
+    self.peerAvailabilityChangedHandler = peerAvailabilityChanged
     self.inputStreamReceiveTimeout = inputStreamReceiveTimeout
   }
 
   // MARK: - Public methods
+
+  /**
+   This method instructs to discover what other devices are within range.
+   
+   - parameters:
+     - errorHandler:
+       Called when advertisement fails.
+   */
   public func startListeningForAdvertisements(errorHandler: ErrorType -> Void) {
     if currentBrowser != nil { return }
 
@@ -53,16 +106,32 @@ public final class BrowserManager {
     currentBrowser = newBrowser
   }
 
+  /**
+   Stops listening for advertisements.
+   */
   public func stopListeningForAdvertisements() {
     currentBrowser?.stopListening()
     currentBrowser = nil
   }
 
+  /**
+   Establish a non-TCP/IP connection to the identified peer and then create a
+   TCP/IP bridge on top of that connection which can be accessed by
+   opening a TCP/IP connection to the port returned in the callback.
+   
+   - parameters:
+     - peerIdentifier:
+       A value mapped to the UUID part of the remote peer's MCPeerID.
+     - syncValue:
+       An opaque string that used for tracking callback calls.
+     - completion:
+       Called when connect succeeded or failed.
+   */
   public func connectToPeer(peerIdentifier: String,
                             syncValue: String,
                             completion: (syncValue: String,
-    error: ErrorType?,
-    port: UInt16?) -> Void) {
+                                         error: ErrorType?,
+                                         port: UInt16?) -> Void) {
 
     guard let currentBrowser = self.currentBrowser else {
       completion(syncValue: syncValue,
@@ -87,36 +156,34 @@ public final class BrowserManager {
 
     do {
       let nonTCPsession = try currentBrowser.inviteToConnect(
-        lastGenerationPeer,
-        sessionConnected: {
-          [weak self] in
-          guard let strongSelf = self else { return }
+                                      lastGenerationPeer,
+                                      sessionConnected: {
+                                        [weak self] in
+                                        guard let strongSelf = self else { return }
 
-          let relay =
-            strongSelf.activeRelays.value[peerIdentifier]
+                                        let relay = strongSelf.activeRelays.value[peerIdentifier]
+                                        relay?.openRelay {
+                                          port, error in
+                                          completion(syncValue: syncValue,
+                                                     error: error,
+                                                     port: port)
+                                        }
+                                      },
+                                      sessionNotConnected: {
+                                        [weak self] in
+                                        guard let strongSelf = self else { return }
 
-          relay?.openRelay {
-            port, error in
-            completion(syncValue: syncValue,
-              error: error,
-              port: port)
-          }
-        },
-        sessionNotConnected: {
-          [weak self] in
-          guard let strongSelf = self else { return }
-
-          strongSelf.activeRelays.modify {
-            if let relay = $0[peerIdentifier] {
-              relay.closeRelay()
-            }
-            $0.removeValueForKey(peerIdentifier)
-          }
-        })
+                                        strongSelf.activeRelays.modify {
+                                          if let relay = $0[peerIdentifier] {
+                                            relay.closeRelay()
+                                          }
+                                          $0.removeValueForKey(peerIdentifier)
+                                        }
+                                      })
 
       activeRelays.modify {
         let relay = BrowserRelay(with: nonTCPsession,
-          createVirtualSocketTimeout: self.inputStreamReceiveTimeout)
+                                 createVirtualSocketTimeout: self.inputStreamReceiveTimeout)
         $0[peerIdentifier] = relay
       }
     } catch let error {
@@ -126,6 +193,11 @@ public final class BrowserManager {
     }
   }
 
+  /**
+   - parameters:
+     - peerIdentifer:
+       A value mapped to the UUID part of the remote peer's MCPeerID.
+   */
   public func disconnect(peerIdentifer: String) {
     guard let relay = activeRelays.value[peerIdentifer] else {
       return
@@ -135,22 +207,49 @@ public final class BrowserManager {
   }
 
   // MARK: - Internal methods
+
+  /**
+   Returns the highest generation advertised for given peerIdentifier.
+
+   - parameters:
+     - peerIdentifier:
+       A value mapped to the UUID part of the remote peer's MCPeerID.
+
+   - returns:
+     `Peer` object with the highest generation advertised for given *peerIdentifier*.
+     If there are no peers with given *peerIdentifier*, return nil.
+   */
   func lastGenerationPeer(for peerIdentifier: String) -> Peer? {
     return availablePeers.withValue {
       $0
-        .filter { $0.uuid == peerIdentifier }
-        .maxElement { $0.0.generation < $0.1.generation }
+      .filter { $0.uuid == peerIdentifier }
+      .maxElement { $0.0.generation < $0.1.generation }
     }
   }
 
   // MARK: - Private handlers
+
+  /**
+   Handle finding nearby peer.
+
+   - parameters:
+     - peer:
+       `Peer` object which was founded.
+   */
   private func handleFound(peer: Peer) {
     availablePeers.modify { $0.append(peer) }
 
     let updatedPeerAvailability = PeerAvailability(peer: peer, available: true)
-    peersAvailabilityChangedHandler([updatedPeerAvailability])
+    peerAvailabilityChangedHandler([updatedPeerAvailability])
   }
 
+  /**
+   Handle losing nearby peer.
+
+   - parameters:
+     - peer:
+       `Peer` object which was lost.
+   */
   private func handleLost(peer: Peer) {
     availablePeers.modify {
       if let indexOfLostPeer = $0.indexOf(peer) {
@@ -159,6 +258,6 @@ public final class BrowserManager {
     }
 
     let updatedPeerAvailability = PeerAvailability(peer: peer, available: false)
-    peersAvailabilityChangedHandler([updatedPeerAvailability])
+    peerAvailabilityChangedHandler([updatedPeerAvailability])
   }
 }
