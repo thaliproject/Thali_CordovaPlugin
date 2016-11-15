8abf16d1933f25d062a64b0c4133b08f0ed41b88
Dersim Davaod <dersim.davaod@hiqo-solutions.com>
2016-11-15 11:58:11 +0300

    Update indentation in VirtualSocketBuilder.swift.

--- lib/ios/ThaliCore/ThaliCore/SocketConnection/VirtualSocketBuilder.swift
+++ lib/ios/ThaliCore/ThaliCore/SocketConnection/VirtualSocketBuilder.swift
@@ -12,15 +12,15 @@
  */
 class VirtualSocketBuilder {
 
-    // MARK: - Private state
-    private let nonTCPsession: Session
-    private var outputStream: NSOutputStream?
-    private var inputStream: NSInputStream?
+  // MARK: - Private state
+  private let nonTCPsession: Session
+  private var outputStream: NSOutputStream?
+  private var inputStream: NSInputStream?
 
-    // MARK: - Initialization
-    init(with nonTCPsession: Session) {
-        self.nonTCPsession = nonTCPsession
-    }
+  // MARK: - Initialization
+  init(with nonTCPsession: Session) {
+    self.nonTCPsession = nonTCPsession
+  }
 }
 
 /**
@@ -28,63 +28,63 @@ class VirtualSocketBuilder {
  */
 final class BrowserVirtualSocketBuilder: VirtualSocketBuilder {
 
-    // MARK: - Internal state
-    internal private(set) var streamName: String
+  // MARK: - Internal state
+  internal private(set) var streamName: String
 
-    // MARK: - Private state
-    private let streamReceivedBackTimeout: NSTimeInterval
-    private var completion: ((VirtualSocket?, ErrorType?) -> Void)?
-    private var streamReceivedBack = Atomic(false)
+  // MARK: - Private state
+  private let streamReceivedBackTimeout: NSTimeInterval
+  private var completion: ((VirtualSocket?, ErrorType?) -> Void)?
+  private var streamReceivedBack = Atomic(false)
 
-    // MARK: - Initialization
-    init(with nonTCPsession: Session,
-              streamName: String,
-              streamReceivedBackTimeout: NSTimeInterval) {
-        self.streamName = streamName
-        self.streamReceivedBackTimeout = streamReceivedBackTimeout
-        super.init(with: nonTCPsession)
-    }
+  // MARK: - Initialization
+  init(with nonTCPsession: Session,
+            streamName: String,
+            streamReceivedBackTimeout: NSTimeInterval) {
+    self.streamName = streamName
+    self.streamReceivedBackTimeout = streamReceivedBackTimeout
+    super.init(with: nonTCPsession)
+  }
 
-    // MARK: - Internal methods
-    func startBuilding(with completion: (VirtualSocket?, ErrorType?) -> Void) {
-        self.completion = completion
+  // MARK: - Internal methods
+  func startBuilding(with completion: (VirtualSocket?, ErrorType?) -> Void) {
+    self.completion = completion
 
-        do {
-            let outputStream = try nonTCPsession.startOutputStream(with: streamName)
-            self.outputStream = outputStream
+    do {
+      let outputStream = try nonTCPsession.startOutputStream(with: streamName)
+      self.outputStream = outputStream
 
-            let streamReceivedBackTimeout = dispatch_time(
-                DISPATCH_TIME_NOW,
-                Int64(self.streamReceivedBackTimeout * Double(NSEC_PER_SEC))
-            )
-            dispatch_after(streamReceivedBackTimeout, dispatch_get_main_queue()) {
-                [weak self] in
-                guard let strongSelf = self else { return }
+      let streamReceivedBackTimeout = dispatch_time(
+        DISPATCH_TIME_NOW,
+        Int64(self.streamReceivedBackTimeout * Double(NSEC_PER_SEC))
+      )
+      dispatch_after(streamReceivedBackTimeout, dispatch_get_main_queue()) {
+        [weak self] in
+        guard let strongSelf = self else { return }
 
-                if strongSelf.streamReceivedBack.value == false {
-                    strongSelf.completion?(nil, ThaliCoreError.ConnectionTimedOut)
-                    strongSelf.completion = nil
-                }
-            }
-        } catch _ {
-            self.completion?(nil, ThaliCoreError.ConnectionFailed)
+        if strongSelf.streamReceivedBack.value == false {
+          strongSelf.completion?(nil, ThaliCoreError.ConnectionTimedOut)
+          strongSelf.completion = nil
         }
+      }
+    } catch _ {
+      self.completion?(nil, ThaliCoreError.ConnectionFailed)
+    }
+  }
+
+  func completeVirtualSocket(with inputStream: NSInputStream) {
+
+    streamReceivedBack.modify { $0 = true }
+
+    guard let outputStream = outputStream else {
+      completion?(nil, ThaliCoreError.ConnectionFailed)
+      completion = nil
+      return
     }
 
-    func completeVirtualSocket(with inputStream: NSInputStream) {
-
-        streamReceivedBack.modify { $0 = true }
-
-        guard let outputStream = outputStream else {
-            completion?(nil, ThaliCoreError.ConnectionFailed)
-            completion = nil
-            return
-        }
-
-        let vs = VirtualSocket(with: inputStream, outputStream: outputStream)
-        completion?(vs, nil)
-        completion = nil
-    }
+    let vs = VirtualSocket(with: inputStream, outputStream: outputStream)
+    completion?(vs, nil)
+    completion = nil
+  }
 }
 
 /**
@@ -92,25 +92,25 @@ final class BrowserVirtualSocketBuilder: VirtualSocketBuilder {
  */
 final class AdvertiserVirtualSocketBuilder: VirtualSocketBuilder {
 
-    // MARK: - Private state
-    private var completion: (VirtualSocket?, ErrorType?) -> Void
+  // MARK: - Private state
+  private var completion: (VirtualSocket?, ErrorType?) -> Void
 
-    // MARK: - Initialization
-    required init(with nonTCPsession: Session,
-                       completion: ((VirtualSocket?, ErrorType?) -> Void)) {
-        self.completion = completion
-        super.init(with: nonTCPsession)
-    }
+  // MARK: - Initialization
+  required init(with nonTCPsession: Session,
+                     completion: ((VirtualSocket?, ErrorType?) -> Void)) {
+    self.completion = completion
+    super.init(with: nonTCPsession)
+  }
 
-    // MARK: - Internal methods
-    func createVirtualSocket(with inputStream: NSInputStream, inputStreamName: String) {
-        do {
-            let outputStream = try nonTCPsession.startOutputStream(with: inputStreamName)
-            let virtualNonTCPSocket = VirtualSocket(with: inputStream,
-                                                    outputStream: outputStream)
-            completion(virtualNonTCPSocket, nil)
-        } catch let error {
-            completion(nil, error)
-        }
+  // MARK: - Internal methods
+  func createVirtualSocket(with inputStream: NSInputStream, inputStreamName: String) {
+    do {
+      let outputStream = try nonTCPsession.startOutputStream(with: inputStreamName)
+      let virtualNonTCPSocket = VirtualSocket(with: inputStream,
+                                              outputStream: outputStream)
+      completion(virtualNonTCPSocket, nil)
+    } catch let error {
+      completion(nil, error)
     }
+  }
 }
