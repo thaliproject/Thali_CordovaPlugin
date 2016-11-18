66cc45b27d9ff1fb320a1c0c150d3cea4e4b1e57
Igor Serzhan <igor.serzhan@hiqo-solutions.com>
2016-11-18 15:30:30 +0300

    Small syntax fix

--- thali/NextGeneration/thaliMobileNativeWrapper.js
+++ thali/NextGeneration/thaliMobileNativeWrapper.js
@@ -388,7 +388,7 @@ function stopNative() {
         if (Object.keys(errorDescriptions).length === 0) {
           resolve();
         } else {
-          reject(errorDescriptions)
+          reject(errorDescriptions);
         }
 
       });
