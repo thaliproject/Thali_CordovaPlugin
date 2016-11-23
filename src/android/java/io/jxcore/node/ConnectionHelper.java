4e01fb79bd04a82b1ef86853626b386dd6e2f01c
Eugene Vabishchevich <eugene.vabishchevich@hiqo-solutions.com>
2016-11-23 16:50:03 +0300

    Fixed cast exception. Implemented proper logic for 1507 issue

--- src/android/java/io/jxcore/node/ConnectionHelper.java
+++ src/android/java/io/jxcore/node/ConnectionHelper.java
@@ -610,12 +610,16 @@ public class ConnectionHelper
                             + who.getPeerProperties().toString()
                             + " disconnected: " + errorMessage);
 
-                        final IncomingSocketThread incomingSocketThread = (IncomingSocketThread) who;
-                        boolean closed = mConnectionModel.closeAndRemoveIncomingConnectionThread(incomingSocketThread.getId());
-                        if (!closed) {
-                            throw new RuntimeException("Trying to close nonexistent incoming connection");
-                        }
-                        JXcoreExtension.notifyIncomingConnectionToPortNumberFailed(incomingSocketThread.getTcpPortNumber());
+                        final String peerId = who.getPeerProperties().getId();
+                        mConnectionModel.closeAndRemoveOutgoingConnectionThread(peerId);
+                    }
+
+                    @Override
+                    public void onTransferError(SocketThreadBase who, String errorMessage) {
+                        Log.w(TAG, "onTransferError: Outgoing connection, peer "
+                            + who.getPeerProperties().toString()
+                            + ", data transfer error: " + errorMessage);
+                        onDisconnected(who, errorMessage);
                     }
                 });
         } catch (IOException e) {
@@ -704,6 +708,19 @@ public class ConnectionHelper
                         Log.w(TAG, "onDisconnected: Incoming connection, peer "
                             + who.getPeerProperties().toString()
                             + " disconnected: " + errorMessage);
+
+                        final IncomingSocketThread incomingSocketThread = (IncomingSocketThread) who;
+                        boolean closed = mConnectionModel.closeAndRemoveIncomingConnectionThread(incomingSocketThread.getId());
+                        if (!closed) {
+                            throw new RuntimeException("Trying to close nonexistent incoming connection");
+                        }
+                        JXcoreExtension.notifyIncomingConnectionToPortNumberFailed(incomingSocketThread.getTcpPortNumber());
+                    }
+
+                    @Override
+                    public void onTransferError(SocketThreadBase who, String errorMessage) {
+                        Log.i(TAG, "onTransferError: Incoming connection, peer "
+                            + who.getPeerProperties().toString() + ", data transfer error, closing connection...");
 
                         final IncomingSocketThread incomingSocketThread = (IncomingSocketThread) who;
                         mConnectionModel.closeAndRemoveIncomingConnectionThread(incomingSocketThread.getId());
