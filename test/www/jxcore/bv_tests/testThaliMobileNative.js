5c082d53a165f82f7de0df659e53fbf106f89281
Eugene Chapko <eugene.chapko@gmail.com>
2017-03-16 19:28:13 +0300

    Add simple data test with extensive logs

--- test/www/jxcore/bv_tests/testThaliMobileNative.js
+++ test/www/jxcore/bv_tests/testThaliMobileNative.js
@@ -303,6 +303,139 @@ test('Can connect to a remote peer', function (t) {
   }, 5000);
 });
 
+function findPeerAndConnect(advertisingPort) {
+  return new Promise(function (resolve, reject) {
+    var connecting = false;
+
+    Mobile('peerAvailabilityChanged').registerToNative(function (peers) {
+      peers.forEach(function (peer) {
+        if (peer.peerAvailable && !connecting) {
+          connecting = true;
+          thaliMobileNativeTestUtils.connectToPeer(peer)
+            .then(function (connection) {
+              resolve({
+                connection: connection,
+                peer: peer
+              });
+            })
+            .catch(function (error) {
+              error.peer = peer;
+              reject(error);
+            });
+        }
+      });
+    });
+    Mobile('startUpdateAdvertisingAndListening')
+      .callNative(advertisingPort, function (err) {
+        if (err) {
+          return reject(err);
+        }
+        Mobile('startListeningForAdvertisements').callNative(function (err) {
+          if (err) {
+            return reject(err);
+          }
+        });
+      });
+  });
+}
+
+function netConnect(port) {
+  return new Promise(function (resolve, reject) {
+    var connectErrorHandler = function (error) {
+      console.log('Connection to the %d port on localhost failed: %s',
+        port, error.stack);
+      reject(error);
+    };
+    var client = net.connect(port, function () {
+      client.removeListener('error', connectErrorHandler);
+      console.log('Connected to the %d port on localhost', port);
+      resolve(client);
+    });
+    client.once('error', connectErrorHandler);
+  });
+}
+
+test('Can shift some data', function (t) {
+  var exchangeData = 'small amount of data';
+
+  var formatPrintableData = function (data) {
+    var ellipsis = data.length > 40 ? '...' : '';
+    return '<' + data.slice(0, 40) + ellipsis + '>';
+  };
+
+  var server = net.createServer(function (socket) {
+    var ended = false;
+    var buffer = '';
+    socket.on('data', function (chunk) {
+      buffer += chunk.toString();
+      console.log('Server received (%d bytes): %s',
+        chunk.length, formatPrintableData(chunk.toString()));
+
+      // when received all data, send it back
+      if (buffer.length === exchangeData.length) {
+        console.log('Server received all data: %s',
+          formatPrintableData(buffer.toString()));
+        var rawData = new Buffer(buffer);
+        console.log('Server sends data back to client (%d bytes): %s',
+          rawData.length, formatPrintableData(buffer));
+        socket.write(rawData, function () {
+          console.log('Server data flushed');
+        });
+        ended = true;
+        socket.end(function () {
+          console.log('Server\'s socket stream finished');
+        });
+      }
+    });
+    socket.on('end', function () {
+      // server ends connection, not client
+      if (!ended) {
+        t.fail(new Error('Unexpected end event'));
+      }
+    });
+    socket.on('error', function (error) {
+      t.fail(error.message);
+    });
+  });
+  server = makeIntoCloseAllServer(server);
+  serverToBeClosed = server;
+
+  function shiftData(sock) {
+    sock.on('error', function (error) {
+      console.log('Client socket error:', error.message, error.stack);
+      t.fail(error.message);
+    });
+
+
+    var receivedData = '';
+    sock.on('data', function (chunk) {
+      receivedData += chunk.toString();
+    });
+    sock.on('end', function () {
+      t.equal(receivedData, exchangeData, 'got the same data back');
+      t.end();
+    });
+
+    var rawData = new Buffer(exchangeData);
+    console.log('Client sends data (%d bytes): %s',
+      rawData.length, formatPrintableData(exchangeData));
+    sock.write(rawData, function () {
+      console.log('Client data flushed');
+    });
+  }
+
+  server.listen(0, function () {
+    var port = server.address().port;
+    findPeerAndConnect(port).then(function (info) {
+      console.log('Native connection established. Peer:', info.peer);
+      var nativePort = info.connection.listeningPort;
+      return netConnect(nativePort);
+    }).then(function (socket) {
+      shiftData(socket);
+    });
+  });
+});
+
 test('Can shift large amounts of data', function (t) {
   var connecting = false;
 
