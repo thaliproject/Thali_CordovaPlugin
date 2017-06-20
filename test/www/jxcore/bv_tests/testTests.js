265139272e29f321c6e079e313a288085ee30811
Jarosław Słota <jslota@ra.rockwell.com>
2017-06-20 21:02:18 +0200

    Removed not needed tests

--- test/www/jxcore/bv_tests/testTests.js
+++ test/www/jxcore/bv_tests/testTests.js
@@ -40,18 +40,6 @@ test.skip('another skip', function () { return false; },
   t.end();
 });
 
-test.skip('skip with skip function', function () { return true; },
-  function (t) {
-  t.ok(false, 'Should not execute skipped test');
-  t.end();
-});
-
-test('skip function', function () { return true; },
-  function (t) {
-  t.ok(false, 'Should not execute skipped test');
-  t.end();
-});
-
 var testObject = {
   testSpyMethod: function() {
     logger.debug('test spy method for global sinon sansbox');
